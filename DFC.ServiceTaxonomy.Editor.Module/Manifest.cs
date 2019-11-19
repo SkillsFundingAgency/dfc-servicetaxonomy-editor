79f556e2eca29ed8e84bc6ce3ad9bfbb5946d71b
Phil Davies <github@phildavies.co.uk>
2019-11-19 15:18:14 +0000

    add (untested) module

---
+++ DFC.ServiceTaxonomy.Editor.Module/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "DFC.ServiceTaxonomy.Editor.Module",
+    Author = "National Careers Service",
+    Website = "http://todo.gov.uk",
+    Version = "0.0.1",
+    Description = "todo."
+)]
