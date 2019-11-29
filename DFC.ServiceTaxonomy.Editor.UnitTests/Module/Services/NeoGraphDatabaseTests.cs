0ddcbf10f44dc7558abccb8c1af6c0248e2cec65
Phil Davies <github@phildavies.co.uk>
2019-11-29 11:21:28 +0000

    OC v1 will use .net core 3.1 + some unit test scaffolding

---
+++ DFC.ServiceTaxonomy.Editor.UnitTests/Module/Services/NeoGraphDatabaseTests.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.Editor.Module.Configuration;
+using DFC.ServiceTaxonomy.Editor.Module.Services;
+using Xunit;
+using FakeItEasy;
+using Microsoft.Extensions.Options;
+
+namespace DFC.ServiceTaxonomy.Editor.UnitTests.Module.Services
+{
+    public class NeoGraphDatabaseTests
+    {
+        [Fact]
+        public void NothingTest()
+        {
+            //todo: SFA.DAS.Testing?? more general equivalent?
+            //todo: fluentassertions??
+            //todo: autofixture??
+            var optionsMonitor = A.Fake<IOptionsMonitor<Neo4jConfiguration>>();
+            var neo4jConfiguration = A.Fake<Neo4jConfiguration>();
+            neo4jConfiguration.Endpoint.Uri = "bolt://example.com";
+            A.CallTo(() => optionsMonitor.CurrentValue).Returns(neo4jConfiguration);
+
+            var neo4jGraphDatabase = new NeoGraphDatabase(optionsMonitor);
+
+            //await neo4jGraphDatabase.MergeNode("", new Dictionary<string, object>());
+        }
+    }
+}
