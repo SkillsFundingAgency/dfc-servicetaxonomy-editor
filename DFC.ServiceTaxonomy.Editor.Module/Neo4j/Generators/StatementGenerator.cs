37ee52942176b48999f732edd18f72678a68c390
Phil Davies <github@phildavies.co.uk>
2019-12-02 13:59:09 +0000

    tidy up

--- DFC.ServiceTaxonomy.Editor.Module/Neo4j/Generators/StatementGenerator.cs
+++ DFC.ServiceTaxonomy.Editor.Module/Neo4j/Generators/StatementGenerator.cs
@@ -23,16 +23,6 @@ namespace DFC.ServiceTaxonomy.Editor.Module.Neo4j.Generators
 
             //todo: for same task for create/edit, first delete all given relationships between source and dest nodes
 
-            // match (n:ncs__JobProfile {uri:'https://nationalcareers.service.gov.uk/JobProfile/111111111'})
-            // match (s:ncs__Skill {uri:'https://nationalcareers.service.gov.uk/Skill/111111111'})
-            // match (q:ncs__QCFLevel {uri:'https://nationalcareers.service.gov.uk/QCFLevel/1111111111111'})
-            // optional match (n)-[r2:ncs__test2]->(:ncs__QCFLevel)
-            // optional match (s)-[r1:ncs__test]->(:ncs__JobProfile)
-            // delete r1, r2
-            // merge (n)-[:ncs__test2]->(q)
-            //     merge (s)-[:ncs__test]->(n)
-            // return s, n, q
-
             //todo: bi-directional relationships
             //todo: rewrite for elegance/perf. selectmany?
             const string sourceIdPropertyValueParamName = "sourceIdPropertyValue";
@@ -40,8 +30,6 @@ namespace DFC.ServiceTaxonomy.Editor.Module.Neo4j.Generators
             var existingRelationshipsMatchBuilder = new StringBuilder();
             var mergeBuilder = new StringBuilder();
             var parameters = new Dictionary<string, object> {{sourceIdPropertyValueParamName, sourceIdPropertyValue}};
-            // we could instead just use the final value of ordinal for this...
-            // var existingRelationshipVariables = new List<string>();
             int ordinal = 0;
             //todo: better name relationship=> relationships, relationships=>?
 
@@ -53,14 +41,10 @@ namespace DFC.ServiceTaxonomy.Editor.Module.Neo4j.Generators
                 {
                     string destNodeVariable = $"d{++ordinal}";
                     string destIdPropertyValueParamName = $"{destNodeVariable}Value";
-                    // string existingRelationshipsVariable = $"r{++ordinal}";
-                    // existingRelationshipVariables.Add(existingRelationshipsVariable);
 
                     nodeMatchBuilder.Append($"\r\nmatch ({destNodeVariable}:{relationship.Key.destNodeLabel} {{{relationship.Key.destIdPropertyName}:${destIdPropertyValueParamName}}})");
                     parameters.Add(destIdPropertyValueParamName, destIdPropertyValue);
 
-                    //todo: distinct relationship.Key.relationshipType
-                    //existingRelationshipsMatchBuilder.Append($"\r\noptional match (s)-[:{relationship.Key.relationshipType}]->(:{relationship.Key.destNodeLabel})");
                     distinctRelationshipTypes[relationship.Key.relationshipType] = relationship.Key.destNodeLabel;
 
                     mergeBuilder.Append($"\r\nmerge (s)-[:{relationship.Key.relationshipType}]->({destNodeVariable})");
@@ -74,7 +58,7 @@ namespace DFC.ServiceTaxonomy.Editor.Module.Neo4j.Generators
             }
 
             var existingRelationshipVariablesString = string.Join(',',Enumerable.Range(1, ordinal).Select(o => $"r{o}"));
-            //var existingRelationshipVariablesString = string.Join(',', existingRelationshipVariables);
+
 
             return new Statement($"{nodeMatchBuilder}\r\n{existingRelationshipsMatchBuilder}\r\ndelete {existingRelationshipVariablesString}\r\n{mergeBuilder}\r\nreturn s", parameters);
         }
