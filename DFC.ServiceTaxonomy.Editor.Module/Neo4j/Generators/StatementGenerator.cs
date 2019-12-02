5ecdc83a5fff8a39158a929af6a34a47be5ff1ae
Phil Davies <github@phildavies.co.uk>
2019-12-02 13:57:46 +0000

    support removing relationships when linked content is deleted

--- DFC.ServiceTaxonomy.Editor.Module/Neo4j/Generators/StatementGenerator.cs
+++ DFC.ServiceTaxonomy.Editor.Module/Neo4j/Generators/StatementGenerator.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Neo4j.Driver;
 
@@ -16,28 +18,65 @@ namespace DFC.ServiceTaxonomy.Editor.Module.Neo4j.Generators
         public static Statement MergeRelationships(string sourceNodeLabel, string sourceIdPropertyName, string sourceIdPropertyValue,
             IDictionary<(string destNodeLabel,string destIdPropertyName,string relationshipType),IEnumerable<string>> relationships)
         {
+            if (!relationships.Any()) // could return noop statement instead
+                throw new ArgumentException($"{nameof(MergeRelationships)} called with no relationships");
+
             //todo: for same task for create/edit, first delete all given relationships between source and dest nodes
+
+            // match (n:ncs__JobProfile {uri:'https://nationalcareers.service.gov.uk/JobProfile/111111111'})
+            // match (s:ncs__Skill {uri:'https://nationalcareers.service.gov.uk/Skill/111111111'})
+            // match (q:ncs__QCFLevel {uri:'https://nationalcareers.service.gov.uk/QCFLevel/1111111111111'})
+            // optional match (n)-[r2:ncs__test2]->(:ncs__QCFLevel)
+            // optional match (s)-[r1:ncs__test]->(:ncs__JobProfile)
+            // delete r1, r2
+            // merge (n)-[:ncs__test2]->(q)
+            //     merge (s)-[:ncs__test]->(n)
+            // return s, n, q
+
             //todo: bi-directional relationships
             //todo: rewrite for elegance/perf. selectmany?
             const string sourceIdPropertyValueParamName = "sourceIdPropertyValue";
-            var matchBuilder = new StringBuilder($"match (s:{sourceNodeLabel} {{{sourceIdPropertyName}:${sourceIdPropertyValueParamName}}})");
+            var nodeMatchBuilder = new StringBuilder($"match (s:{sourceNodeLabel} {{{sourceIdPropertyName}:${sourceIdPropertyValueParamName}}})");
+            var existingRelationshipsMatchBuilder = new StringBuilder();
             var mergeBuilder = new StringBuilder();
             var parameters = new Dictionary<string, object> {{sourceIdPropertyValueParamName, sourceIdPropertyValue}};
-            int destOrdinal = 0;
+            // we could instead just use the final value of ordinal for this...
+            // var existingRelationshipVariables = new List<string>();
+            int ordinal = 0;
             //todo: better name relationship=> relationships, relationships=>?
+
+            var distinctRelationshipTypes = new Dictionary<string,string>();
+
             foreach (var relationship in relationships)
             {
                 foreach (string destIdPropertyValue in relationship.Value)
                 {
-                    string destNodeVariable = $"d{++destOrdinal}";
+                    string destNodeVariable = $"d{++ordinal}";
                     string destIdPropertyValueParamName = $"{destNodeVariable}Value";
-                    matchBuilder.Append($", ({destNodeVariable}:{relationship.Key.destNodeLabel} {{{relationship.Key.destIdPropertyName}:${destIdPropertyValueParamName}}})");
+                    // string existingRelationshipsVariable = $"r{++ordinal}";
+                    // existingRelationshipVariables.Add(existingRelationshipsVariable);
+
+                    nodeMatchBuilder.Append($"\r\nmatch ({destNodeVariable}:{relationship.Key.destNodeLabel} {{{relationship.Key.destIdPropertyName}:${destIdPropertyValueParamName}}})");
                     parameters.Add(destIdPropertyValueParamName, destIdPropertyValue);
+
+                    //todo: distinct relationship.Key.relationshipType
+                    //existingRelationshipsMatchBuilder.Append($"\r\noptional match (s)-[:{relationship.Key.relationshipType}]->(:{relationship.Key.destNodeLabel})");
+                    distinctRelationshipTypes[relationship.Key.relationshipType] = relationship.Key.destNodeLabel;
+
                     mergeBuilder.Append($"\r\nmerge (s)-[:{relationship.Key.relationshipType}]->({destNodeVariable})");
                 }
             }
 
-            return new Statement($"{matchBuilder}\r\n{mergeBuilder} return s", parameters);
+            ordinal = 0;
+            foreach (var relationshipTypeToDestNodeLabel in distinctRelationshipTypes)
+            {
+                existingRelationshipsMatchBuilder.Append($"\r\noptional match (s)-[r{++ordinal}:{relationshipTypeToDestNodeLabel.Key}]->(:{relationshipTypeToDestNodeLabel.Value})");
+            }
+
+            var existingRelationshipVariablesString = string.Join(',',Enumerable.Range(1, ordinal).Select(o => $"r{o}"));
+            //var existingRelationshipVariablesString = string.Join(',', existingRelationshipVariables);
+
+            return new Statement($"{nodeMatchBuilder}\r\n{existingRelationshipsMatchBuilder}\r\ndelete {existingRelationshipVariablesString}\r\n{mergeBuilder}\r\nreturn s", parameters);
         }
     }
 }
