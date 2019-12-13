11d196250d9519068008a0986d8f23f428f5f645
Phil Davies <github@phildavies.co.uk>
2019-12-13 12:03:28 +0000

    add todo onlyadd graph sync part syncer

---
+++ DFC.ServiceTaxonomy.Editor.Module/GraphSyncers/GraphUriIdFieldGraphSyncer.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using OrchardCore.ContentManagement.Metadata.Models;
+
+namespace DFC.ServiceTaxonomy.Editor.Module.GraphSyncers
+{
+    //todo: once we swap graph field to graph sync part
+    public class GraphUriIdFieldGraphSyncer
+    {
+        public string PartName
+        {
+            get { return "GraphSyncPart"; }
+        }
+
+        public void AddSyncComponents(
+            dynamic graphSyncContent,
+            Dictionary<string, object> nodeProperties,
+            Dictionary<(string destNodeLabel, string destIdPropertyName, string relationshipType), IEnumerable<string>> nodeRelationships,
+            ContentTypePartDefinition contentTypePartDefinition)
+        {
+            nodeProperties.Add("uri", graphSyncContent.UriId.ToString());
+        }
+    }
+}
