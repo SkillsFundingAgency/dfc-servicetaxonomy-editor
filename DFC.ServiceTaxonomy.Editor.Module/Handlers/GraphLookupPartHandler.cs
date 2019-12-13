db1823c4749cdce7a8335664ad2369f00fcf87a5
Phil Davies <github@phildavies.co.uk>
2019-12-13 11:49:05 +0000

    add todo only

--- DFC.ServiceTaxonomy.Editor.Module/Handlers/GraphLookupPartHandler.cs
+++ DFC.ServiceTaxonomy.Editor.Module/Handlers/GraphLookupPartHandler.cs
@@ -5,6 +5,7 @@ using OrchardCore.ContentManagement.Handlers;
 
 namespace DFC.ServiceTaxonomy.Editor.Module.Handlers
 {
+    //todo: bulk publish doesn't sync
     public class GraphLookupPartHandler : ContentPartHandler<GraphLookupPart>
     {
         public override Task UpdatedAsync(UpdateContentContext context, GraphLookupPart part)
