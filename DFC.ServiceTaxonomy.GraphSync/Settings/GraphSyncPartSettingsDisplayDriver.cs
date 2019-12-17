e16e62a7245ef24070201ca24b1d76af1e6d186f
Phil Davies <github@phildavies.co.uk>
2019-12-17 08:45:32 +0000

    graph sync id field wip

--- DFC.ServiceTaxonomy.GraphSync/Settings/GraphSyncPartSettingsDisplayDriver.cs
+++ DFC.ServiceTaxonomy.GraphSync/Settings/GraphSyncPartSettingsDisplayDriver.cs
@@ -81,29 +81,31 @@ namespace DFC.ServiceTaxonomy.GraphSync.Settings
         // {
             var model = new GraphSyncPartSettingsViewModel();
 
-            await context.Updater.TryUpdateModelAsync(model, Prefix);
-
-            // could do with an ordered set
-            // namespaceprefix should never be null. throw instead?
-            if (model.NamespacePrefix != null)
+            if (await context.Updater.TryUpdateModelAsync(model, Prefix,
+                m => m.NamespacePrefix))
             {
-                var currentNamespacePrefixConfiguration = _namespacePrefixConfiguration.CurrentValue;
+                // could do with an ordered set
+                // namespaceprefix should never be null. throw instead?
+                if (model.NamespacePrefix != null)
+                {
+                    var currentNamespacePrefixConfiguration = _namespacePrefixConfiguration.CurrentValue;
 
-                // if user has entered a new prefix, update the global list of prefixes, so that they get the choice when editing graph uri id fields in new content types
-                // what do we do with pollution in the list, e.g. someone adds an incorrect prefix?
+                    // if user has entered a new prefix, update the global list of prefixes, so that they get the choice when editing graph uri id fields in new content types
+                    // what do we do with pollution in the list, e.g. someone adds an incorrect prefix?
 
-                if (!currentNamespacePrefixConfiguration.NamespacePrefixOptions.Contains(model.NamespacePrefix))
-                    currentNamespacePrefixConfiguration.NamespacePrefixOptions.Add(model.NamespacePrefix);
+                    if (!currentNamespacePrefixConfiguration.NamespacePrefixOptions.Contains(model.NamespacePrefix))
+                        currentNamespacePrefixConfiguration.NamespacePrefixOptions.Add(model.NamespacePrefix);
+                }
+
+                // we just (manually) populate a datalist with NamespacePrefixOptions in the view, so we don't get the list back in the model
+                // we could update the model, but we want the prefixes to be global, i.e. if a new content item is created, it should contain the full list of prefixes
+                // so we always populate the model from the global list, instead of storing the current set in the model
+                // note: if the list in the appsettings is updated, we'll probably lose the current global list
+                // we could update appsettings with new prefixes, but if we do that, we'd probably want to allow the user to delete them,
+                // so perhaps we just don't update appsettings in the app service when running ;-)
+
+                context.Builder.WithSettings(new GraphSyncPartSettings {NamespacePrefix = model.NamespacePrefix});
             }
-
-            // we just (manually) populate a datalist with NamespacePrefixOptions in the view, so we don't get the list back in the model
-            // we could update the model, but we want the prefixes to be global, i.e. if a new content item is created, it should contain the full list of prefixes
-            // so we always populate the model from the global list, instead of storing the current set in the model
-            // note: if the list in the appsettings is updated, we'll probably lose the current global list
-            // we could update appsettings with new prefixes, but if we do that, we'd probably want to allow the user to delete them,
-            // so perhaps we just don't update appsettings in the app service when running ;-)
-
-            context.Builder.WithSettings(model);
 
             return Edit(contentTypePartDefinition);
         }
