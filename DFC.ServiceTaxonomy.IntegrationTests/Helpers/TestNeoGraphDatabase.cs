d3a778b9cc6778e4f639874b3a376a98b7d66291
Phil Davies <github@phildavies.co.uk>
2019-12-18 15:16:59 +0000

    integration tests: * make neo4j driver's scope span all integration tests * make each test execute in a transaction and roll it back when the test finishes

---
+++ DFC.ServiceTaxonomy.IntegrationTests/Helpers/TestNeoGraphDatabase.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.Neo4j.Configuration;
+using Microsoft.Extensions.Options;
+using Neo4j.Driver;
+
+namespace DFC.ServiceTaxonomy.IntegrationTests.Helpers
+{
+    public interface IGraphDatabaseTestRun : IDisposable /*: IGraphDatabase << currently, but might change it) */
+    {
+        Task<List<T>> RunReadQuery<T>(Query query, Func<IRecord, T> operation);
+        Task RunWriteQueries(params Query[] queries);
+    }
+
+    public class TestNeoGraphDatabase : IDisposable
+    {
+        private readonly IDriver _driver;
+
+        public TestNeoGraphDatabase(IOptionsMonitor<Neo4jConfiguration> neo4jConfigurationOptions)
+        {
+            // Each IDriver instance maintains a pool of connections inside, as a result, it is recommended to only use one driver per application.
+            // It is considerably cheap to create new sessions and transactions, as sessions and transactions do not create new connections as long as there are free connections available in the connection pool.
+            //  driver is thread-safe, while the session or the transaction is not thread-safe.
+            //todo: add configuration/settings menu item/page so user can enter this
+            var neo4jConfiguration = neo4jConfigurationOptions.CurrentValue;
+
+            //todo: pass logger, see https://github.com/neo4j/neo4j-dotnet-driver
+            // o => o.WithLogger(logger)
+            _driver = GraphDatabase.Driver(neo4jConfiguration.Endpoint.Uri, AuthTokens.Basic(neo4jConfiguration.Endpoint.Username, neo4jConfiguration.Endpoint.Password));
+        }
+
+        public async Task<IGraphDatabaseTestRun> StartTestRun()
+        {
+            GraphDatabaseTestRun testRun = new GraphDatabaseTestRun();
+            await testRun.Initialise(_driver);
+            return testRun;
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+
+        private class GraphDatabaseTestRun : IGraphDatabaseTestRun, IDisposable
+        {
+            private IAsyncSession? _session;
+            private IAsyncTransaction? _transaction;
+
+            public async Task Initialise(IDriver driver)
+            {
+                _session = driver.AsyncSession();
+                _transaction = await _session.BeginTransactionAsync();
+            }
+
+            public async Task<List<T>> RunReadQuery<T>(Query query, Func<IRecord, T> operation)
+            {
+                if (_transaction == null)
+                    throw new Exception("Not Initialised");
+
+                IResultCursor result = await _transaction.RunAsync(query);
+                return await result.ToListAsync(operation);
+            }
+
+            public async Task RunWriteQueries(params Query[] queries)
+            {
+                if (_transaction == null)
+                    throw new Exception("Not Initialised");
+
+                foreach (Query query in queries)
+                {
+                    await _transaction.RunAsync(query);
+                }
+            }
+
+            public void Dispose()
+            {
+                _session?.CloseAsync();
+            }
+        }
+    }
+}
--- DFC.ServiceTaxonomy.IntegrationTests/Neo4j/MergeNodesTests.cs
+++ DFC.ServiceTaxonomy.IntegrationTests/Neo4j/MergeNodesTests.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.IntegrationTests.Helpers;
 using DFC.ServiceTaxonomy.Neo4j.Configuration;
 using DFC.ServiceTaxonomy.Neo4j.Generators;
-using DFC.ServiceTaxonomy.Neo4j.Services;
 using FakeItEasy;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration;
@@ -14,11 +14,11 @@ using Xunit;
 
 namespace DFC.ServiceTaxonomy.IntegrationTests.Neo4j
 {
-    public class MergeNodesTests
+    public class GraphDatabaseFixture : IDisposable
     {
-        private readonly NeoGraphDatabase _neoGraphDatabase;
+        public TestNeoGraphDatabase GraphTestDatabase { get; }
 
-        public MergeNodesTests()
+        public GraphDatabaseFixture()
         {
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -31,8 +31,40 @@ namespace DFC.ServiceTaxonomy.IntegrationTests.Neo4j
             var optionsMonitor = A.Fake<IOptionsMonitor<Neo4jConfiguration>>();
             A.CallTo(() => optionsMonitor.CurrentValue).Returns(neo4jConfiguration);
 
-            _neoGraphDatabase = new NeoGraphDatabase(optionsMonitor);
+            GraphTestDatabase = new TestNeoGraphDatabase(optionsMonitor);
         }
+
+        public void Dispose()
+        {
+            GraphTestDatabase?.Dispose();
+        }
+    }
+
+    [CollectionDefinition("Graph Database Integration")]
+    public class GraphDatabaseIntegrationCollection : ICollectionFixture<GraphDatabaseFixture>
+    {
+    }
+
+    //base class?
+    [Collection("Graph Database Integration")]
+    public class MergeNodesTests : IAsyncLifetime, IDisposable
+    {
+        private GraphDatabaseFixture _graphDatabaseFixture;
+        private IGraphDatabaseTestRun? _graphDatabaseTestRun;
+
+        public MergeNodesTests(GraphDatabaseFixture graphDatabaseFixture)
+        {
+            _graphDatabaseFixture = graphDatabaseFixture;
+        }
+
+        public async Task InitializeAsync()
+        {
+            _graphDatabaseTestRun = await _graphDatabaseFixture.GraphTestDatabase.StartTestRun();
+        }
+
+        public Task DisposeAsync() => Task.CompletedTask;
+
+        public void Dispose() => _graphDatabaseTestRun?.Dispose();
 
         [Fact]
         public async Task CreateNode_NoExistingNode_Test()
@@ -47,14 +79,17 @@ namespace DFC.ServiceTaxonomy.IntegrationTests.Neo4j
             Query query = QueryGenerator.MergeNodes(nodeLabel, testProperties, idPropertyName);
 
             //todo: create test NeoGraphDatabase that keeps trans open, then rollbacks after test?
-            await _neoGraphDatabase.RunWriteQueries(query);
+            await _graphDatabaseTestRun!.RunWriteQueries(query);
 
             //todo: we could convert to INode here, but if conversion fails, we won't get an assert failing
-            List<IRecord> actualRecords = await _neoGraphDatabase.RunReadQuery(
+            List<IRecord> actualRecords = await _graphDatabaseTestRun.RunReadQuery(
                 new Query($"match (n:{nodeLabel}) return n"),
                 r => r);
 
             // note: Records on a result cannot be accessed if the session or transaction where the result is created has been closed. (https://github.com/neo4j/neo4j-dotnet-driver)
+            // Any query results obtained within a transaction function should be consumed within that function. Transaction functions can return values but these should be derived values rather than raw results. (https://neo4j.com/docs/driver-manual/1.7/sessions-transactions/#driver-transactions)
+            //todo: ^^ should probably not ignore this!
+            //todo: use reactive session?
 
             // no decent xUnit support for this sort of thing. use fluentassertions, comparenetobjecs, other?
             //todo: when this fails, it returns a disgusting assert failure message
