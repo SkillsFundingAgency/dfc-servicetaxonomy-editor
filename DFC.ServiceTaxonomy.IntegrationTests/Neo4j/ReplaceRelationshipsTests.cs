5bf3d5dcfad814678773a74662a968270b2c7bd9
Phil Davies <github@phildavies.co.uk>
2020-01-06 15:09:57 +0000

    add unit test

--- DFC.ServiceTaxonomy.IntegrationTests/Neo4j/ReplaceRelationshipsTests.cs
+++ DFC.ServiceTaxonomy.IntegrationTests/Neo4j/ReplaceRelationshipsTests.cs
@@ -179,9 +179,69 @@ namespace DFC.ServiceTaxonomy.IntegrationTests.Neo4j
                 relationshipType, destNodeLabel, relationshipVariable));
         }
 
-        //todo:
-        //        public async Task ReplaceRelationships_CreateNoNewRelationship_ExistingRelationship_Test()
+        [Fact]
+        public async Task ReplaceRelationships_CreateNoNewRelationship_ExistingRelationship_Test()
+        {
+            const string sourceNodeLabel = "sourceNodeLabel";
+            const string sourceIdPropertyName = "sourceId";
+            string sourceIdPropertyValue = Guid.NewGuid().ToString();
 
+            const string destNodeLabel = "destNodeLabel";
+            const string destIdPropertyName = "destId";
+            string destIdPropertyValue = Guid.NewGuid().ToString();
+
+            string preexistingDestIdPropertyValue = Guid.NewGuid().ToString();
+
+            const string relationshipType = "relationshipType";
+            const string relationshipVariable = "r";
+
+            //todo: needs to return id
+            // create source node to create relationship from
+            await MergeNode(sourceNodeLabel, sourceIdPropertyName,
+                new Dictionary<string, object> {{sourceIdPropertyName, sourceIdPropertyValue}});
+
+            // create destination node for preexisting relationship
+            await MergeNode(destNodeLabel, destIdPropertyName,
+                new Dictionary<string, object> {{destIdPropertyName, preexistingDestIdPropertyValue}});
+
+            // create destination node to create new relationship to
+            await MergeNode(destNodeLabel, destIdPropertyName,
+                new Dictionary<string, object> {{destIdPropertyName, destIdPropertyValue}});
+
+            // create pre-existing relationships
+            //todo: don't use cut for arrangement
+            var preExistingRelationships = new ReadOnlyDictionary<(string,string,string),IEnumerable<string>>(
+                new Dictionary<(string,string,string),IEnumerable<string>> {{(destNodeLabel, destIdPropertyName, relationshipType), new[] {destIdPropertyValue}}});
+
+            Query preexistingQuery = new ReplaceRelationshipsCommand
+            {
+                SourceNodeLabel = sourceNodeLabel,
+                SourceIdPropertyName = sourceIdPropertyName,
+                SourceIdPropertyValue = sourceIdPropertyValue,
+                Relationships = preExistingRelationships
+            };
+            await _graphDatabase.RunWriteQueries(preexistingQuery);
+
+            //todo: is readonly enough, or should we clone? probably need to clone
+            var relationships = new ReadOnlyDictionary<(string,string,string),IEnumerable<string>>(
+                new Dictionary<(string,string,string),IEnumerable<string>> {{(destNodeLabel, destIdPropertyName, relationshipType), new string[0] }});
+
+            // act
+            Query query = new ReplaceRelationshipsCommand
+            {
+                SourceNodeLabel = sourceNodeLabel,
+                SourceIdPropertyName = sourceIdPropertyName,
+                SourceIdPropertyValue = sourceIdPropertyValue,
+                Relationships = relationships
+            };
+            await _graphDatabase.RunWriteQueries(query);
+
+            AssertResult(relationshipVariable,new ExpectedRelationship[0],
+                await AllRelationships(sourceNodeLabel, sourceIdPropertyName, sourceIdPropertyValue,
+                relationshipType, destNodeLabel, relationshipVariable));
+        }
+
+        //todo:
         //        public async Task ReplaceRelationships_CreateMultipleNewRelationship_NoExistingRelationships_Test()
         //        public async Task ReplaceRelationships_CreateMultipleNewRelationship_ExistingRelationship_Test()
     }
