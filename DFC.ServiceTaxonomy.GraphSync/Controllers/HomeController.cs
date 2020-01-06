6698a7d3b49012c8ba546a53efa97d5b2428abcb
Phil Davies <github@phildavies.co.uk>
2020-01-06 15:26:55 +0000

    remove done todo

--- DFC.ServiceTaxonomy.GraphSync/Controllers/HomeController.cs
+++ DFC.ServiceTaxonomy.GraphSync/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 
 namespace DFC.ServiceTaxonomy.GraphSync.Controllers
 {
-    //todo: should this module also contain the graph sync activity? probably
     public class HomeController : Controller
     {
         public ActionResult Index()
