5a6702c777c3f7ae1fd27b6acb00a33d482ada2b
Phil Davies <github@phildavies.co.uk>
2020-01-23 12:29:43 +0000

    add resthttpclient

---
+++ GetJobProfiles/RestHttpClient/RestHttpClient.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace GetJobProfiles.RestHttpClient
+{
+    public class RestHttpClient : IRestHttpClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public RestHttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<T> Get<T>(Uri uri, object queryData = null, CancellationToken cancellationToken = default)
+        {
+            var response = await GetResponse(uri, queryData, cancellationToken).ConfigureAwait(false);
+            return await response.Content.ReadAsAsync<T>(cancellationToken).ConfigureAwait(false);
+        }
+
+        public Task<T> Get<T>(string uri, object queryData = null, CancellationToken cancellationToken = default)
+        {
+            return Get<T>(new Uri(uri, UriKind.RelativeOrAbsolute), queryData, cancellationToken);
+        }
+
+        public async Task<string> Get(Uri uri, object queryData = null, CancellationToken cancellationToken = default)
+        {
+            var response = await GetResponse(uri, queryData, cancellationToken).ConfigureAwait(false);
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }
+
+        public Task<string> Get(string uri, object queryData = null, CancellationToken cancellationToken = default)
+        {
+            return Get(new Uri(uri, UriKind.RelativeOrAbsolute), queryData, cancellationToken);
+        }
+
+        public async Task<TResponse> PostAsJson<TResponse>(string uri, CancellationToken cancellationToken = default)
+        {
+            var resultAsString = await PostAsJson<object>(uri, null, cancellationToken).ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<TResponse>(resultAsString);
+        }
+
+        public async Task<string> PostAsJson<TRequest>(string uri, TRequest requestData, CancellationToken cancellationToken = default)
+        {
+            var response = await _httpClient.PostAsJsonAsync(uri, requestData, cancellationToken).ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw CreateClientException(response, await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+            }
+
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }
+
+        public async Task<TResponse> PostAsJson<TRequest, TResponse>(string uri, TRequest requestData, CancellationToken cancellationToken = default)
+        {
+            var resultAsString = await PostAsJson(uri, requestData, cancellationToken).ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<TResponse>(resultAsString);
+        }
+
+        public async Task<string> PutAsJson<TRequest>(string uri, TRequest requestData, CancellationToken cancellationToken = default)
+        {
+            var response = await _httpClient.PutAsJsonAsync<TRequest>(uri, requestData, cancellationToken).ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw CreateClientException(response, await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+            }
+
+            return await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        }
+
+        public async Task<TResponse> PutAsJson<TRequest, TResponse>(string uri, TRequest requestData, CancellationToken cancellationToken = default)
+        {
+            var resultAsString = await PutAsJson(uri, requestData, cancellationToken).ConfigureAwait(false);
+
+            return JsonSerializer.Deserialize<TResponse>(resultAsString);
+        }
+
+        protected virtual Exception CreateClientException(HttpResponseMessage httpResponseMessage, string content)
+        {
+            return new RestHttpClientException(httpResponseMessage, content);
+        }
+
+        protected virtual async Task<HttpResponseMessage> GetResponse(Uri uri, object queryData = null, CancellationToken cancellationToken = default)
+        {
+            if (queryData != null)
+            {
+                uri = new Uri(AddQueryString(uri.ToString(), queryData), UriKind.RelativeOrAbsolute);
+            }
+
+            var response = await _httpClient.GetAsync(uri, cancellationToken).ConfigureAwait(false);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw CreateClientException(response, await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+            }
+
+            return response;
+        }
+
+        private string AddQueryString(string uri, object queryData)
+        {
+            var queryDataDictionary = queryData.GetType().GetProperties()
+                .ToDictionary(x => x.Name, x => x.GetValue(queryData)?.ToString() ?? string.Empty);
+            return QueryHelpers.AddQueryString(uri, queryDataDictionary);
+        }
+    }
+}
