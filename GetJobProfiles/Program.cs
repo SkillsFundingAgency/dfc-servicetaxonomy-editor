bdbb1e344708f1203b044b2ae2e5cf2d0768eba1
Phil Davies <github@phildavies.co.uk>
2020-01-23 12:08:55 +0000

    add initial GetJobProfiles console project

---
+++ GetJobProfiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetJobProfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
