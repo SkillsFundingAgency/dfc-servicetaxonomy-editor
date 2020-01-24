a8a77c3610a8b6a1e28d80e87bd9511563385dd7
Phil Davies <github@phildavies.co.uk>
2020-01-24 12:39:04 +0000

    GetJobProfiles wip

--- GetJobProfiles/Models/Recipe/ContentItem.cs
+++ GetJobProfiles/Models/Recipe/ContentItem.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace GetJobProfiles.Models.Recipe
 {
     public class ContentItem
@@ -13,11 +15,61 @@ namespace GetJobProfiles.Models.Recipe
         public string CreatedUtc { get; set; }
         public string Owner { get; set; }
         public string Author { get; set; }
+    }
+
+    public class JobProfileContentItem : ContentItem
+    {
         public TitlePart TitlePart { get; set; }
+        public HtmlField HtbBodies { get; set; }
+        public TextField HtbTitleOptions { get; set; }
+        public HtmlField HtbOtherRequirements { get; set; }
+        public HtmlField HtbCareerTips { get; set; }
+        public HtmlField HtbFurtherInformation { get; set; }
+        public ContentPicker HtbRestrictions { get; set; }
+        public ContentPicker HtbRegistrations { get; set; }
+        public GraphLookupPart GraphLookupPart { get; set; }    // todo: multiple?
+        public GraphSyncPart GraphSyncPart { get; set; }    // todo: multiple?
+        public BagPart BagPart { get; set; }
     }
 
     public class TitlePart
     {
         public string Title { get; set; }
+    }
+
+    public class HtmlField
+    {
+        public string Html { get; set; }
+    }
+
+    public class TextField
+    {
+        public string Text { get; set; }
+    }
+
+    public class ContentPicker
+    {
+        public string ContentItemIds { get; set; }
+    }
+
+    public class GraphLookupPart
+    {
+        public Node[] Nodes { get; set; }
+    }
+
+    public class Node
+    {
+        public string Id { get; set; }
+        public string DisplayText { get; set; }
+    }
+
+    public class GraphSyncPart
+    {
+        public string Text { get; set; }
+    }
+
+    public class BagPart    //todo: multiples??
+    {
+        public ContentItem[] ContentItems { get; set; }
     }
 }
--- GetJobProfiles/Program.cs
+++ GetJobProfiles/Program.cs
@@ -7,10 +7,15 @@ using System.Threading;
 using System.Threading.Tasks;
 using GetJobProfiles.Models.API;
 using GetJobProfiles.Models.Recipe;
+using Microsoft.Extensions.Configuration;
 using MoreLinq.Extensions;
 
 // when we run this for real, we should run it against prod (or preprod), so that we get the current real details,
 // and no test job profiles slip through the net
+
+/* we're gonna have to zip/include graph lookup to occupation (which won't come from the job profile api
+ *
+ */
 
 namespace GetJobProfiles
 {
@@ -23,13 +28,17 @@ namespace GetJobProfiles
             const int take = 1;
             const int napTimeMs = 0;
 
+            var config = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.Development.json", optional: true)
+                .Build();
+
             var httpClient = new HttpClient
             {
                 BaseAddress = new Uri("https://dev.api.nationalcareersservice.org.uk/job-profiles/"),
                 DefaultRequestHeaders =
                 {
                     {"version", "v1"},
-                    {"Ocp-Apim-Subscription-Key", ""}
+                    {"Ocp-Apim-Subscription-Key", config["Ocp-Apim-Subscription-Key"]}
                 }
             };
             var client = new RestHttpClient.RestHttpClient(httpClient);
@@ -99,7 +108,7 @@ namespace GetJobProfiles
 
         public static ContentItem ConvertJobProfile(JobProfile jobProfile, string timestamp)
         {
-            var contentItem = new ContentItem
+            var contentItem = new JobProfileContentItem
             {
                 //DisplayText vs Title
                 ContentItemId = "[js:uuid()]",
