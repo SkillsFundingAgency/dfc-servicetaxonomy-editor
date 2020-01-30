311283932650f3e1e0a2fe722a513a0699fa041c
Phil Davies <github@phildavies.co.uk>
2020-01-30 10:16:31 +0000

    tidy up

--- GetJobProfiles/Models/API/SocCode.cs
+++ GetJobProfiles/Models/API/SocCode.cs
@@ -12,21 +12,5 @@ namespace GetJobProfiles.Models.API
 
         public SocCodeContentItem ToContentItem(string timestamp) =>
             new SocCodeContentItem(Unit, timestamp, Title);
-        // {
-        //     ContentItemId = generator.GenerateUniqueId(),
-        //     ContentItemVersionId = generator.GenerateUniqueId(),
-        //     ContentType = "SOCCode",
-        //     DisplayText = Unit,
-        //     Latest = true,
-        //     Published = true,
-        //     ModifiedUtc = $"{DateTime.UtcNow:O}",        //todo: global
-        //     PublishedUtc = $"{DateTime.UtcNow:O}",
-        //     CreatedUtc = $"{DateTime.UtcNow:O}",
-        //     Owner = "[js: parameters('AdminUsername')]",
-        //     Author = "[js: parameters('AdminUsername')]",
-        //     GraphSyncPart = new GraphSyncPart($"http://nationalcareers.service.gov.uk/soccode/{Guid.NewGuid()}"),
-        //     TitlePart = new TitlePart(Unit),
-        //     Description = new TextField(Title)
-        // };
     }
 }
