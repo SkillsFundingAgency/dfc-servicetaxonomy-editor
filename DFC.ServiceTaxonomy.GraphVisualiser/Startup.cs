cdc99f94732efeddd1df1800bc304b5d104b4548
phil <github@phildavies.co.uk>
2020-02-12 09:31:15 +0000

    plug in graph db

--- DFC.ServiceTaxonomy.GraphVisualiser/Startup.cs
+++ DFC.ServiceTaxonomy.GraphVisualiser/Startup.cs
@@ -1,6 +1,9 @@
 using System;
+using DFC.ServiceTaxonomy.Neo4j.Configuration;
+using DFC.ServiceTaxonomy.Neo4j.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 
@@ -10,6 +13,13 @@ namespace DFC.ServiceTaxonomy.GraphVisualiser
     {
         public override void ConfigureServices(IServiceCollection services)
         {
+            // Configuration
+            var serviceProvider = services.BuildServiceProvider();
+            var configuration = serviceProvider.GetService<IConfiguration>();
+
+            services.Configure<Neo4jConfiguration>(configuration.GetSection("Neo4j"));
+            // Graph Database
+            services.AddSingleton<IGraphDatabase, NeoGraphDatabase>();
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
