4d4103765e573a4920c22f7f4aa7bae832235e53
Phil Davies <github@phildavies.co.uk>
2020-02-19 12:58:03 +0000

    job profile import wip

--- GetJobProfiles/Models/Recipe/ContentItems/UniversityLinkContentItem.cs
+++ GetJobProfiles/Models/Recipe/ContentItems/UniversityLinkContentItem.cs
@@ -1,7 +1,18 @@
-﻿namespace GetJobProfiles.Models.Recipe.ContentItems
+﻿using GetJobProfiles.Models.Recipe.Fields;
+using GetJobProfiles.Models.Recipe.Parts;
+
+namespace GetJobProfiles.Models.Recipe.ContentItems
 {
-    public class UniversityLinkContentItem
+    public class UniversityLinkContentItem : ContentItem
     {
-        
+        public UniversityLinkContentItem(string title, string timestamp, string contentItemId) : base("UniversityLink", title, timestamp, contentItemId)
+        {
+            TitlePart = new TitlePart(title);
+            GraphSyncPart = new GraphSyncPart("UniversityLink");
+        }
+
+        public LinkField Link { get; set; }
+        public TitlePart TitlePart { get; set; }
+        public GraphSyncPart GraphSyncPart { get; set; }
     }
 }
---
+++ GetJobProfiles/Models/Recipe/Fields/LinkField.cs
@@ -0,0 +1,8 @@
+﻿namespace GetJobProfiles.Models.Recipe.Fields
+{
+    public class LinkField
+    {
+        public string Url { get; set; }
+        public string Text { get; set; }
+    }
+}
