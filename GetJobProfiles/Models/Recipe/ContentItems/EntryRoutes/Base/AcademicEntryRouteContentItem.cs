cf9d630732131c354e26143ea6b4e6a79c49c39d
phil <github@phildavies.co.uk>
2020-02-21 11:22:51 +0000

    tidy up

--- GetJobProfiles/Models/Recipe/ContentItems/EntryRoutes/Base/AcademicEntryRouteContentItem.cs
+++ GetJobProfiles/Models/Recipe/ContentItems/EntryRoutes/Base/AcademicEntryRouteContentItem.cs
@@ -18,8 +18,6 @@ namespace GetJobProfiles.Models.Recipe.ContentItems.EntryRoutes.Base
             GraphSyncPart = new GraphSyncPart(contentType);
         }
 
-        //todo: might need to have an EponymousBagPart to pick up the correct content type name (or somesuch)
-        //todo: prob gonna have to add json polymorphic converter too
         public virtual AcademicEntryRoutePart EponymousPart { get; set; }
         public GraphSyncPart GraphSyncPart { get; set; }
     }
@@ -33,79 +31,3 @@ namespace GetJobProfiles.Models.Recipe.ContentItems.EntryRoutes.Base
         public ContentPicker Links { get; set; }
     }
 }
-
-/*
- * {
-  "ContentItems": [
-    {
-      "ContentItemId": "42f54e2p517vx483shgjbreyw3",
-      "ContentItemVersionId": null,
-      "ContentType": "UniversityRoute",
-      "DisplayText": "University Route",
-      "Latest": false,
-      "Published": false,
-      "ModifiedUtc": "2020-02-14T11:36:57.3030372Z",
-      "PublishedUtc": null,
-      "CreatedUtc": null,
-      "Owner": null,
-      "Author": "admin",
-      "TitlePart": {
-        "Title": "University Route"
-      },
-      "UniversityRoute": {
-        "RelevantSubjects": {
-          "Html": "<p>You could study for a degree in a subject like:</p><ul><li>zoology</li><li>animal ecology</li><li>animal behaviour</li><li>conservation</li></ul>"
-        },
-        "FurtherInfo": {
-          "Html": "<p>You'll need a relevant postgraduate qualification like a master's degree or PhD for some jobs, particularly in research.<br></p>"
-        },
-        "RequirementsPrefix": {
-          "ContentItemIds": [
-            "46r3q8j08m47ytd4zpd459bqnp"
-          ]
-        },
-        "Requirements": {
-          "ContentItemIds": [
-            "44psrmmfam5x5x96b71akmqk5w",
-            "4gykd3k0zk9ac27aqdmm24vxck"
-          ]
-        },
-        "Links": {
-          "ContentItemIds": [
-            "41rdx8tthvtsbxma8tm3a6zzm4",
-            "4gw16rcda3qs74p725c3t9s05t",
-            "4sn31w5f1dnmcvwg0zhw6949d3"
-          ]
-        }
-      },
-      "GraphSyncPart": {
-        "Text": "http://nationalcareers.service.gov.uk/universityroute/f380d966-1ba4-4f49-befe-73f903e57a6d"
-      }
-    },
-    {
-      "ContentItemId": "4zsppznfgwzf8533ptb2qcs8mk",
-      "ContentItemVersionId": null,
-      "ContentType": "WorkRoute",
-      "DisplayText": "Work Route",
-      "Latest": false,
-      "Published": false,
-      "ModifiedUtc": "2020-02-14T11:36:57.3400392Z",
-      "PublishedUtc": null,
-      "CreatedUtc": null,
-      "Owner": null,
-      "Author": "admin",
-      "TitlePart": {
-        "Title": "Work Route"
-      },
-      "WorkRoute": {
-        "Description": {
-          "Html": "<p>work route info</p>"
-        }
-      },
-      "GraphSyncPart": {
-        "Text": "http://nationalcareers.service.gov.uk/workroute/bd080dc3-4aab-4ca0-be24-80acc6d8713c"
-      }
-    }
-  ]
-}
-*/
--- GetJobProfiles/Models/Recipe/Parts/BagPart.cs
+++ GetJobProfiles/Models/Recipe/Parts/BagPart.cs
@@ -6,7 +6,6 @@ namespace GetJobProfiles.Models.Recipe.Parts
     public class BagPart
     {
         //can oc support >1 bagparts in a content type?
-        //todo: might need BagPartContentItem?
         public List<ContentItem> ContentItems { get; set; } = new List<ContentItem>();
     }
 }
