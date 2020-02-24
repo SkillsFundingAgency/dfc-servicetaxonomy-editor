6574476647e43ee5cd25dbf555fd0f05c9a41024
phil <github@phildavies.co.uk>
2020-02-24 10:46:18 +0000

    add todo

--- DFC.ServiceTaxonomy.GraphSync/GraphSyncers/BagPartGraphSyncer.cs
+++ DFC.ServiceTaxonomy.GraphSync/GraphSyncers/BagPartGraphSyncer.cs
@@ -36,7 +36,8 @@ namespace DFC.ServiceTaxonomy.GraphSync.GraphSyncers
             {
                 var graphSyncer = _serviceProvider.GetRequiredService<IGraphSyncer>();
 
-                IMergeNodeCommand? mergeNodeCommand = await graphSyncer.SyncToGraph(contentItem!["ContentType"]!.ToString(), contentItem!);
+                IMergeNodeCommand? mergeNodeCommand = await graphSyncer.SyncToGraph(
+                    contentItem!["ContentType"]!.ToString(), contentItem!);
                 // only sync the content type contained in the bag if it has a graph lookup part
                 if (mergeNodeCommand == null)
                     continue;
@@ -54,6 +55,10 @@ namespace DFC.ServiceTaxonomy.GraphSync.GraphSyncers
                 replaceRelationshipsCommand.SourceIdPropertyValue = (string?)nodeProperties[replaceRelationshipsCommand.SourceIdPropertyName];
 
                 //todo: hackalert! pick relationshiptype from hint
+                // no hint for bag (or named bag parts)
+                // also, might want 1 relationships for all content types, i.e. ncs__hasEntryRoute (name in content type also contains how to become)
+                // or 1 relationship per content type, e.g. ncs__hasUniversityRoute
+                // can we add something to the contained content for the relationship?
                 string relationshipType = "ncs__hasBagPart";
 
                 //todo: hackalert! destNodeLabel should be a set of labels
