9d2ed06af939bb3dabf1658ca5c79fc7bceede84
Phil Davies <github@phildavies.co.uk>
2020-03-10 16:42:20 +0000

    occupation import wip

--- GetJobProfiles/EscoJobProfileMapper.cs
+++ GetJobProfiles/EscoJobProfileMapper.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using CsvHelper;
-using GetJobProfiles.Models.API;
-using GetJobProfiles.Models.Recipe.ContentItems;
-using GetJobProfiles.Models.Recipe.Parts;
-using MoreLinq;
-
-namespace GetJobProfiles
-{
-    public class EscoJobProfileMapper
-    {
-        public void Map(IEnumerable<JobProfileContentItem> jobProfiles)
-        {
-            using (var reader = new StreamReader(@"SeedData\esco_job_profile_map.csv"))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-            {
-                //read all the rows in the csv
-                var escoJobProfileMap = csv.GetRecords<EscoJobProfileMap>().ToArray().OrderBy(jp => jp.Url);
-                var distinctEscoJobProfileMap = escoJobProfileMap.DistinctBy(jp => jp.Url).OrderBy(jp => jp.Url);
-
-                if (escoJobProfileMap.Count() != distinctEscoJobProfileMap.Count())
-                {
-                    ColorConsole.WriteLine($"{escoJobProfileMap.Count() - distinctEscoJobProfileMap.Count()} duplicate job profiles in map", ConsoleColor.Black, ConsoleColor.Red);
-                }
-
-                foreach (var item in distinctEscoJobProfileMap)
-                {
-                    JobProfileContentItem profile = jobProfiles
-                        .SingleOrDefault(x => x.EponymousPart.JobProfileWebsiteUrl.Text.Split("/").Last() == item.Url);
-
-                    if (profile != null)
-                    {
-                        //todo: allow >1 graphlookuppart in a contenttype: change graphlookup to named part
-                        profile.GraphLookupPart = new GraphLookupPart
-                        {
-                            Nodes = new[]
-                            {
-                                new Node { DisplayText = item.EscoTitle, Id = item.EscoUri }
-                            }
-                        };
-                    }
-                }
-            }
-        }
-    }
-}
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using System.IO;
+// using System.Linq;
+// using CsvHelper;
+// using GetJobProfiles.Models.API;
+// using GetJobProfiles.Models.Recipe.ContentItems;
+// using GetJobProfiles.Models.Recipe.Parts;
+// using MoreLinq;
+//
+// namespace GetJobProfiles
+// {
+//     public class EscoJobProfileMapper
+//     {
+//         public void Map(IEnumerable<JobProfileContentItem> jobProfiles)
+//         {
+//             using (var reader = new StreamReader(@"SeedData\esco_job_profile_map.csv"))
+//             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+//             {
+//                 //read all the rows in the csv
+//                 var escoJobProfileMap = csv.GetRecords<EscoJobProfileMap>().ToArray().OrderBy(jp => jp.Url);
+//                 var distinctEscoJobProfileMap = escoJobProfileMap.DistinctBy(jp => jp.Url).OrderBy(jp => jp.Url);
+//
+//                 if (escoJobProfileMap.Count() != distinctEscoJobProfileMap.Count())
+//                 {
+//                     ColorConsole.WriteLine($"{escoJobProfileMap.Count() - distinctEscoJobProfileMap.Count()} duplicate job profiles in map", ConsoleColor.Black, ConsoleColor.Red);
+//                 }
+//
+//                 foreach (var item in distinctEscoJobProfileMap)
+//                 {
+//                     JobProfileContentItem profile = jobProfiles
+//                         .SingleOrDefault(x => x.EponymousPart.JobProfileWebsiteUrl.Text.Split("/").Last() == item.Url);
+//
+//                     if (profile != null)
+//                     {
+//                         //todo: allow >1 graphlookuppart in a contenttype: change graphlookup to named part
+//                         profile.GraphLookupPart = new GraphLookupPart
+//                         {
+//                             Nodes = new[]
+//                             {
+//                                 new Node { DisplayText = item.EscoTitle, Id = item.EscoUri }
+//                             }
+//                         };
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
---
+++ GetJobProfiles/Models/Recipe/ContentItems/OccupationContentItem.cs
@@ -0,0 +1,24 @@
+﻿using GetJobProfiles.Models.Recipe.ContentItems.Base;
+using GetJobProfiles.Models.Recipe.Parts;
+
+namespace GetJobProfiles.Models.Recipe.ContentItems
+{
+    public class OccupationContentItem : ContentItem
+    {
+        public OccupationContentItem(string title, string timestamp, string contentItemId = null)
+            : base("Occupation", title, timestamp, contentItemId)
+        {
+            TitlePart = new TitlePart(title);
+            GraphSyncPart = new GraphSyncPart("Occupation");
+
+            DisplayText = TitlePart.Title;
+        }
+
+        public TitlePart TitlePart { get; set; }
+        //public OccupationPart EponymousPart { get; set; }
+        public GraphLookupPart GraphLookupPart { get; set; }
+        //todo: need to set graph sync's Text to have same guid as esco occupation
+        // ^^ add to cypher recipe after creating nodes for labels & creating relationships
+        public GraphSyncPart GraphSyncPart { get; set; }
+    }
+}
