1caabbf3a3a71e1a679e232090c55382316d61e0
Phil Davies <github@phildavies.co.uk>
2020-03-16 11:54:03 +0000

    remove customquery

--- DFC.ServiceTaxonomy.Neo4j/Queries/Interfaces/ICustomQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿//namespace DFC.ServiceTaxonomy.Neo4j.Queries.Interfaces
-//{
-//    interface ICustomQuery<out TRecord> : IQuery<TRecord>
-//    {
-//        public string? QueryStatement { get; set; }
-//    }
-//}
