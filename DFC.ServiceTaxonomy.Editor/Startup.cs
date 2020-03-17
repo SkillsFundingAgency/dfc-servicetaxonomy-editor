a47506bd746f38e7718b0ed30ae783bc2f260664
Stu Ratcliffe <email@sturatcliffe.co.uk>
2020-03-17 13:48:58 +0000

    Log to appinsights

--- DFC.ServiceTaxonomy.Editor/Startup.cs
+++ DFC.ServiceTaxonomy.Editor/Startup.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -6,8 +7,24 @@ namespace DFC.ServiceTaxonomy.Editor
 {
     public class Startup
     {
+        public Startup(IHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry(options =>
+                options.InstrumentationKey = Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+
             services.AddOrchardCms();
         }
 
