233be4653568f9b61e81fee8dd4487c946401340
Phil Davies <github@phildavies.co.uk>
2020-03-25 12:39:35 +0000

    reinstate audit sync task

--- DFC.ServiceTaxonomy.GraphSync/Activities/AuditSyncIssuesTask.cs
+++ DFC.ServiceTaxonomy.GraphSync/Activities/AuditSyncIssuesTask.cs
@@ -1,20 +1,20 @@
 using System;
 using System.Collections.Generic;
-// using System.Linq;
+using System.Linq;
 using System.Threading.Tasks;
 using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Interfaces;
-// using DFC.ServiceTaxonomy.GraphSync.Models;
+using DFC.ServiceTaxonomy.GraphSync.Models;
 using DFC.ServiceTaxonomy.GraphSync.Services.Interface;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Metadata;
-// using OrchardCore.ContentManagement.Records;
+using OrchardCore.ContentManagement.Records;
 using OrchardCore.Workflows.Abstractions.Models;
 using OrchardCore.Workflows.Activities;
 using OrchardCore.Workflows.Models;
 using YesSql;
-// using YesSql.Services;
+using YesSql.Services;
 
 namespace DFC.ServiceTaxonomy.GraphSync.Activities
 {
@@ -57,73 +57,70 @@ namespace DFC.ServiceTaxonomy.GraphSync.Activities
 #pragma warning restore S3220 // Method calls should not resolve ambiguously to overloads with "params"
         }
 
-        public override Task<ActivityExecutionResult> ExecuteAsync(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        public override async Task<ActivityExecutionResult> ExecuteAsync(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
         {
             _logger.LogInformation($"{nameof(AuditSyncIssuesTask)} triggered");
 
-            //todo: reinstate!
-            // //todo: don't calc these in ctor, check at sync val time, also ctor get called many times, not always when executing
-            // var contentTypes = _contentDefinitionManager
-            //     .ListTypeDefinitions()
-            //     .Where(x => x.Parts.Any(p => p.Name == nameof(GraphSyncPart)))
-            //     .ToDictionary(x => x.Name);
-            //
-            // var timestamp = DateTime.UtcNow;
-            // var log = (await _session.Query<AuditSyncLog>().FirstOrDefaultAsync()) ??
-            //           new AuditSyncLog {LastSynced = DateTime.MinValue};
-            //
-            // //todo: do we want to load 10s thousands at once??
-            // var contentItems = await _session
-            //     //do we only care about the latest published items?
-            //     .Query<ContentItem, ContentItemIndex>(x =>
-            //         x.ContentType.IsIn(contentTypes.Keys) && x.Latest && x.Published && (x.CreatedUtc >= log.LastSynced ||  x.ModifiedUtc >= log.LastSynced))
-            //     .ListAsync();
-            //
-            // foreach (var contentItem in contentItems)
-            // {
-            //     //todo: do we need a new _graphSyncValidator each time?
-            //     if (!await _graphSyncValidator.CheckIfContentItemSynced(contentItem))
-            //     {
-            //         _syncFailedContentItems.Add(contentItem);
-            //     }
-            // }
-            //
-            // for (int i = 0; i < _syncFailedContentItems.Count; i++)
-            // {
-            //     try
-            //     {
-            //         var contentItem = _syncFailedContentItems[i];
-            //
-            //         await _mergeGraphSyncer.SyncToGraph(
-            //             contentItem.ContentType,
-            //             contentItem.ContentItemId,
-            //             contentItem.ContentItemVersionId,
-            //             contentItem.Content,
-            //             contentItem.CreatedUtc,
-            //             contentItem.ModifiedUtc);
-            //
-            //         _syncFailedContentItems.RemoveAt(i);
-            //
-            //         if (!await _graphSyncValidator.CheckIfContentItemSynced(contentItem))
-            //         {
-            //             _syncFailedContentItems.Add(contentItem);
-            //         }
-            //     }
-            //     catch
-            //     {
-            //         //I don't want to do anything here, why won't this build without this comment?
-            //     }
-            // }
-            //
-            // //anything left in the syncFailedContentItems collection is still failing to sync - report this somehow
-            //
-            // log.LastSynced = timestamp;
-            // _session.Save(log);
-            // await _session.CommitAsync();
-            //
-            //return Outcomes("Done");
+             //todo: don't calc these in ctor, check at sync val time, also ctor get called many times, not always when executing
+             var contentTypes = _contentDefinitionManager
+                 .ListTypeDefinitions()
+                 .Where(x => x.Parts.Any(p => p.Name == nameof(GraphSyncPart)))
+                 .ToDictionary(x => x.Name);
 
-            return Task.FromResult(Outcomes("Done"));
+             var timestamp = DateTime.UtcNow;
+             var log = (await _session.Query<AuditSyncLog>().FirstOrDefaultAsync()) ??
+                       new AuditSyncLog {LastSynced = DateTime.MinValue};
+
+             //todo: do we want to load 10s thousands at once??
+             var contentItems = await _session
+                 //do we only care about the latest published items?
+                 .Query<ContentItem, ContentItemIndex>(x =>
+                     x.ContentType.IsIn(contentTypes.Keys) && x.Latest && x.Published && (x.CreatedUtc >= log.LastSynced ||  x.ModifiedUtc >= log.LastSynced))
+                 .ListAsync();
+
+             foreach (var contentItem in contentItems)
+             {
+                 //todo: do we need a new _graphSyncValidator each time?
+                 if (!await _graphSyncValidator.CheckIfContentItemSynced(contentItem))
+                 {
+                     _syncFailedContentItems.Add(contentItem);
+                 }
+             }
+
+             for (int i = 0; i < _syncFailedContentItems.Count; i++)
+             {
+                 try
+                 {
+                     var contentItem = _syncFailedContentItems[i];
+
+                     await _mergeGraphSyncer.SyncToGraph(
+                         contentItem.ContentType,
+                         contentItem.ContentItemId,
+                         contentItem.ContentItemVersionId,
+                         contentItem.Content,
+                         contentItem.CreatedUtc,
+                         contentItem.ModifiedUtc);
+
+                     _syncFailedContentItems.RemoveAt(i);
+
+                     if (!await _graphSyncValidator.CheckIfContentItemSynced(contentItem))
+                     {
+                         _syncFailedContentItems.Add(contentItem);
+                     }
+                 }
+                 catch
+                 {
+                     //I don't want to do anything here, why won't this build without this comment?
+                 }
+             }
+
+             //anything left in the syncFailedContentItems collection is still failing to sync - report this somehow
+
+             log.LastSynced = timestamp;
+             _session.Save(log);
+             await _session.CommitAsync();
+
+            return Outcomes("Done");
         }
     }
 
