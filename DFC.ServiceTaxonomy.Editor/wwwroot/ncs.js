3037c2fb7c910034322df412fef37b23ac361115
Stu Ratcliffe <email@sturatcliffe.co.uk>
2020-03-31 15:01:51 +0100

    Include a custom script on every page to reorder the admin menu items until an OC issue is resolved.

---
+++ DFC.ServiceTaxonomy.Editor/wwwroot/ncs.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    var ul = $("ul#adminMenu li#new ul");
+    var li = ul.children("li");
+
+    li.detach().sort(function (a, b) {
+        return (a.innerText > b.innerText) - (a.innerText < b.innerText);
+    });
+
+    ul.append(li);
+});
