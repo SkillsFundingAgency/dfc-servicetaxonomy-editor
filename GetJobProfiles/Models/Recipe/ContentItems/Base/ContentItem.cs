076abcb30b0ba05e0e896ffaa8de8c4d36ff7e3c
Phil Davies <github@phildavies.co.uk>
2020-04-02 11:52:47 +0100

    truncate displaytext if necessary

--- GetJobProfiles/Models/Recipe/ContentItems/Base/ContentItem.cs
+++ GetJobProfiles/Models/Recipe/ContentItems/Base/ContentItem.cs
@@ -5,6 +5,8 @@ namespace GetJobProfiles.Models.Recipe.ContentItems.Base
     public class ContentItem
     {
         private static readonly DefaultIdGenerator _generator = new DefaultIdGenerator();
+        public const int MaxDisplayTextLength = 250;
+        private string _displayText;
 
         public ContentItem(string contentType, string title, string timestamp, string contentItemId = null)
         {
@@ -28,7 +30,13 @@ namespace GetJobProfiles.Models.Recipe.ContentItems.Base
         public string ContentItemId { get; set; }
         public string ContentItemVersionId { get; set; }
         public string ContentType { get; set; }
-        public string DisplayText { get; set; }
+
+        public string DisplayText
+        {
+            get => _displayText;
+            set => _displayText = Truncate(value);
+        }
+
         public bool Latest { get; set; }
         public bool Published { get; set; }
         public string ModifiedUtc { get; set; }
@@ -36,5 +44,11 @@ namespace GetJobProfiles.Models.Recipe.ContentItems.Base
         public string CreatedUtc { get; set; }
         public string Owner { get; set; }
         public string Author { get; set; }
+
+        private string Truncate(string displayText)
+        {
+            // display text is stored in the oc database in a column that has a fixed size
+            return displayText.Substring(0, ContentItem.MaxDisplayTextLength);
+        }
     }
 }
