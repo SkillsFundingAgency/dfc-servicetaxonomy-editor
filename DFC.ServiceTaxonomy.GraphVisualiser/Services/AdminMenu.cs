37ef696dc6daeace7ca341a97cd4d778bdb538bf
Ian Crisp <icrisp@program-systems.co.uk>
2020-04-03 13:16:45 +0100

    Added menu to sidebar for Ontology visualiser

---
+++ DFC.ServiceTaxonomy.GraphVisualiser/Services/AdminMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.GraphVisualiser.Controllers;
+using Microsoft.Extensions.Localization;
+using OrchardCore.Navigation;
+
+namespace DFC.ServiceTaxonomy.GraphVisualiser.Services
+{
+    public class AdminMenu : INavigationProvider
+    {
+        private readonly IStringLocalizer S;
+
+        public AdminMenu(IStringLocalizer<AdminMenu> localizer)
+        {
+            S = localizer;
+        }
+
+        public Task BuildNavigationAsync(string name, NavigationBuilder builder)
+        {
+            if (!String.Equals(name, "admin", StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.CompletedTask;
+            }
+
+            builder
+                .Add(S["Visualiser"], "after", rootView => rootView
+                   .Add(S["Ontology"], "1", ontology => ontology
+                       .Action(nameof(VisualiseController.Viewer), "Visualise", new { area = typeof(Startup)!.Namespace, target = "_blank" })));
+
+            return Task.CompletedTask;
+        }
+    }
+}
