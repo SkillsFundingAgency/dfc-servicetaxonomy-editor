d0a74c025d1762c584fb2f88d8e7e214f87fbde3
Phil Davies <github@phildavies.co.uk>
2020-04-07 14:56:03 +0100

    content picker with preview wip

--- DFC.ServiceTaxonomy.ContentPickerPreview/Startup.cs
+++ DFC.ServiceTaxonomy.ContentPickerPreview/Startup.cs
@@ -3,42 +3,13 @@ using DFC.ServiceTaxonomy.ContentPickerPreview.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using OrchardCore.Admin;
 using OrchardCore.Modules;
 using OrchardCore.ContentManagement;
 
 namespace DFC.ServiceTaxonomy.ContentPickerPreview
 {
-    //options for preview picker:
-    // derive from current field?
-    // use real field and tweak in ui using js?
-
-    //custom part
-    //reuse contentpicker field
-    //new render field
-    //bit of js?
-
-    //can we use a widget within the admin section?
-
-    //todo: if get copy of vue-multiselect-wrapper working for poc, use original script and listen for vue-multiselect-created event and @@input to vue-multiselect?
-
     public class Startup : StartupBase
     {
-        private readonly AdminOptions _adminOptions;
-
-        static Startup()
-        {
-            // these are needed to be able to access the view models  in display shapes rendered by the liquid markup engine
-            // TemplateContext.GlobalMemberAccessStrategy.Register<TitlePartViewModel>();
-            // TemplateContext.GlobalMemberAccessStrategy.Register<TitlePartSettingsViewModel>();
-        }
-
-        public Startup(IOptions<AdminOptions> adminOptions)
-        {
-            _adminOptions = adminOptions.Value;
-        }
-
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IContentPickerResultProvider, PreviewContentPickerResultProvider>();
