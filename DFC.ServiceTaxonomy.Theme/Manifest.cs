a0c2e41381fbece6fb7498e4fa4a4cc59d35876c
Phil Davies <github@phildavies.co.uk>
2020-04-07 12:11:38 +0100

    add theme for overriding preview

---
+++ DFC.ServiceTaxonomy.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "StaxTheme",
+    Author = "National Careers Service",
+    Website = "https://github.com/SkillsFundingAgency/dfc-servicetaxonomy-editor",
+    Version = "0.0.1",
+    Description = "DFC.ServiceTaxonomy.Theme"
+)]
