dd1b0b427e12da3838490c4fe5b32acc6c29387a
Phil Davies <github@phildavies.co.uk>
2020-04-21 08:24:52 +0100

    remove using

--- DFC.ServiceTaxonomy.GraphSync/Queries/NodeWithOutgoingRelationshipsQuery.cs
+++ DFC.ServiceTaxonomy.GraphSync/Queries/NodeWithOutgoingRelationshipsQuery.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using DFC.ServiceTaxonomy.GraphSync.Queries.Models;
 using DFC.ServiceTaxonomy.Neo4j.Exceptions;
