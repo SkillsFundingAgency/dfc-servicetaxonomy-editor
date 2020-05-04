4ea574cfff7dab758782accf1c1a0ac682eb03c4
Phil Davies <github@phildavies.co.uk>
2020-05-04 11:33:34 +0100

    publish to event grid task wip

---
+++ DFC.ServiceTaxonomy.Editor/Activities/Tasks/PublishToEventGridTask.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Localization;
+using OrchardCore.Workflows.Abstractions.Models;
+using OrchardCore.Workflows.Activities;
+using OrchardCore.Workflows.Models;
+
+namespace DFC.ServiceTaxonomy.Editor.Activities.Tasks
+{
+    public class PublishToEventGridTask : TaskActivity
+    {
+        public PublishToEventGridTask(IStringLocalizer<PublishToEventGridTask> localizer)
+        {
+            T = localizer;
+        }
+
+        public override IEnumerable<Outcome> GetPossibleOutcomes(WorkflowExecutionContext workflowContext, ActivityContext activityContext)
+        {
+            return Outcomes(T["Done"], T["Failed"]);
+        }
+
+        private IStringLocalizer T { get; }
+
+        public override string Name => nameof(PublishToEventGridTask);
+        public override LocalizedString DisplayText => T["Publish content state changes to Azure Event Grid"];
+        public override LocalizedString Category => T["Event Grid"];
+    }
+}
