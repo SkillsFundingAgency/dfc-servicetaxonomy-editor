1b9765712132b8c53263c96087ce7d41152e78ee
Phil Davies <github@phildavies.co.uk>
2020-05-06 14:08:26 +0100

    add new events module

---
+++ DFC.ServiceTaxonomy.Events/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "DFC.ServiceTaxonomy.Events",
+    Author = "The Orchard Team",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "DFC.ServiceTaxonomy.Events",
+    Category = "Content Management"
+)]
---
+++ DFC.ServiceTaxonomy.Events/Startup.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace DFC.ServiceTaxonomy.Events
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        {
+            routes.MapAreaControllerRoute(
+                name: "Home",
+                areaName: "DFC.ServiceTaxonomy.Events",
+                pattern: "Home/Index",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
