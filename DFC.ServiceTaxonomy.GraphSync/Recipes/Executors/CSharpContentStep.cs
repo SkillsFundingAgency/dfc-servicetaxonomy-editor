adf55ca4bfba4f8602a5e49fdfbb228b5e28b00c
Phil Davies <github@phildavies.co.uk>
2020-05-15 08:01:10 +0100

    debug import option for csharpcontentstep

--- DFC.ServiceTaxonomy.GraphSync/Recipes/Executors/CSharpContentStep.cs
+++ DFC.ServiceTaxonomy.GraphSync/Recipes/Executors/CSharpContentStep.cs
@@ -51,64 +51,54 @@ namespace DFC.ServiceTaxonomy.GraphSync.Recipes.Executors
             if (model?.Data == null)
                 return;
 
-            try
+            string json = ReplaceCSharpHelpers(model.Data.ToString(), model.DebugImport);
+            JArray data = JArray.Parse(json);
+
+            foreach (JToken token in data)
             {
-                string json = ReplaceCSharpHelpers(model.Data.ToString());
-                JArray data = JArray.Parse(json);
+                ContentItem? contentItem = token.ToObject<ContentItem>();
+                if (contentItem == null)
+                    continue;
 
-                foreach (JToken token in data)
+                DateTime? modifiedUtc = contentItem.ModifiedUtc;
+                DateTime? publishedUtc = contentItem.PublishedUtc;
+                ContentItem existing = await _contentManager.GetVersionAsync(contentItem.ContentItemVersionId);
+
+                if (existing == null)
                 {
-                    ContentItem? contentItem = token.ToObject<ContentItem>();
-                    if (contentItem == null)
-                        continue;
+                    // Initializes the Id as it could be interpreted as an updated object when added back to YesSql
+                    contentItem.Id = 0;
+                    await _contentManager.CreateAsync(contentItem);
+                    _contentManagerSession.Clear();
 
-                    DateTime? modifiedUtc = contentItem.ModifiedUtc;
-                    DateTime? publishedUtc = contentItem.PublishedUtc;
-                    ContentItem existing = await _contentManager.GetVersionAsync(contentItem.ContentItemVersionId);
-
-                    if (existing == null)
-                    {
-                        // Initializes the Id as it could be interpreted as an updated object when added back to YesSql
-                        contentItem.Id = 0;
-                        await _contentManager.CreateAsync(contentItem);
-                        _contentManagerSession.Clear();
-
-                        // Overwrite ModifiedUtc & PublishedUtc values that handlers have changes
-                        // Should not be necessary if IContentManager had an Import method
-                        contentItem.ModifiedUtc = modifiedUtc;
-                        contentItem.PublishedUtc = publishedUtc;
-                    }
-                    else
-                    {
-                        // Replaces the id to force the current item to be updated
-                        existing.Id = contentItem.Id;
-                        _session.Save(existing);
-                    }
+                    // Overwrite ModifiedUtc & PublishedUtc values that handlers have changes
+                    // Should not be necessary if IContentManager had an Import method
+                    contentItem.ModifiedUtc = modifiedUtc;
+                    contentItem.PublishedUtc = publishedUtc;
                 }
+                else
+                {
+                    // Replaces the id to force the current item to be updated
+                    existing.Id = contentItem.Id;
+                    _session.Save(existing);
+                }
+            }
 
 #pragma warning disable S1215
-                GC.Collect();
+            GC.Collect();
 #pragma warning restore S1215
-            }
-            catch (Exception e)
-            {
-                if (!model.FailSilently)
-                    throw;
-
-                _logger.LogError($"{context.Name} recipe failed to import: {e}");
-            }
         }
 
         //todo: move these instead of c&p if works
         //todo: some issue with []
         private static readonly Regex _cSharpHelperRegex = new Regex(@"«c#:([^»]+)»", RegexOptions.Compiled);
-        private string ReplaceCSharpHelpers(string recipeFragment)
+        private string ReplaceCSharpHelpers(string recipeFragment, bool debugImport)
         {
-            return _cSharpHelperRegex.Replace(recipeFragment, match => EvaluateCSharp(match.Groups[1].Value).GetAwaiter().GetResult());
+            return _cSharpHelperRegex.Replace(recipeFragment, match => EvaluateCSharp(match.Groups[1].Value, debugImport).GetAwaiter().GetResult());
         }
 
         private static readonly Regex _getContentItemIdByDisplayTextRegex = new Regex(@"^\s*await\s*Content.GetContentItemIdByDisplayText\s*\(\s*""([^""]+)""\s*,\s*""([^""]+)""\s*\)\s*$", RegexOptions.Compiled);
-        private async Task<string> EvaluateCSharp(string code)
+        private async Task<string> EvaluateCSharp(string code, bool debugImport)
         {
             // can't see how to get json.net to unescape value strings!
             code = code.Replace("\\\"", "\"");
@@ -117,7 +107,13 @@ namespace DFC.ServiceTaxonomy.GraphSync.Recipes.Executors
             Match match = _getContentItemIdByDisplayTextRegex.Match(code);
             if (match.Success)
             {
-                return await _cypherToContentCSharpScriptGlobals.Content.GetContentItemIdByDisplayText(match.Groups[1].Value, match.Groups[2].Value);
+                string contentType = match.Groups[1].Value;
+                string displayText = match.Groups[2].Value;
+
+                if (debugImport)
+                    return await GetContentItemIdByDisplayTextDebug(contentType, displayText);
+
+                return await GetContentItemIdByDisplayText(contentType, displayText);
             }
 
             var script = CSharpScript.Create<string>(code, globalsType: typeof(ICypherToContentCSharpScriptGlobals));
@@ -125,9 +121,28 @@ namespace DFC.ServiceTaxonomy.GraphSync.Recipes.Executors
             return await runner(_cypherToContentCSharpScriptGlobals);
         }
 
+        public async Task<string> GetContentItemIdByDisplayText(string contentType, string displayText)
+        {
+            return await _cypherToContentCSharpScriptGlobals.Content.GetContentItemIdByDisplayText(contentType, displayText);
+        }
+
+        public async Task<string> GetContentItemIdByDisplayTextDebug(string contentType, string displayText)
+        {
+            try
+            {
+                return await _cypherToContentCSharpScriptGlobals.Content.GetContentItemIdByDisplayText(contentType, displayText);
+            }
+            catch
+            {
+                _logger.LogError($"{contentType} with DisplayText '{displayText}' not found!");
+            }
+
+            return "";
+        }
+
         public class CSharpContentStepModel
         {
-            public bool FailSilently { get; set; }
+            public bool DebugImport { get; set; }
             public JArray? Data { get; set; }
         }
     }
