4d5b88e5569e0ab71d1dbcac90c206627b88a824
Phil Davies <github@phildavies.co.uk>
2020-05-18 10:34:23 +0100

    add itemId to event data

--- DFC.ServiceTaxonomy.Events/Models/ContentEventData.cs
+++ DFC.ServiceTaxonomy.Events/Models/ContentEventData.cs
@@ -3,13 +3,15 @@
     public class ContentEventData
     {
         public string Api { get; }
+        public string ItemId { get; }
         public string VersionId { get; }
         public string DisplayText { get; }
         public string Author { get; }
 
-        public ContentEventData(string api, string versionId, string displayText, string author)
+        public ContentEventData(string api, string itemId, string versionId, string displayText, string author)
         {
             Api = api;
+            ItemId = itemId;
             VersionId = versionId;
             DisplayText = displayText;
             Author = author;
