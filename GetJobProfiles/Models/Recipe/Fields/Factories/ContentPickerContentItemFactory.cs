07a98af6b9c4aac7f22a1b0ba795394178804a80
Phil Davies <github@phildavies.co.uk>
2020-05-20 13:04:25 +0100

    entry routes wip

--- GetJobProfiles/Models/Recipe/Fields/Factories/ContentPickerContentItemFactory.cs
+++ GetJobProfiles/Models/Recipe/Fields/Factories/ContentPickerContentItemFactory.cs
@@ -3,57 +3,9 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using GetJobProfiles.Models.Recipe.ContentItems.Base;
-using GetJobProfiles.Models.Recipe.ContentItems.EntryRoutes.Base;
 
 namespace GetJobProfiles.Models.Recipe.Fields.Factories
 {
-    public class ImportedTitleHtmlDescriptionEqualityComparer : EqualityComparer<TitleHtmlDescriptionContentItem>
-    {
-        public override bool Equals(TitleHtmlDescriptionContentItem i1, TitleHtmlDescriptionContentItem i2)
-        {
-            if (i1 == null && i2 == null)
-                return true;
-
-            if (i1 == null || i2 == null)
-                return false;
-
-            return i1.EponymousPart.Description.Html == i2.EponymousPart.Description.Html;
-        }
-
-        public override int GetHashCode(TitleHtmlDescriptionContentItem item)
-        {
-            return item.EponymousPart.Description.Html.GetHashCode();
-        }
-    }
-
-    public class ImportedAcademicRouteEqualityComparer : EqualityComparer<AcademicEntryRouteContentItem>
-    {
-        public override bool Equals(AcademicEntryRouteContentItem r1, AcademicEntryRouteContentItem r2)
-        {
-            if (r1 == null && r2 == null)
-                return true;
-
-            if (r1 == null || r2 == null)
-                return false;
-
-            return r1.EponymousPart.FurtherInfo.Html == r2.EponymousPart.FurtherInfo.Html &&
-                   r1.EponymousPart.RelevantSubjects.Html == r2.EponymousPart.RelevantSubjects.Html &&
-                   r1.EponymousPart.Links.ContentItemIds.SequenceEqual(r2.EponymousPart.Links.ContentItemIds) &&
-                   r1.EponymousPart.Requirements.ContentItemIds.SequenceEqual(r2.EponymousPart.Requirements.ContentItemIds) &&
-                   r1.EponymousPart.RequirementsPrefix.ContentItemIds.SequenceEqual(r2.EponymousPart.RequirementsPrefix.ContentItemIds);
-        }
-
-        public override int GetHashCode(AcademicEntryRouteContentItem item)
-        {
-            return item.EponymousPart.FurtherInfo.Html.GetHashCode()
-                ^ item.EponymousPart.RelevantSubjects.Html.GetHashCode()
-                ^ item.EponymousPart.Links.ContentItemIds.GetHashCode()
-                ^ item.EponymousPart.Requirements.ContentItemIds.GetHashCode()
-                ^ item.EponymousPart.RequirementsPrefix.ContentItemIds.GetHashCode();
-
-        }
-    }
-
     public class ContentPickerContentItemFactory<T> where T : ContentItem
     {
         private readonly EqualityComparer<T> _comparer;
