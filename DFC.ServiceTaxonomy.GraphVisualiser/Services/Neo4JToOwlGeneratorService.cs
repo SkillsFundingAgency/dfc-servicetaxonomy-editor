fc23d416cb8470a505231b13537c718f2305e894
Tom Tollafield <thomas.tollafield@gmail.com>
2020-05-20 11:44:02 +0100

    - Amended label creation

--- DFC.ServiceTaxonomy.GraphVisualiser/Services/Neo4JToOwlGeneratorService.cs
+++ DFC.ServiceTaxonomy.GraphVisualiser/Services/Neo4JToOwlGeneratorService.cs
@@ -44,7 +44,7 @@ namespace DFC.ServiceTaxonomy.GraphVisualiser.Services
                               {
                                   Id = $"Class{a.Key - minNodeId}",
                                   Key = a.Key,
-                                  Type = a.Value.Labels.First(l => /*l.StartsWith("ncs__") ||*/ l == "esco__Occupation" || l == "esco__Skill"),
+                                  Type = a.Value.Labels.First(l => l != "Resource" || l == "esco__Occupation" || l == "esco__Skill"),
                                   Label = GetPropertyValue(a, new[] { prefLabel, "Description", "FurtherInfo" }),
                                   Comment = GetPropertyValue(a, new[] { "Description" }),
                                   StaxProperties = a.Value.Properties.Where(p => p.Key != prefLabel).Select(p => $"{p.Key}:{p.Value}").ToList(),
