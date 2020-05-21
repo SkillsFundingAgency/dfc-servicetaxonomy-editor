0fcaf7323d1e3ee13eb17f403b5f246a277474a2
Phil Davies <github@phildavies.co.uk>
2020-05-21 08:36:14 +0100

    title options importer

--- GetJobProfiles/Importers/TitleOptionsImporter.cs
+++ GetJobProfiles/Importers/TitleOptionsImporter.cs
@@ -1,7 +1,30 @@
-﻿namespace GetJobProfiles.Importers
+﻿using System.Collections.Generic;
+using System.Linq;
+using NPOI.XSSF.UserModel;
+
+namespace GetJobProfiles.Importers
 {
     public class TitleOptionsImporter
     {
-        
+        public Dictionary<string, string> Import(XSSFWorkbook workbook)
+        {
+            var sheet = workbook.GetSheet("JobProfile");
+            int dynamicTitleColumnIndex = sheet.GetRow(0).Cells.Single(x => x.StringCellValue == "DynamicTitlePrefix")
+                .ColumnIndex;
+            int titleColumnIndex = sheet.GetRow(0).Cells.Single(x => x.StringCellValue == "Title").ColumnIndex;
+
+            var titleOptionsLookup = new Dictionary<string, string>();
+
+            for (int i = 1; i <= sheet.LastRowNum; ++i)
+            {
+                var row = sheet.GetRow(i);
+                string categories = row.GetCell(dynamicTitleColumnIndex).StringCellValue;
+                string uri = row.GetCell(titleColumnIndex).StringCellValue.TrimStart('/');
+
+                titleOptionsLookup.Add(uri, categories);
+            }
+
+            return titleOptionsLookup;
+        }
     }
 }
