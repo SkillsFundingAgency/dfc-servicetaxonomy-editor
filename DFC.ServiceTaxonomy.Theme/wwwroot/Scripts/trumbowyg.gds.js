eec5ed65c1bac86399be3fa1da748a34315d10cc
Phil Davies <github@phildavies.co.uk>
2020-05-27 14:39:06 +0100

    trumbowyg gds plugin wip

--- DFC.ServiceTaxonomy.Theme/wwwroot/Scripts/trumbowyg.gds.js
+++ DFC.ServiceTaxonomy.Theme/wwwroot/Scripts/trumbowyg.gds.js
@@ -3,42 +3,55 @@
 
     // Plugin default options
     var defaultOptions = {
+        paragraphs: [
+            'body',
+            'lead',
+            'small'
+        ]
     };
 
     // If the plugin is a button
-    function buildButtonDef (trumbowyg) {
-        return {
-            fn: function () {
-                // Plugin button logic
-            }
-        }
-    }
+    // function buildButtonDef (trumbowyg) {
+    //     return {
+    //         fn: function () {
+    //             // Plugin button logic
+    //         }
+    //     }
+    // }
 
     $.extend(true, $.trumbowyg, {
         // Add some translations
         langs: {
             en: {
-                myplugin: 'My plugin'
+                gds: 'GDS',
+                paragraphs: {
+                    'body': 'Body',
+                    'lead': 'Lead',
+                    'small': "Small"
+                }
             }
         },
         // Register plugin in Trumbowyg
         plugins: {
-            myplugin: {
+            gds: {
                 // Code called by Trumbowyg core to register the plugin
                 init: function (trumbowyg) {
                     // Fill current Trumbowyg instance with the plugin default options
-                    trumbowyg.o.plugins.myplugin = $.extend(true, {},
+                    trumbowyg.o.plugins.gds = $.extend(true, {},
                         defaultOptions,
-                        trumbowyg.o.plugins.myplugin || {}
+                        trumbowyg.o.plugins.gds || {}
                     );
 
                     // If the plugin is a paste handler, register it
-                    trumbowyg.pasteHandlers.push(function(pasteEvent) {
-                        // My plugin paste logic
-                    });
+                    // trumbowyg.pasteHandlers.push(function(pasteEvent) {
+                    //     // My plugin paste logic
+                    // });
 
                     // If the plugin is a button
-                    trumbowyg.addBtnDef('myplugin', buildButtonDef(trumbowyg));
+                    //trumbowyg.addBtnDef('myplugin', buildButtonDef(trumbowyg));
+                    trumbowyg.addBtnDef('paragraphs', {
+                        dropdown: buildDropdown(trumbowyg)
+                    });
                 },
                 // Return a list of button names which are active on current element
                 tagHandler: function (element, trumbowyg) {
@@ -48,5 +61,44 @@
                 }
             }
         }
-    })
+    });
+
+    function setParagraph(trumbowyg, paragraph) {
+        trumbowyg.$ed.focus();
+        trumbowyg.saveRange();
+
+        // Temporary size
+        trumbowyg.execCmd('fontSize', '1');
+
+        // Find <font> elements that were added and change to <span> with chosen size
+        trumbowyg.$ed.find('font[size="1"]').replaceWith(function() {
+            return $('<span/>', {
+                css: { 'font-size': size },
+                html: this.innerHTML,
+            });
+        });
+
+        // Remove and leftover <span> elements
+        $(trumbowyg.range.startContainer.parentElement).find('span[style=""]').contents().unwrap();
+
+        trumbowyg.restoreRange();
+    }
+
+    function buildDropdown(trumbowyg) {
+        var dropdown = [];
+
+        $.each(trumbowyg.o.plugins.gds.paragraphs, function (index, paragraph) {
+            trumbowyg.addBtnDef('paragraph_' + paragraph, {
+                text: '<span>' + (trumbowyg.lang.paragraphs[paragraph] || paragraph) + '</span>',
+                hasIcon: false,
+                fn: function () {
+                    setParagraph(trumbowyg, paragraph);
+                }
+            });
+            dropdown.push('paragraph_' + paragraph);
+        });
+
+        return dropdown;
+    }
+
 })(jQuery);
