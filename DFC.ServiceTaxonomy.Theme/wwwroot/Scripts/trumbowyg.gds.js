a15941d627b69b5597da4ed725ece5fc758e7b28
Phil Davies <github@phildavies.co.uk>
2020-05-28 12:42:50 +0100

    trumbowyg gds plugin wip

--- DFC.ServiceTaxonomy.Theme/wwwroot/Scripts/trumbowyg.gds.js
+++ DFC.ServiceTaxonomy.Theme/wwwroot/Scripts/trumbowyg.gds.js
@@ -15,37 +15,36 @@
             {name: 'h2', class: 'govuk-heading-l'},
             {name: 'h3', class: "govuk-heading-m"},
             {name: 'h4', class: "govuk-heading-s"}
+        ],
+        fontWeights: [
+            {name: 'bold', class: 'govuk-!-font-weight-bold'},
+            {name: 'regular', class: 'govuk-!-font-weight-regular'}
         ]
     };
-
-    // If the plugin is a button
-    // function buildButtonDef (trumbowyg) {
-    //     return {
-    //         fn: function () {
-    //             // Plugin button logic
-    //         }
-    //     }
-    // }
 
     $.extend(true, $.trumbowyg, {
         // Add some translations
         langs: {
             en: {
                 gds: 'GDS',
+                paragraph: 'Paragraph',
                 paragraphs: {
                     'body': 'Body',
                     'lead': 'Lead',
                     'small': "Small"
                 },
-                paragraph: 'Paragraph',
+                heading: 'Heading',
                 headings: {
                     'h1': 'h1',
                     'h2': 'h2',
                     'h3': "h3",
                     'h4': "h4",
                 },
-                heading: 'heading'
-
+                fontWeight: 'Font Weight',
+                fontWeights: {
+                    'bold': 'Bold',
+                    'regular': 'Regular'
+                },
             }
         },
         // Register plugin in Trumbowyg
@@ -59,22 +58,24 @@
                         trumbowyg.o.plugins.gds || {}
                     );
 
-                    // If the plugin is a button
-                    //trumbowyg.addBtnDef('myplugin', buildButtonDef(trumbowyg));
-
-                    trumbowyg.addBtnDef('paragraphs', {
+                    trumbowyg.addBtnDef('paragraph', {
                         dropdown: buildParagraphsDropdown(trumbowyg),
                         ico: 'p',
                         title: trumbowyg.lang.paragraph
                     });
 
-                    trumbowyg.addBtnDef('headings', {
+                    trumbowyg.addBtnDef('heading', {
                         dropdown: buildHeadingsDropdown(trumbowyg),
                         //todo: new icon, just H instead of H1
                         ico: 'h1',
                         title: trumbowyg.lang.heading
                     });
 
+                    trumbowyg.addBtnDef('fontWeight', {
+                        dropdown: buildFontWeightsDropdown(trumbowyg),
+                        ico: 'strong',
+                        title: trumbowyg.lang.fontWeight
+                    });
                 },
                 // Return a list of button names which are active on current element
                 tagHandler: function (element, trumbowyg) {
@@ -88,7 +89,7 @@
 
     function setParagraph(trumbowyg, paragraph) {
 
-        trumbowyg.execCmd('formatBlock', 'p')
+        trumbowyg.execCmd('formatBlock', 'p');
 
         var selection = trumbowyg.doc.getSelection();
         $(selection.focusNode.parentNode).addClass(paragraph.class);
@@ -109,10 +110,22 @@
 
     function setHeading(trumbowyg, heading) {
 
-        trumbowyg.execCmd('formatBlock', heading.name)
+        trumbowyg.execCmd('formatBlock', heading.name);
 
         var selection = trumbowyg.doc.getSelection();
         $(selection.focusNode.parentNode).addClass(heading.name);
+    }
+
+    // some browsers set <b>, some, <strong>, but semantic rewriting should convert to strong anyway
+    //todo: govuk-!-font-weight-bold can be set on other elements such as <p> and <span>, so we could be more clever in what we do
+    function setFontWeight(trumbowyg, fontWeight) {
+
+        trumbowyg.execCmd('formatBlock', fontWeight.name);
+
+        //todo: if heading.name == 'regular' replace <strong> with <span> containing class
+
+        var selection = trumbowyg.doc.getSelection();
+        $(selection.focusNode.parentNode).addClass(fontWeight.name);
     }
 
     //todo: use this instead?
@@ -140,6 +153,7 @@
         $.each(trumbowyg.o.plugins.gds.paragraphs, function (index, paragraph) {
             trumbowyg.addBtnDef('paragraph_' + paragraph.name, {
                 text: '<span>' + (trumbowyg.lang.paragraphs[paragraph.name] || paragraph.name) + '</span>',
+                //todo: either different icon for each, or no icons
                 ico: 'p',
                 tag: 'p',
                 fn: function () {
@@ -167,6 +181,25 @@
                 tag: heading.name,
                 fn: function () {
                     setHeading(trumbowyg, heading);
+                }
+            });
+            dropdown.push(buttonName);
+        });
+        return dropdown;
+    }
+
+    function buildFontWeightsDropdown(trumbowyg) {
+        var dropdown = [];
+
+        $.each(trumbowyg.o.plugins.gds.fontWeights, function (index, fontWeight) {
+            var buttonName = 'fontWeight_' + fontWeight.name;
+            trumbowyg.addBtnDef(buttonName, {
+                text: '<span>' + (trumbowyg.lang.fontWeights[fontWeight.name] || fontWeight.name) + '</span>',
+                //todo: add icon to fontWeight, find one suitable for regular
+                ico: 'strong',
+                tag: 'strong',
+                fn: function () {
+                    setFontWeight(trumbowyg, fontWeight);
                 }
             });
             dropdown.push(buttonName);
