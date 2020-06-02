c2a2ce0a56bf2666d245481800024e08aed21561
Stu Ratcliffe <stu@ratcliffe.io>
2020-06-02 11:07:05 +0100

    Automate sass compilation on publish. Added watch task to automatically regenerate on file changes during development.

---
+++ DFC.ServiceTaxonomy.Theme/gulpfile.js
@@ -0,0 +1,27 @@
+﻿/// <binding ProjectOpened='watch' />
+var gulp = require('gulp');
+
+gulp.task('css', function () {
+    var sass = require('gulp-dart-sass');
+    var sourcemaps = require('gulp-sourcemaps');
+
+    return gulp.src('./wwwroot/Styles/trumbowyg_scoped_govuk_frontend.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('wwwroot/Styles/'));
+});
+
+gulp.task('watch', gulp.series('css', function () {
+    return gulp.watch('./wwwroot/Styles/trumbowyg_scoped_govuk_frontend.scss', gulp.series('css'));
+}));
+
+gulp.task('default', gulp.series('css', function () {
+    var csso = require('gulp-csso');
+    var rename = require('gulp-rename');
+
+    return gulp.src('./wwwroot/Styles/trumbowyg_scoped_govuk_frontend.css')
+        .pipe(csso())
+        .pipe(rename('trumbowyg_scoped_govuk_frontend.min.css'))
+        .pipe(gulp.dest('./wwwroot/Styles/'));
+}));
