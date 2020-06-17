445599d7b00e52997445c1afe6bc2b1c1311741d
Stu Ratcliffe <stu@ratcliffe.io>
2020-06-17 15:05:56 +0100

    AutoroutePart graph syncer and unit tests

---
+++ DFC.ServiceTaxonomy.UnitTests/GraphSync/GraphSyncers/Parts/AutoroutePartGraphSyncerTests/AutoroutePartGraphSyncer_AddSyncComponentsTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Interfaces;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Parts;
+using DFC.ServiceTaxonomy.Neo4j.Commands.Interfaces;
+using FakeItEasy;
+using Newtonsoft.Json.Linq;
+using OrchardCore.ContentManagement.Metadata.Models;
+using Xunit;
+
+namespace DFC.ServiceTaxonomy.UnitTests.GraphSync.GraphSyncers.Parts.AutoroutePartGraphSyncerTests
+{
+    public class AutoroutePartGraphSyncer_AddSyncComponentsTests
+    {
+        public dynamic? Content { get; set; }
+        public IMergeNodeCommand MergeNodeCommand { get; set; }
+        public IReplaceRelationshipsCommand ReplaceRelationshipsCommand { get; set; }
+        public ContentTypePartDefinition ContentTypePartDefinition { get; set; }
+        public IGraphSyncHelper GraphSyncHelper { get; set; }
+        public AutoroutePartGraphSyncer AutoroutePartGraphSyncer { get; set; }
+
+        public AutoroutePartGraphSyncer_AddSyncComponentsTests()
+        {
+            MergeNodeCommand = A.Fake<IMergeNodeCommand>();
+            //todo: best way to do this?
+            MergeNodeCommand.Properties = new Dictionary<string, object>();
+
+            ReplaceRelationshipsCommand = A.Fake<IReplaceRelationshipsCommand>();
+            ContentTypePartDefinition = A.Fake<ContentTypePartDefinition>();
+            GraphSyncHelper = A.Fake<IGraphSyncHelper>();
+
+            AutoroutePartGraphSyncer = new AutoroutePartGraphSyncer();
+        }
+
+        [Fact]
+        public async Task AddSyncComponents_TitleInContent_TitleAddedToMergeNodeCommandsProperties()
+        {
+            const string path = "path";
+
+            Content = JObject.Parse($"{{\"Path\": \"{path}\"}}");
+
+            await CallAddSyncComponents();
+
+            IDictionary<string,object> expectedProperties = new Dictionary<string, object>
+                {{"autoroute_path", path}};
+
+            Assert.Equal(expectedProperties, MergeNodeCommand.Properties);
+        }
+
+        [Fact]
+        public async Task AddSyncComponents_NullTitleInContent_TitleNotAddedToMergeNodeCommandsProperties()
+        {
+            Content = JObject.Parse("{\"Path\": null}");
+
+            await CallAddSyncComponents();
+
+            IDictionary<string, object> expectedProperties = new Dictionary<string, object>();
+            Assert.Equal(expectedProperties, MergeNodeCommand.Properties);
+        }
+
+        private async Task CallAddSyncComponents()
+        {
+            await AutoroutePartGraphSyncer.AddSyncComponents(
+                Content,
+                MergeNodeCommand,
+                ReplaceRelationshipsCommand,
+                ContentTypePartDefinition,
+                GraphSyncHelper);
+        }
+    }
+}
