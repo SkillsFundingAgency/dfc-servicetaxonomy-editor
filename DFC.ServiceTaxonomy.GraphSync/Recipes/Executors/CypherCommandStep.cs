8962968c4339117b0ce7dddd071477bec50e7865
Phil Davies <github@phildavies.co.uk>
2020-06-30 15:30:15 +0100

    graph cluster wip (doesn't build) : CypherCommand recipe step wip

--- DFC.ServiceTaxonomy.GraphSync/Recipes/Executors/CypherCommandStep.cs
+++ DFC.ServiceTaxonomy.GraphSync/Recipes/Executors/CypherCommandStep.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Helpers;
 using DFC.ServiceTaxonomy.Neo4j.Commands.Interfaces;
-using DFC.ServiceTaxonomy.Neo4j.Services;
+using DFC.ServiceTaxonomy.Neo4j.Services.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -18,18 +19,18 @@ namespace DFC.ServiceTaxonomy.GraphSync.Recipes.Executors
     {
         public const string StepName = "CypherCommand";
 
-        private readonly IGraphDatabase _graphDatabase;
+        private readonly IGraphCluster _graphCluster;
         private readonly IServiceProvider _serviceProvider;
         private readonly ILogger<CypherCommandStep> _logger;
         private readonly string _contentApiBaseUrl;
 
         public CypherCommandStep(
-            IGraphDatabase graphDatabase,
+            IGraphCluster graphCluster,
             IServiceProvider serviceProvider,
             IConfiguration configuration,
             ILogger<CypherCommandStep> logger)
         {
-            _graphDatabase = graphDatabase;
+            _graphCluster = graphCluster;
             _serviceProvider = serviceProvider;
             _contentApiBaseUrl = configuration.GetValue<string>("ContentApiPrefix") ?? throw new ArgumentNullException($"ContentApiPrefix not present");
             _logger = logger;
@@ -49,25 +50,35 @@ namespace DFC.ServiceTaxonomy.GraphSync.Recipes.Executors
                     if (command == null)
                         continue;
 
-                    _logger.LogInformation($"Retrieved cypher command from recipe's {StepName} step. Executing {command}");
-
-                    var customCommand = _serviceProvider.GetRequiredService<ICustomCommand>();
-
-                    customCommand.Command = command.Replace("<<ContentApiPrefix>>", _contentApiBaseUrl);
-
-                    await _graphDatabase.Run(customCommand);
+                    //todo: default to run on all GraphReplicaSets -add support for such a thing to the cluster
+                    foreach (string? graphReplicaSetName in step!.GraphReplicaSets ?? new[] {GraphReplicaSetNames.Published, GraphReplicaSetNames.Draft})
+                    {
+                        await ExecuteCommand(graphReplicaSetName, command);
+                    }
                 }
             }
-            catch (Exception ex)
+            catch (Exception exception)
             {
-                _logger.LogWarning($"Exception: {ex}");
+                _logger.LogWarning(exception, $"{StepName} recipe step failed.");
                 throw;
             }
+        }
+
+        private async Task ExecuteCommand(string graphReplicaSetName, string command)
+        {
+            _logger.LogInformation($"Retrieved cypher command from recipe's {StepName} step. Executing {command} on the {graphReplicaSetName} graph replica set.");
+
+            var customCommand = _serviceProvider.GetRequiredService<ICustomCommand>();
+
+            customCommand.Command = command.Replace("<<ContentApiPrefix>>", _contentApiBaseUrl);
+
+            await _graphCluster.Run(graphReplicaSetName, customCommand);
         }
 
 #pragma warning disable S3459
         private class CypherCommandStepModel
         {
+            public string[]? GraphReplicaSets { get; set; }
             public string[]? Commands { get; set; }
         }
 #pragma warning restore S3459
