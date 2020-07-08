875d183d06e50fa11d9c0ee33518ba0f7259fb32
Phil Davies <github@phildavies.co.uk>
2020-07-08 12:02:48 +0100

    draft/published: unit tests wip

--- DFC.ServiceTaxonomy.UnitTests/GraphSync/GraphSyncers/Parts/AutoroutePartGraphSyncerTests/AutoroutePartGraphSyncer_ValidateSyncComponentTests.cs
+++ DFC.ServiceTaxonomy.UnitTests/GraphSync/GraphSyncers/Parts/AutoroutePartGraphSyncerTests/AutoroutePartGraphSyncer_ValidateSyncComponentTests.cs
@@ -1,82 +1,39 @@
-﻿//todo: reinstate
+﻿using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Parts;
+using DFC.ServiceTaxonomy.UnitTests.UnitTestHelpers.PartGraphSyncer;
+using FakeItEasy;
+using Newtonsoft.Json.Linq;
+using Xunit;
 
-// using System.Collections.Generic;
-// using System.Threading.Tasks;
-// using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Interfaces;
-// using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Parts;
-// using DFC.ServiceTaxonomy.GraphSync.Queries.Models;
-// using FakeItEasy;
-// using Neo4j.Driver;
-// using Newtonsoft.Json.Linq;
-// using OrchardCore.ContentManagement;
-// using OrchardCore.ContentManagement.Metadata.Models;
-// using Xunit;
-//
-// namespace DFC.ServiceTaxonomy.UnitTests.GraphSync.GraphSyncers.Parts.AutoroutePartGraphSyncerTests
-// {
-//     public class AutoroutePartGraphSyncer_ValidateSyncComponentTests
-//     {
-//         public JObject Content { get; set; }
-//         public ContentTypePartDefinition ContentTypePartDefinition { get; set; }
-//         public IContentManager ContentManager { get; set; }
-//         public INodeWithOutgoingRelationships NodeWithOutgoingRelationships { get; set; }
-//         public INode SourceNode { get; set; }
-//         public IGraphSyncHelper GraphSyncHelper { get; set; }
-//         public IGraphValidationHelper GraphValidationHelper { get; set; }
-//         public IDictionary<string, int> ExpectedRelationshipCounts { get; set; }
-//         public AutoroutePartGraphSyncer AutoroutePartGraphSyncer { get; set; }
-//
-//         const string _contentKey = "Path";
-//         const string _nodeTitlePropertyName = "autoroute_path";
-//
-//         public AutoroutePartGraphSyncer_ValidateSyncComponentTests()
-//         {
-//             Content = JObject.Parse("{}");
-//             ContentTypePartDefinition = A.Fake<ContentTypePartDefinition>();
-//             ContentManager = A.Fake<IContentManager>();
-//
-//             SourceNode = A.Fake<INode>();
-//             NodeWithOutgoingRelationships = A.Fake<INodeWithOutgoingRelationships>();
-//             A.CallTo(() => NodeWithOutgoingRelationships.SourceNode).Returns(SourceNode);
-//
-//             GraphSyncHelper = A.Fake<IGraphSyncHelper>();
-//
-//             GraphValidationHelper = A.Fake<IGraphValidationHelper>();
-//
-//             ExpectedRelationshipCounts = new Dictionary<string, int>();
-//
-//             AutoroutePartGraphSyncer = new AutoroutePartGraphSyncer();
-//         }
-//
-//         [Theory]
-//         [InlineData(true, true)]
-//         [InlineData(false, false)]
-//         public async Task ValidateSyncComponentTests(bool expected, bool stringContentPropertyMatchesNodePropertyReturns)
-//         {
-//             A.CallTo(() => GraphValidationHelper.StringContentPropertyMatchesNodeProperty(
-//                 _contentKey,
-//                 A<JObject>._,
-//                 _nodeTitlePropertyName,
-//                 SourceNode)).Returns((stringContentPropertyMatchesNodePropertyReturns, ""));
-//
-//             (bool validated, _) = await CallValidateSyncComponent();
-//
-//             Assert.Equal(expected, validated);
-//         }
-//
-//         //todo: test that verifies that failure reason is returned
-//         //todo: test to check nothing added to ExpectedRelationshipCounts
-//
-//         private async Task<(bool validated, string failureReason)> CallValidateSyncComponent()
-//         {
-//             return await AutoroutePartGraphSyncer.ValidateSyncComponent(
-//                 Content,
-//                 ContentTypePartDefinition,
-//                 ContentManager,
-//                 NodeWithOutgoingRelationships,
-//                 GraphSyncHelper,
-//                 GraphValidationHelper,
-//                 ExpectedRelationshipCounts, TODO);
-//         }
-//     }
-// }
+namespace DFC.ServiceTaxonomy.UnitTests.GraphSync.GraphSyncers.Parts.AutoroutePartGraphSyncerTests
+{
+    public class AutoroutePartGraphSyncer_ValidateSyncComponentTests : PartGraphSyncer_ValidateSyncComponentTests
+    {
+        const string ContentKey = "Path";
+        const string NodeTitlePropertyName = "autoroute_path";
+
+        public AutoroutePartGraphSyncer_ValidateSyncComponentTests()
+        {
+            ContentPartGraphSyncer = new AutoroutePartGraphSyncer();
+        }
+
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        public async Task ValidateSyncComponentTests(bool expected, bool stringContentPropertyMatchesNodePropertyReturns)
+        {
+            A.CallTo(() => GraphValidationHelper.StringContentPropertyMatchesNodeProperty(
+                ContentKey,
+                A<JObject>._,
+                NodeTitlePropertyName,
+                SourceNode)).Returns((stringContentPropertyMatchesNodePropertyReturns, ""));
+
+            (bool validated, _) = await CallValidateSyncComponent();
+
+            Assert.Equal(expected, validated);
+        }
+
+        //todo: test that verifies that failure reason is returned
+        //todo: test to check nothing added to ExpectedRelationshipCounts
+    }
+}
