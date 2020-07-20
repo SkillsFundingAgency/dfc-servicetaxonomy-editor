6dcf8c6b8758132f29a0c3e0a58647e2a3714aae
Stu Ratcliffe <stu@ratcliffe.io>
2020-07-20 15:23:25 +0100

    Published / preview graph visualisation

---
+++ DFC.ServiceTaxonomy.GraphVisualiser/Models/ContentVisualiseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using OrchardCore.ContentManagement;
+using OrchardCore.DisplayManagement.Views;
+
+namespace DFC.ServiceTaxonomy.GraphVisualiser.Models
+{
+    public class ContentVisualiseViewModel : ShapeViewModel
+    {
+        public ContentVisualiseViewModel(ContentItem contentItem, string? buttonSize = null)
+        {
+            ContentItem = contentItem;
+            ButtonSize = buttonSize;
+        }
+
+        public ContentItem ContentItem { get; set; }
+        public string? ButtonSize { get; set; }
+    }
+}
