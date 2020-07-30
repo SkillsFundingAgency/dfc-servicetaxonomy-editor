a2142836c03c41cc90259eb5226570046b43c7a7
Phil Davies <github@phildavies.co.uk>
2020-07-30 10:41:57 +0100

    term part syncer wip

---
+++ DFC.ServiceTaxonomy.GraphSync/GraphSyncers/Parts/TermPartGraphSyncer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Exceptions;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
+using OrchardCore.ContentManagement;
+using OrchardCore.Taxonomies.Models;
+
+namespace DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Parts
+{
+    public class TermPartGraphSyncer : IContentPartGraphSyncer
+    {
+        public string PartName => nameof(TermPart);
+
+        private const string TaxonomyContentItemId = "TaxonomyContentItemId";
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public TermPartGraphSyncer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task AddSyncComponents(JObject content, IGraphMergeContext context)
+        {
+            string? taxonomyContentItemId = (string?)content[TaxonomyContentItemId];
+            if (taxonomyContentItemId == null)
+                throw new GraphSyncException($"{PartName} is missing {TaxonomyContentItemId}.");
+
+            ContentItem contentItem = await context.ContentItemVersion.GetContentItem(context.ContentManager, taxonomyContentItemId);
+
+            IGraphSyncHelper termGraphSyncHelper = _serviceProvider.GetRequiredService<IGraphSyncHelper>();
+            termGraphSyncHelper.ContentType = contentItem.ContentType;
+
+            //todo: override/extension that takes a contentitem
+            context.ReplaceRelationshipsCommand.AddRelationshipsTo(
+                $"hasParent{contentItem.ContentType}",
+                null,
+                await termGraphSyncHelper.NodeLabels(),
+                termGraphSyncHelper.ContentIdPropertyName,
+                termGraphSyncHelper.GetIdPropertyValue(contentItem.Content.GraphSyncPart, context.ContentItemVersion));
+        }
+
+        public Task<(bool validated, string failureReason)> ValidateSyncComponent(
+            JObject content,
+            IValidateAndRepairContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
