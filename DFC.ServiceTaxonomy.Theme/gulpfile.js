59fbec0725e15580e20f12525a0b67fdacfa8095
Stu Ratcliffe <stu@ratcliffe.io>
2020-08-10 15:08:09 +0100

    Automate ncs stylesheet generation

--- DFC.ServiceTaxonomy.Theme/gulpfile.js
+++ DFC.ServiceTaxonomy.Theme/gulpfile.js
@@ -1,18 +1,33 @@
 ﻿/// <binding ProjectOpened='watch' />
-var gulp = require('gulp');
+var gulp = require('gulp'); 
 
-gulp.task('default', function () {
+gulp.task('clean', function () {
+    var del = require('del');
+    
+    return del('./wwwroot/Fonts/**', { force: true });
+});
+
+gulp.task('fonts', gulp.series('clean', function () {
+    return gulp.src('./node_modules/nationalcareers_toolkit/assets/dist/fonts/*')
+        .pipe(gulp.dest('./wwwroot/Fonts/'));
+}));
+
+gulp.task('default', gulp.series('fonts', function () {
     var sass = require('gulp-dart-sass');
     var sourcemaps = require('gulp-sourcemaps');
     var csso = require('gulp-csso');
     var rename = require('gulp-rename');
+    var replace = require('gulp-replace');
 
     return gulp.src('./wwwroot/Styles/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
+        .pipe(replace(/nationalcareers_toolkit\/fonts\//g, 'DFC.ServiceTaxonomy.Theme/Fonts/'))
         .pipe(gulp.dest('wwwroot/Styles/'))
-        .pipe(csso())
+        .pipe(csso({
+            restructure: false
+        }))
         .pipe(rename(function (path) {
             return {
                 dirname: path.dirname,
@@ -21,7 +36,7 @@ gulp.task('default', function () {
             }
         }))
         .pipe(gulp.dest('./wwwroot/Styles/'));
-});
+}));
 
 gulp.task('watch', gulp.series('default', function () {
     return gulp.watch('./wwwroot/Styles/*.scss', gulp.series('default'));
