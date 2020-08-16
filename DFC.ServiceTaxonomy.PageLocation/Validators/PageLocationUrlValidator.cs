a6b188c30ac37c550aebb9a0e7107eb6b2b5f5da
Stu Ratcliffe <stu@ratcliffe.io>
2020-08-16 11:49:36 +0100

    Validate against both draft and pub

--- DFC.ServiceTaxonomy.PageLocation/Validators/PageLocationUrlValidator.cs
+++ DFC.ServiceTaxonomy.PageLocation/Validators/PageLocationUrlValidator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DFC.ServiceTaxonomy.Taxonomies.Helper;
 using DFC.ServiceTaxonomy.Taxonomies.Validation;
@@ -12,10 +11,12 @@ namespace DFC.ServiceTaxonomy.PageLocation.Validators
     public class PageLocationUrlValidator : ITaxonomyTermValidator
     {
         private readonly ISession _session;
+        private readonly IContentManager _contentManager;
 
-        public PageLocationUrlValidator(ISession session)
+        public PageLocationUrlValidator(ISession session, IContentManager contentManager)
         {
             _session = session;
+            _contentManager = contentManager;
         }
 
         public string ErrorMessage => "The generated URL for this Page Location has already been used as a Page URL.";
@@ -30,8 +31,17 @@ namespace DFC.ServiceTaxonomy.PageLocation.Validators
             string url = TaxonomyHelpers.BuildTermUrl(term, taxonomy);
             //TODO: check whether or not we only care about published pages, but I think we care about both
             IEnumerable<ContentItem> pages = await _session.Query<ContentItem, ContentItemIndex>(x => x.ContentType == "Page" && x.Latest).ListAsync();
-            //TODO: use nameof, but doing so would introduce a circular dependency between the page location and taxonomies projects
-            return pages.All(x => ((string)x.Content.PageLocationPart.FullUrl).Trim('/') != url);
+            
+            foreach (var page in pages)
+            {
+                //TODO: use nameof, but doing so would introduce a circular dependency between the page location and taxonomies projects
+                string draftUrl = ((string?)(await _contentManager.GetAsync(page.ContentItemId, VersionOptions.Draft))?.Content.PageLocationPart.FullUrl)?.Trim('/') ?? string.Empty;
+                string pubUrl = ((string?)(await _contentManager.GetAsync(page.ContentItemId, VersionOptions.Published))?.Content.PageLocationPart.FullUrl)?.Trim('/') ?? string.Empty;
+
+                if (draftUrl == url || pubUrl == url) return false;
+            }
+
+            return true;
         }
     }
 }
