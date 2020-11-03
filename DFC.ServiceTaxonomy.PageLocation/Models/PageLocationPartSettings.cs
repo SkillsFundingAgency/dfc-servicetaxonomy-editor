3dee0f0b6f2f9e485f08cb43df5f2a799a901653
Stu Ratcliffe <stu@ratcliffe.io>
2020-11-03 16:59:18 +0000

    Added page location part to job profile and job category recipe. Added missing page locations to taxonomy recipe. Refactored page location part to allow default page location path to be configured in part settings.

--- DFC.ServiceTaxonomy.PageLocation/Models/PageLocationPartSettings.cs
+++ DFC.ServiceTaxonomy.PageLocation/Models/PageLocationPartSettings.cs
@@ -3,5 +3,6 @@
     public class PageLocationPartSettings
     {
         public bool DisplayRedirectLocationsAndDefaultPageForLocation { get; set; }
+        public string? DefaultPageLocationPath { get; set; }
     }
 }
