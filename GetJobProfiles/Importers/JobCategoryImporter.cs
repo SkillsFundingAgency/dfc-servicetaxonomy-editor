ae8b1565d36b803553bf5ea0262d6b91a3d7e3dd
Tom Tollafield <tom.tollafield@contractsonline.org>
2020-11-05 08:05:23 +0000

    - Trim Job Categories - Update ONet Skill dependency

--- GetJobProfiles/Importers/JobCategoryImporter.cs
+++ GetJobProfiles/Importers/JobCategoryImporter.cs
@@ -11,6 +11,7 @@ namespace GetJobProfiles.Importers
     public class JobCategoryImporter
     {
         private static readonly DefaultIdGenerator _generator = new DefaultIdGenerator();
+
         public IEnumerable<JobCategoryContentItem> JobCategoryContentItems { get; private set; }
 
         public Dictionary<string, string> JobCategoryContentItemIdDictionary { get; private set; }
@@ -33,26 +34,26 @@ namespace GetJobProfiles.Importers
                 jobCategoryDictionary.Add(uri, categories);
             }
 
-            JobCategoryContentItemIdDictionary = jobCategoryDictionary.SelectMany(z=>z.Value).Distinct().Select(jc => new { Id = _generator.GenerateUniqueId(), Title = jc}).ToDictionary(y => y.Title, y => y.Id);
+            JobCategoryContentItemIdDictionary = jobCategoryDictionary.SelectMany(z => z.Value).Distinct().Select(jc => new { Id = _generator.GenerateUniqueId(), Title = jc.Trim() }).ToDictionary(y => y.Title, y => y.Id);
 
-            JobCategoryContentItems = jobCategoryDictionary.SelectMany(dict => dict.Value).Distinct().ToList().Select(
-                category => new JobCategoryContentItem(category, timestamp, JobCategoryContentItemIdDictionary[category])
-                {
-                    EponymousPart = new JobCategoryPart
-                    {
-                        Description = new HtmlField(category),
-                        JobProfiles = new ContentPicker
-                        {
-                            ContentItemIds = jobProfiles
+            JobCategoryContentItems = jobCategoryDictionary.SelectMany(dict => dict.Value).Distinct().ToList().Select(z => z.Trim()).Select(category
+                 => new JobCategoryContentItem(category, timestamp, JobCategoryContentItemIdDictionary[category])
+                 {
+                     EponymousPart = new JobCategoryPart
+                     {
+                         Description = new HtmlField(category),
+                         JobProfiles = new ContentPicker
+                         {
+                             ContentItemIds = jobProfiles
                                 .Where(jp =>
                                     jobCategoryDictionary.Where(dict => dict.Value.Any(val => val == category))
                                         .Select(dict => dict.Key).Any(uri =>
                                             jp.JobProfileHeader.JobProfileWebsiteUrl.Text.EndsWith(uri)))
                                 .Select(jp => jp.ContentItemId)
-                        },
-                        WebsiteURI = new TextField($"/job-categories/{category.ToLower().Replace(' ', '-')}")
-                    }
-                });
+                         },
+                         WebsiteURI = new TextField($"/job-categories/{category.ToLower().Replace(' ', '-')}")
+                     }
+                 });
         }
     }
 }
