a88d9a034f94e6503acc0a9aa74b41da4c975fb1
Phil Davies <github@phildavies.co.uk>
2020-11-16 09:34:00 +0000

    restore orchestrator unit tests wip

--- DFC.ServiceTaxonomy.UnitTests/GraphSync/Orchestrators/SyncOrchestratorTests/SyncOrchestrator_RestoreTests.cs
+++ DFC.ServiceTaxonomy.UnitTests/GraphSync/Orchestrators/SyncOrchestratorTests/SyncOrchestrator_RestoreTests.cs
@@ -1,33 +1,56 @@
 ﻿using System;
 using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Helpers;
 using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Interfaces;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Interfaces.ContentItemVersions;
 using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Results.AllowSync;
 using DFC.ServiceTaxonomy.GraphSync.Handlers.Interfaces;
+using DFC.ServiceTaxonomy.GraphSync.Services;
 using FakeItEasy;
+using OrchardCore.ContentManagement;
 using Xunit;
 
 namespace DFC.ServiceTaxonomy.UnitTests.GraphSync.Orchestrators.SyncOrchestratorTests
 {
     public class SyncOrchestrator_RestoreTests : SyncOrchestratorTestsBase
     {
+        public SyncOperation SyncOperation { get; }
+        public IDeleteGraphSyncer PublishedDeleteGraphSyncer { get; set; }
+
         public SyncOrchestrator_RestoreTests()
         {
+            SyncOperation = SyncOperation.Restore;
+
+            PublishedDeleteGraphSyncer = A.Fake<IDeleteGraphSyncer>();
+
+            A.CallTo(() => PublishedDeleteGraphSyncer.DeleteAllowed(
+                    A<ContentItem>._,
+                    A<IContentItemVersion>.That.Matches(v => v.GraphReplicaSetName == GraphReplicaSetNames.Published),
+                    SyncOperation,
+                    null,
+                    null))
+                .Returns(PublishedAllowSync);
+
             A.CallTo(() => ServiceProvider.GetService(A<Type>.That.Matches(
                     t => t.Name == (nameof(IMergeGraphSyncer)))))
                 .Returns(PreviewMergeGraphSyncer);
+
+            A.CallTo(() => ServiceProvider.GetService(A<Type>.That.Matches(
+                    t => t.Name == (nameof(IDeleteGraphSyncer)))))
+                .Returns(PublishedDeleteGraphSyncer);
         }
 
-        //[Theory]
-        // [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Allowed, true)]
-        // [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Blocked, false)]
-        // //todo: throw an exception, if either is notrequired, would expect them both to be notrequired?
-        // [InlineData(AllowSyncResult.Allowed, AllowSyncResult.NotRequired, true)]
-        // [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Allowed, false)]
-        // [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Blocked, false)]
-        // [InlineData(AllowSyncResult.Blocked, AllowSyncResult.NotRequired, false)]
-        // [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Allowed, true)]
-        // [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Blocked, false)]
-        // [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.NotRequired, true)]
+        [Theory]
+         [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Allowed, true)]
+         [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Blocked, false)]
+         //todo: throw an exception, if either is notrequired, would expect them both to be notrequired?
+         [InlineData(AllowSyncResult.Allowed, AllowSyncResult.NotRequired, true)]
+         [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Allowed, false)]
+         [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Blocked, false)]
+         [InlineData(AllowSyncResult.Blocked, AllowSyncResult.NotRequired, false)]
+         [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Allowed, true)]
+         [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Blocked, false)]
+         [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.NotRequired, true)]
         public async Task Restore_SyncAllowedSyncMatrix_ReturnsBool(
             AllowSyncResult publishedAllowSyncAllowedResult,
             AllowSyncResult previewAllowSyncAllowedResult,
@@ -50,16 +73,16 @@ namespace DFC.ServiceTaxonomy.UnitTests.GraphSync.Orchestrators.SyncOrchestrator
         // we should only ever get NotRequired returned by both published and preview
         // not in conjunction with Allowed or Blocked
         //todo: add an exception guard for it?
-        //[Theory]
-        // [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Allowed, 1)]
-        // [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Blocked, 0)]
-        // [InlineData(AllowSyncResult.Allowed, AllowSyncResult.NotRequired, 0)]
-        // [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Allowed, 0)]
-        // [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Blocked, 0)]
-        // [InlineData(AllowSyncResult.Blocked, AllowSyncResult.NotRequired, 0)]
-        // [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Allowed, 0)]
-        // [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Blocked, 0)]
-        // [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.NotRequired, 0)]
+        [Theory]
+         [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Allowed, 1)]
+         [InlineData(AllowSyncResult.Allowed, AllowSyncResult.Blocked, 0)]
+         [InlineData(AllowSyncResult.Allowed, AllowSyncResult.NotRequired, 0)]
+         [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Allowed, 0)]
+         [InlineData(AllowSyncResult.Blocked, AllowSyncResult.Blocked, 0)]
+         [InlineData(AllowSyncResult.Blocked, AllowSyncResult.NotRequired, 0)]
+         [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Allowed, 0)]
+         [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.Blocked, 0)]
+         [InlineData(AllowSyncResult.NotRequired, AllowSyncResult.NotRequired, 0)]
         public async Task Restore_EventGridPublishingHandlerCalled(
             AllowSyncResult publishedAllowSyncResult,
             AllowSyncResult previewAllowSyncResult,
@@ -73,7 +96,7 @@ namespace DFC.ServiceTaxonomy.UnitTests.GraphSync.Orchestrators.SyncOrchestrator
 
             await SyncOrchestrator.Restore(ContentItem);
 
-            A.CallTo(() => EventGridPublishingHandler.Published(
+            A.CallTo(() => EventGridPublishingHandler.Restored(
                     A<IOrchestrationContext>.That.Matches(ctx => Equals(ctx.ContentItem, ContentItem))))
                 .MustHaveHappened(publishedCalled, Times.Exactly);
         }
