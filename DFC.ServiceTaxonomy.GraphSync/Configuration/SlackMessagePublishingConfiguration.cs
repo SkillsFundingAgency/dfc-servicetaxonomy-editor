81b15b62cccfc8d54d98469096ac93269a9ad7f8
Stu Ratcliffe <stu@ratcliffe.io>
2020-11-18 07:04:00 +0000

    Remove default slack publish settings

--- DFC.ServiceTaxonomy.GraphSync/Configuration/SlackMessagePublishingConfiguration.cs
+++ DFC.ServiceTaxonomy.GraphSync/Configuration/SlackMessagePublishingConfiguration.cs
@@ -2,7 +2,7 @@
 {
     public class SlackMessagePublishingConfiguration
     {
-        public bool? PublishToSlack { get; set; } = true;
-        public string? SlackWebhookEndpoint { get; set; } = "https://hooks.slack.com/services/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";
+        public bool? PublishToSlack { get; set; }
+        public string? SlackWebhookEndpoint { get; set; }
     }
 }
