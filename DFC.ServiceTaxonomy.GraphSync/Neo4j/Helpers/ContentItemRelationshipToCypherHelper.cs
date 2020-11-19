a829d5bf2d77c890359cf37872ab9ab42ebfc504
Phil Davies <github@phildavies.co.uk>
2020-11-19 09:50:31 +0000

    visualiser wip

--- DFC.ServiceTaxonomy.GraphSync/Neo4j/Helpers/ContentItemRelationshipToCypherHelper.cs
+++ DFC.ServiceTaxonomy.GraphSync/Neo4j/Helpers/ContentItemRelationshipToCypherHelper.cs
@@ -13,48 +13,28 @@ namespace DFC.ServiceTaxonomy.GraphSync.Neo4j.Helpers
             List<ContentItemRelationship> currentList,
             IDescribeRelationshipsContext parentContext)
         {
-            if (context.AvailableRelationships != null)
+            foreach (var child in context.AvailableRelationships)
             {
-                foreach (var child in context.AvailableRelationships)
+                if (child == null)
+                    continue;
+
+                var parentRelationship = parentContext.AvailableRelationships.FirstOrDefault(x => x.Destination.All(child.Source.Contains));
+
+                if (parentRelationship != null && !string.IsNullOrEmpty(parentRelationship.RelationshipPathString))
                 {
-                    if (child != null)
-                    {
-                        //context.CurrentDepth = parentContext.CurrentDepth + 1;
-                        var parentRelationship = parentContext.AvailableRelationships.FirstOrDefault(x => x.Destination.All(child.Source.Contains));
-
-                        if (parentRelationship != null && !string.IsNullOrEmpty(parentRelationship.RelationshipPathString))
-                        {
-                            var relationshipString = $"{parentRelationship.RelationshipPathString}-[r{context.CurrentDepth}:{child.Relationship}]-(d{context.CurrentDepth}:{string.Join(":", child.Destination!)})";
-                            child.RelationshipPathString = relationshipString;
-                        }
-                        else
-                        {
-                            //context.CurrentDepth = 1;
-                            child.RelationshipPathString = $@"match (s:{string.Join(":", context.SourceNodeLabels)} {{{context.SourceNodeIdPropertyName}: '{context.SourceNodeId}'}})-[r{1}:{child.Relationship}]-(d{1}:{string.Join(":", child.Destination!)})";
-                        }
-                    }
+                    var relationshipString = $"{parentRelationship.RelationshipPathString}-[r{context.CurrentDepth}:{child.Relationship}]-(d{context.CurrentDepth}:{string.Join(":", child.Destination!)})";
+                    child.RelationshipPathString = relationshipString;
                 }
-
-                currentList.AddRange(context.AvailableRelationships);
+                else
+                {
+                    child.RelationshipPathString = $@"match (s:{string.Join(":", context.SourceNodeLabels)} {{{context.SourceNodeIdPropertyName}: '{context.SourceNodeId}'}})-[r{1}:{child.Relationship}]-(d{1}:{string.Join(":", child.Destination!)})";
+                }
             }
 
-            //if (maxVisualiserDepth != null && context.CurrentDepth >= maxVisualiserDepth.Value)
-            //{
-            //    return currentList;
-            //}
+            currentList.AddRange(context.AvailableRelationships);
 
             foreach (var childContext in context.ChildContexts)
             {
-                //var graphSyncPartSettings = context.SyncNameProvider.GetGraphSyncPartSettings(childContext.ContentItem.ContentType);
-
-                //int? childMaxDepth = maxVisualiserDepth;
-
-                //if (graphSyncPartSettings?.VisualiserNodeDepth != null && graphSyncPartSettings.VisualiserNodeDepth.Value < maxVisualiserDepth &&
-                //    maxVisualiserDepth - context.CurrentDepth >= context.CurrentDepth + graphSyncPartSettings.VisualiserNodeDepth)
-                //{
-                //        childMaxDepth = context.CurrentDepth + graphSyncPartSettings.VisualiserNodeDepth;
-                //}
-
                 await GetRelationships((IDescribeRelationshipsContext)childContext, currentList, context);
             }
 
