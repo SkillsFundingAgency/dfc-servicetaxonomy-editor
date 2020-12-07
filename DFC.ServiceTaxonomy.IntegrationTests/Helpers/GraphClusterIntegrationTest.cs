f9be43e264bfb3d2d78891f78fe0f49505a18151
Phil Davies <github@phildavies.co.uk>
2020-12-07 14:33:23 +0000

    replica write protect wip

--- DFC.ServiceTaxonomy.IntegrationTests/Helpers/GraphClusterIntegrationTest.cs
+++ DFC.ServiceTaxonomy.IntegrationTests/Helpers/GraphClusterIntegrationTest.cs
@@ -1,10 +1,15 @@
-﻿using DFC.ServiceTaxonomy.Neo4j.Services.Internal;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.Neo4j.Services.Internal;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace DFC.ServiceTaxonomy.IntegrationTests.Helpers
 {
     public class GraphClusterIntegrationTest //: IAsyncLifetime
     {
+        public const int NumberOfReplicasConfiguredForPublishedSet = 2;
+
         public ITestOutputHelper TestOutputHelper { get; }
         private readonly GraphClusterCollectionFixture _graphClusterCollectionFixture;
 
@@ -22,6 +27,25 @@ namespace DFC.ServiceTaxonomy.IntegrationTests.Helpers
             {
                 return _graphClusterCollectionFixture.GraphClusterLowLevel;
             }
+        }
+
+        protected void ReferenceCountTest(int parallelLoops)
+        {
+            const int replicaInstance = 0;
+
+            var replicaSet = GraphClusterLowLevel.GetGraphReplicaSetLowLevel("published");
+
+            Parallel.For(0, parallelLoops, (i, state) =>
+            {
+                TestOutputHelper.WriteLine($"Thread id: {Thread.CurrentThread.ManagedThreadId}");
+
+                replicaSet.Disable(replicaInstance);
+                replicaSet.Enable(replicaInstance);
+            });
+
+            int enabledInstanceCount = replicaSet.EnabledInstanceCount;
+            Assert.Equal(NumberOfReplicasConfiguredForPublishedSet, enabledInstanceCount);
+            Assert.True(replicaSet.IsEnabled(replicaInstance));
         }
 
         // public Task InitializeAsync()
---
+++ DFC.ServiceTaxonomy.IntegrationTests/Neo4j/Services/Internal/GraphClusterLowLevel_MultiThreaded_EnableDisableTests.cs
@@ -0,0 +1,32 @@
+﻿using DFC.ServiceTaxonomy.IntegrationTests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace DFC.ServiceTaxonomy.IntegrationTests.Neo4j.Services.Internal
+{
+    // assertions are in the base class methods
+#pragma warning disable S2699
+
+    [Collection("GraphCluster Integration")]
+    public class GraphClusterLowLevel_MultiThreaded_EnableDisableTests : GraphClusterIntegrationTest
+    {
+        public GraphClusterLowLevel_MultiThreaded_EnableDisableTests(
+            GraphClusterCollectionFixture graphDatabaseCollectionFixture,
+            ITestOutputHelper testOutputHelper)
+            : base(graphDatabaseCollectionFixture, testOutputHelper)
+        {
+        }
+
+        //todo: need proper tear down, as multi-threaded test is breaking single threaded test, 2 parts:
+        // 1) stop tests running in parallel with each other
+        // 2) ensure tests start with a clean graph cluster
+
+        //[Fact]
+        public void SingleThreaderReferenceCountTest()
+        {
+            ReferenceCountTest(100);
+        }
+    }
+
+#pragma warning restore S2699
+}
