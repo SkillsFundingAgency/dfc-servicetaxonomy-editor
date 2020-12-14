b75a374c490fdd5eb80ef87c03908fd8d36e29c0
Phil Davies <github@phildavies.co.uk>
2020-12-14 13:56:36 +0000

    graph tests and tidy wip

--- DFC.ServiceTaxonomy.UnitTests/Neo4j/Services/Internal/GraphTests.cs
+++ DFC.ServiceTaxonomy.UnitTests/Neo4j/Services/Internal/GraphTests.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.Neo4j.Commands.Interfaces;
 using DFC.ServiceTaxonomy.Neo4j.Queries.Interfaces;
 using DFC.ServiceTaxonomy.Neo4j.Services.Internal;
 using Microsoft.Extensions.Logging;
@@ -13,6 +18,8 @@ namespace DFC.ServiceTaxonomy.UnitTests.Neo4j.Services.Internal
         internal INeoEndpoint NeoEndpoint { get; set; }
         internal ILogger Logger { get; set; }
         internal IQuery<int>[] Queries { get; set; }
+        internal ICommand[] Commands { get; set; }
+        internal ManualResetEventSlim TestFinished { get; set; }
         internal const string GraphName = "Steffi";
         internal const bool DefaultGraph = true;
 
@@ -24,12 +31,74 @@ namespace DFC.ServiceTaxonomy.UnitTests.Neo4j.Services.Internal
             Graph = new Graph(NeoEndpoint, GraphName, DefaultGraph, 0, Logger);
 
             Queries = new[] {A.Fake<IQuery<int>>()};
+            Commands = new[] {A.Fake<ICommand>()};
+
+            TestFinished = new ManualResetEventSlim(false);
         }
 
         [Fact]
-        public void InFlightCount_RunThrewException_CountIs0()
+        public async Task InFlightCount_RunQueries_CountMatchesInFlightQueries()
+        {
+            // arrange
+            const int inFlightRuns = 10;
+
+            A.CallTo(() => NeoEndpoint.Run(Queries, GraphName, DefaultGraph))
+                .ReturnsLazily(() =>
+                {
+                    TestFinished.Wait();
+                    return new List<int>();
+                });
+
+            var tasks = Enumerable.Range(0, inFlightRuns).Select(i => Task.Run(() => Graph.Run(Queries)));
+
+            await Task.WhenAny(Task.WhenAll(tasks), Task.Delay(2000));
+
+            // act + assert
+            Assert.Equal((ulong)inFlightRuns, Graph.InFlightCount);
+
+            // clean up
+            TestFinished.Set();
+            await Task.WhenAll(tasks);
+        }
+
+        [Fact]
+        public async Task InFlightCount_RunCommands_CountMatchesInFlightCommands()
+        {
+            // arrange
+            const int inFlightRuns = 10;
+
+            A.CallTo(() => NeoEndpoint.Run(Commands, GraphName, DefaultGraph))
+                .ReturnsLazily(() =>
+                {
+                    TestFinished.Wait();
+                    return Task.CompletedTask;
+                });
+
+            var tasks = Enumerable.Range(0, inFlightRuns).Select(i => Task.Run(() => Graph.Run(Commands)));
+
+            await Task.WhenAny(Task.WhenAll(tasks), Task.Delay(2000));
+
+            // act + assert
+            Assert.Equal((ulong)inFlightRuns, Graph.InFlightCount);
+
+            // clean up
+            TestFinished.Set();
+            await Task.WhenAll(tasks);
+        }
+
+        [Fact]
+        public void InFlightCount_RunQueriesThrewException_CountIs0()
         {
             A.CallTo(() => NeoEndpoint.Run(Queries, GraphName, DefaultGraph))
+                .Throws<Exception>();
+
+            Assert.Equal(0ul, Graph.InFlightCount);
+        }
+
+        [Fact]
+        public void InFlightCount_RunCommandsThrewException_CountIs0()
+        {
+            A.CallTo(() => NeoEndpoint.Run(Commands, GraphName, DefaultGraph))
                 .Throws<Exception>();
 
             Assert.Equal(0ul, Graph.InFlightCount);
