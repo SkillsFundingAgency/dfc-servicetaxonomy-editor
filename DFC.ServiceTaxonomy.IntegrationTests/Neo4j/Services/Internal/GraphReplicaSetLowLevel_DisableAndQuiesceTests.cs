cee0fcacdbb82e0950bbff88edd2c22e1d467f6f
Phil Davies <github@phildavies.co.uk>
2020-12-14 08:41:05 +0000

    replica disable wip

--- DFC.ServiceTaxonomy.IntegrationTests/Neo4j/Services/Internal/GraphReplicaSetLowLevel_DisableAndQuiesceTests.cs
+++ DFC.ServiceTaxonomy.IntegrationTests/Neo4j/Services/Internal/GraphReplicaSetLowLevel_DisableAndQuiesceTests.cs
@@ -11,9 +11,9 @@ using Xunit.Abstractions;
 namespace DFC.ServiceTaxonomy.IntegrationTests.Neo4j.Services.Internal
 {
     [Collection("GraphCluster Integration")]
-    public class GraphReplicaSetLowLevel_DisableTests : GraphClusterIntegrationTest
+    public class GraphReplicaSetLowLevel_DisableAndQuiesceTests : GraphClusterIntegrationTest
     {
-        public GraphReplicaSetLowLevel_DisableTests(
+        public GraphReplicaSetLowLevel_DisableAndQuiesceTests(
             GraphClusterCollectionFixture graphClusterCollectionFixture,
             ITestOutputHelper testOutputHelper)
             : base(graphClusterCollectionFixture, testOutputHelper)
@@ -23,7 +23,7 @@ namespace DFC.ServiceTaxonomy.IntegrationTests.Neo4j.Services.Internal
         // we could split this test out into multiple tests,
         // but it takes a while to run, so we combine multiple tests into one
         [Fact]
-        public void DisableAndQuiecseReplicaTest()
+        public void DisableAndQuiesceReplicaTest()
         {
             const int disableReplicaInstance = 0;
             const string disableReplicaInstanceGraphName = "neo4j";
