fc51f46652f079207cd662625804d998e1dd6216
Phil Davies <github@phildavies.co.uk>
2021-02-08 15:08:36 +0000

    Merged with cherry pick c6c668f5560d0de982275914d7b85a04183624a2

---
+++ DFC.ServiceTaxonomy.ContentApproval/DeveloperNotes.md
@@ -0,0 +1,3 @@
+﻿## Developer Notes
+A recipe **ft2-82-content-approval-module.recipe.json** has been created to enable this feature and generate the required roles to support the workflow.
+This should be merged into the main Service Taxonomy recipe before this is pulled into live. 
---
+++ DFC.ServiceTaxonomy.ContentApproval/Permissions/CanPerformReviewPermissions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OrchardCore.Security.Permissions;
+
+namespace DFC.ServiceTaxonomy.ContentApproval.Permissions
+{
+    public class CanPerformReviewPermissions : IPermissionProvider
+    {
+        public static readonly Permission CanPerformContentDesignReviewPermission = new Permission(nameof(CanPerformContentDesignReviewPermission), "Can perform content design review");
+        public static readonly Permission CanPerformSMEReviewPermission = new Permission(nameof(CanPerformSMEReviewPermission), "Can perform SME review");
+        public static readonly Permission CanPerformUXReviewPermission = new Permission(nameof(CanPerformUXReviewPermission), "Can perform UX review");
+
+        public Task<IEnumerable<Permission>> GetPermissionsAsync() =>
+            Task.FromResult(new[]
+            {
+                CanPerformContentDesignReviewPermission,
+                CanPerformSMEReviewPermission,
+                CanPerformUXReviewPermission
+            }
+            .AsEnumerable());
+
+        public IEnumerable<PermissionStereotype> GetDefaultStereotypes() =>
+            new[]
+            {
+                new PermissionStereotype
+                {
+                    Name = "Administrator",
+                    Permissions = GetPermissionsAsync().Result
+                },
+            };
+    }
+}
