7647f94e9ea7b8285dc76d5c217c0e0391dcf7af
Phil Davies <github@phildavies.co.uk>
2021-02-15 11:03:21 +0000

    content approval dashboard widgets: update card types

--- DFC.ServiceTaxonomy.ContentApproval/Models/ContentApprovalDashboardPart.cs
+++ DFC.ServiceTaxonomy.ContentApproval/Models/ContentApprovalDashboardPart.cs
@@ -7,19 +7,23 @@ namespace DFC.ServiceTaxonomy.ContentApproval.Models
     {
         InDraft,
         // single WaitingForReview or one for each?
-        WaitingForReview_UX,
-        WaitingForReview_SME,
-        WaitingForReview_Stakeholder,
-        WaitingForReview_ContentDesign,
+        // WaitingForReview_UX,
+        // WaitingForReview_SME,
+        // WaitingForReview_Stakeholder,
+        // WaitingForReview_ContentDesign,
+        WaitingForReview,
         // single InReview or one for each?
-        InReview_UX,
-        InReview_SME,
-        InReview_Stakeholder,
-        InReview_ContentDesign,
-        ForcePublished,
-        EmergencyEdited,
-        Unpublished,
-        Deleted
+        // InReview_UX,
+        // InReview_SME,
+        // InReview_Stakeholder,
+        // InReview_ContentDesign,
+        InReview,
+        Published
+        // these will be on a report card and may or may not be part of this part (probably not)
+        // ForcePublished,
+        // EmergencyEdited,
+        // Unpublished,
+        // Deleted
     }
 
     public class ContentApprovalDashboardPart : ContentPart
