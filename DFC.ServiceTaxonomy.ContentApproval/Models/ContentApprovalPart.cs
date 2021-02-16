2ba88c6c3f24cffd0e6d1dfb4f1ddbe6208f6843
Phil Davies <github@phildavies.co.uk>
2021-02-16 11:03:25 +0000

    Merge pull request #527 from SkillsFundingAgency/feature/ft28-editing-content
    FT2-28 editing content

---
+++ DFC.ServiceTaxonomy.ContentApproval/Models/ContentApprovalPart.cs
@@ -0,0 +1,10 @@
+﻿using OrchardCore.ContentManagement;
+
+namespace DFC.ServiceTaxonomy.ContentApproval.Models
+{
+    public class ContentApprovalPart : ContentPart
+    {
+        public ContentApprovalStatus ApprovalStatus { get; set; }
+        public string? Comment { get; set; }
+    }
+}
---
+++ DFC.ServiceTaxonomy.ContentApproval/ViewModels/ContentApprovalPartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DFC.ServiceTaxonomy.ContentApproval.Models;
+
+namespace DFC.ServiceTaxonomy.ContentApproval.ViewModels
+{
+    public class ContentApprovalPartViewModel
+    {
+        [Required]
+        public ContentApprovalStatus ApprovalStatus { get; set; }
+        public string? Comment { get; set; }
+        public string? ContentItemId { get; set; }
+    }
+}
