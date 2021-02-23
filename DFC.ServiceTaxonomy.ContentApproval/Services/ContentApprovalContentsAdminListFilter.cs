9195c2d82fa75f78d9ad30590294827a84e2d576
Phil Davies <github@phildavies.co.uk>
2021-02-23 08:32:26 +0000

    tidy up

--- DFC.ServiceTaxonomy.ContentApproval/Services/ContentApprovalContentsAdminListFilter.cs
+++ DFC.ServiceTaxonomy.ContentApproval/Services/ContentApprovalContentsAdminListFilter.cs
@@ -11,48 +11,16 @@ namespace DFC.ServiceTaxonomy.ContentApproval.Services
 {
     public class ContentApprovalContentsAdminListFilter : IContentsAdminListFilter
     {
-        // private readonly IContentDefinitionManager _contentDefinitionManager;
-        //
-        // public ContentApprovalContentsAdminListFilter(IContentDefinitionManager contentDefinitionManager)
-        // {
-        //     _contentDefinitionManager = contentDefinitionManager;
-        // }
-
         public async Task FilterAsync(ContentOptionsViewModel model, IQuery<ContentItem> query, IUpdateModel updater)
         {
             var viewModel = new ContentApprovalContentsAdminListFilterViewModel();
             //todo: do we need to set Prefix?
-            // ContentApproval.SelectedApprovalStatus ??
+
             //todo: common const for prefix
-            if (await updater.TryUpdateModelAsync(viewModel, "ContentApproval"))
+            if (await updater.TryUpdateModelAsync(viewModel, "ContentApproval")
+                && viewModel.SelectedApprovalStatus.HasValue)
             {
-                // Show localization content items
-                // This is intended to be used by adding ?Localization.ShowLocalizedContentTypes to an AdminMenu url.
-                // if (viewModel.ShowLocalizedContentTypes)
-                // {
-                //     var localizedTypes = _contentDefinitionManager
-                //         .ListTypeDefinitions()
-                //         .Where(x =>
-                //             x.Parts.Any(p =>
-                //                 p.PartDefinition.Name == nameof(ContentApprovalPart)))
-                //         .Select(x => x.Name);
-                //
-                //     query.With<ContentItemIndex>(x => x.ContentType.IsIn(localizedTypes));
-                // }
-                // else
-                // if (!String.IsNullOrEmpty(viewModel.SelectedApprovalStatus))
-                // {
-                    //todo: do we need published and latest?
-                    //query.With<ContentApprovalPartIndex>(i => (i.Published || i.Latest) && i.Culture == viewModel.SelectedCulture);
-
-                    //
-#pragma warning disable S1066
-                    if (viewModel.SelectedApprovalStatus.HasValue)
-                    {
-                        query.With<ContentApprovalPartIndex>(i => i.ReviewStatus == viewModel.SelectedApprovalStatus);
-                    }
-
-                    // }
+                query.With<ContentApprovalPartIndex>(i => i.ReviewStatus == viewModel.SelectedApprovalStatus);
             }
         }
     }
