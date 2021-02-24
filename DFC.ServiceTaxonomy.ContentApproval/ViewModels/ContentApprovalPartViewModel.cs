b17a03136febf00fd4fa31bd5d6b9769a117548e
Andy Rose <andy@cyberian.co.uk>
2021-02-24 13:17:18 +0000

    FT2-75 updating review button to handle review types

--- DFC.ServiceTaxonomy.ContentApproval/ViewModels/ContentApprovalPartViewModel.cs
+++ DFC.ServiceTaxonomy.ContentApproval/ViewModels/ContentApprovalPartViewModel.cs
@@ -1,4 +1,7 @@
-﻿using DFC.ServiceTaxonomy.ContentApproval.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using DFC.ServiceTaxonomy.ContentApproval.Extensions;
+using DFC.ServiceTaxonomy.ContentApproval.Models;
 
 namespace DFC.ServiceTaxonomy.ContentApproval.ViewModels
 {
@@ -7,5 +10,8 @@ namespace DFC.ServiceTaxonomy.ContentApproval.ViewModels
         public ContentReviewStatus? ReviewStatus { get; set; }
         public string? Comment { get; set; }
         public string? ContentItemId { get; set; }
+
+        public IEnumerable<KeyValuePair<string, string>> ReviewTypes => EnumExtensions.GetEnumNameAndDisplayNameDictionary( typeof(ReviewType))
+            .Where(rt => rt.Key != "None");
     }
 }
