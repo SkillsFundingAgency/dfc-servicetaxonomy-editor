448990f6d0ba072fb56e92eff37be477e5fd027d
Andy Rose <andy@cyberian.co.uk>
2021-02-25 13:43:49 +0000

    FT2-75 fix for string constant copy issue on publish

--- DFC.ServiceTaxonomy.ContentApproval/Models/Constants.cs
+++ DFC.ServiceTaxonomy.ContentApproval/Models/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public const string SubmitSaveKey = "submit.Save";
-        public const string SubmitPublishKey = "submit.Save";
+        public const string SubmitPublishKey = "submit.Publish";
         public const string SubmitRequiresRevisionValue = "submit.RequiresRevision";
         
         public const string SubmitRequestApprovalValuePrefix = "submit.RequestApproval-";
