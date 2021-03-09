f9270c7e90e6fa8a77d6fb1bbf7cf09093792263
Andy Rose <andy@cyberian.co.uk>
2021-03-09 17:40:05 +0000

    Removing unnecessary using statement

--- DFC.ServiceTaxonomy.ContentApproval/Migrations.cs
+++ DFC.ServiceTaxonomy.ContentApproval/Migrations.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using OrchardCore.Data.Migration;
 using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.ContentManagement.Metadata.Settings;
