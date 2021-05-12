654799e4d2a8e89d54cffb50eda38023c2b74af4
Andy Rose <andy@cyberian.co.uk>
2021-05-12 10:49:29 +0100

    First draft fix

---
+++ DFC.ServiceTaxonomy.ContentApproval/Configuration/PagesConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace DFC.ServiceTaxonomy.ContentApproval.Configuration
+{
+    public class PagesConfiguration
+    {
+        public string? PublishedAppPrefix { get; set; }
+        public string? PreviewAppPrefix { get; set; }
+    }
+}
