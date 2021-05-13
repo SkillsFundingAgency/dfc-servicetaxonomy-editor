fc26e1651e05c6c63b17ebf2ac9a108237037c90
Andy Rose <andy@cyberian.co.uk>
2021-05-13 16:00:07 +0100

    Merge pull request #632 from SkillsFundingAgency/ft2-520-preview-fix
    Fix for new module manifest

--- DFC.ServiceTaxonomy.CustomEditor/Manifest.cs
+++ DFC.ServiceTaxonomy.CustomEditor/Manifest.cs
@@ -3,9 +3,9 @@ using OrchardCore.Modules.Manifest;
 [assembly: Module(
     Name = "Customising the STAX editor controls",
     Author = "National Careers Service",
-    Website = "https://dfc-dev-stax-editor-as.azurewebsites.net",
+    Website = "https://github.com/SkillsFundingAgency/dfc-servicetaxonomy-editor",
     Version = "0.0.1",
     Description = "Enables custom overrides of standard orchard core controls.",
     Category = "Content Management",
-    Dependencies = new[] { "OrchardCore.Contents", "DFC.ServiceTaxonomy.ContentApproval", "DFC.ServiceTaxonomy.PageLocation" }
+    Dependencies = new[] { "OrchardCore.Contents", "DFC.ServiceTaxonomy.PageLocation" }
 )]
