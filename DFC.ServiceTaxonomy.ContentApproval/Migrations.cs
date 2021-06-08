d5b89c77a17df7f96170f5268042eaada50852ae
Andy Rose <andy@cyberian.co.uk>
2021-06-08 14:03:26 +0100

    Adding content type management to content admin role

--- DFC.ServiceTaxonomy.ContentApproval/Migrations.cs
+++ DFC.ServiceTaxonomy.ContentApproval/Migrations.cs
@@ -65,7 +65,7 @@ namespace DFC.ServiceTaxonomy.ContentApproval
 
             await _recipeMigrator.ExecuteAsync("stax-content-approval.recipe.json", this);
 
-            return await Task.FromResult(7);
+            return await Task.FromResult(8);
         }
 
         public async Task<int> UpdateFrom1Async()
@@ -156,6 +156,13 @@ namespace DFC.ServiceTaxonomy.ContentApproval
             await _recipeMigrator.ExecuteAsync("stax-content-approval-amendment-04.recipe.json", this);
 
             return 7;
+        }
+
+        public async Task<int> UpdateFrom7Async()
+        {
+            await _recipeMigrator.ExecuteAsync("stax-content-approval-amendment-05.recipe.json", this);
+
+            return 8;
         }
     }
 }
