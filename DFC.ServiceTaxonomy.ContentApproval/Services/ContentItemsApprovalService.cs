f5c2fbb2f835e4a671b44f368ee4e17730858273
Andy Rose <andy@cyberian.co.uk>
2021-06-17 15:52:48 +0100

    Merge pull request #683 from SkillsFundingAgency/ft2-611-dashboard-service-refactor
    Refactor the contentitemsapprovalservice to just use yessql queries

--- DFC.ServiceTaxonomy.ContentApproval/Services/ContentItemsApprovalService.cs
+++ DFC.ServiceTaxonomy.ContentApproval/Services/ContentItemsApprovalService.cs
@@ -8,9 +8,6 @@ using DFC.ServiceTaxonomy.ContentApproval.Models.Enums;
 using Microsoft.AspNetCore.Http;
 using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Records;
-using OrchardCore.Contents.Services;
-using OrchardCore.Contents.ViewModels;
-using OrchardCore.DisplayManagement.ModelBinding;
 using YesSql;
 using ISession = YesSql.ISession;
 
@@ -19,40 +16,35 @@ namespace DFC.ServiceTaxonomy.ContentApproval.Services
     public class ContentItemsApprovalService : IContentItemsApprovalService
     {
         private readonly ISession _session;
-        private readonly IContentsAdminListQueryService _contentsAdminListQueryService;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
         public ContentItemsApprovalService(
             ISession session,
-            IContentsAdminListQueryService contentsAdminListQueryService,
             IHttpContextAccessor httpContextAccessor)
         {
             _session = session;
             _httpContextAccessor = httpContextAccessor;
-            _contentsAdminListQueryService = contentsAdminListQueryService;
         }
-        public async Task<ContentItemsApprovalCounts> GetManageContentItemCounts(DashboardItemsStatusCard card, IUpdateModel updater)
+        public async Task<ContentItemsApprovalCounts> GetManageContentItemCounts(DashboardItemsStatusCard card)
         {
             ContentItemsApprovalCounts counts = new ContentItemsApprovalCounts();
 
             switch (card)
             {
                 case DashboardItemsStatusCard.InDraft:
-                    counts.Count = await GetManageContentItemCount(card, updater);
+                    counts.Count =  GetManageContentItemCount(card);
                     break;
                 case DashboardItemsStatusCard.Published:
-                    counts.Count = await GetManageContentItemCount(card, updater);
-                    var forcePublishedCount = await GetManageContentItemCount(card, updater, null, true);
-                    counts.SubCounts = new[] {counts.Count, forcePublishedCount };
+                    counts.Count =  GetManageContentItemCount(card);
+                    var forcePublishedCount = GetManageContentItemCount(card, null, true);
+                    counts.SubCounts = new[] { counts.Count, forcePublishedCount };
                     break;
                 case DashboardItemsStatusCard.WaitingForReview:
                 case DashboardItemsStatusCard.InReview:
                     var reviewTypes = Enum.GetValues(typeof(ReviewType)).Cast<ReviewType>();
-                    var reviewTypeCountTasks = reviewTypes.Select(rt => GetManageContentItemCount(card, updater, rt==ReviewType.None?(ReviewType?)null:rt));
-
-                    int[] reviewTypeCounts = await Task.WhenAll(reviewTypeCountTasks);
+                    var reviewTypeCounts = reviewTypes.Select(rt => GetManageContentItemCount(card, rt==ReviewType.None?(ReviewType?)null:rt)).ToArray();
                     counts.Count = reviewTypeCounts[0];
-                    counts.SubCounts = reviewTypeCounts.ToArray();
+                    counts.SubCounts = reviewTypeCounts;
                     break;
                 case DashboardItemsStatusCard.MyWorkItems:
                     counts.MyItems = await GetMyContent();
@@ -84,65 +76,47 @@ namespace DFC.ServiceTaxonomy.ContentApproval.Services
             return myContent;
         }
 
-        // if we need to count 'will need review' items, we'll have to also check for a draft version
-        // can we do that using With<> ? or do apply custom c# filtering after FilterAsync on the items before count??
-        // or generate a sql query, but then can't reuse _defaultContentsAdminListFilter (so easily)
-
-        private async Task<int> GetManageContentItemCount(
-            DashboardItemsStatusCard card, IUpdateModel updater,
-            ReviewType? reviewType = null, bool isForcePublished = false)
+        private int GetManageContentItemCount(
+            DashboardItemsStatusCard card,
+            ReviewType? reviewType = null,
+            bool isForcePublished = false)
         {
-            //var querySession = _session.Query<ContentItem>();
-
-            var filterOptions = new ContentOptionsViewModel();
+            var querySession = _session.Query<ContentItem>();
 
             switch (card)
             {
-                case DashboardItemsStatusCard.InDraft:
-                    filterOptions.ContentsStatus = ContentsStatus.Draft;
-                    //querySession.With<ContentItemIndex>(i => i.Latest && !i.Published);
-                    break;
-                case DashboardItemsStatusCard.Published:
-                    filterOptions.ContentsStatus = ContentsStatus.Published;
-                    //querySession.With<ContentItemIndex>(i => i.Latest && !i.Published);
-                    break;
                 case DashboardItemsStatusCard.WaitingForReview:
-                    filterOptions.ContentsStatus = ContentsStatus.AllVersions;
+                    querySession.With<ContentItemIndex>(i => i.Latest); // Equivalent to Orchard Core All Versions filter; this excludes deleted content items
+                    querySession.With<ContentApprovalPartIndex>(i => i.ReviewStatus == (int?)ReviewStatus.ReadyForReview);
+                    if (reviewType != null)
+                    {
+                        querySession.With<ContentApprovalPartIndex>(i => i.ReviewType == (int)reviewType);
+                    }
                     break;
                 case DashboardItemsStatusCard.InReview:
-                    filterOptions.ContentsStatus = ContentsStatus.AllVersions;
+                    querySession.With<ContentItemIndex>(i => i.Latest); // Equivalent to Orchard Core All Versions filter; this excludes deleted content items
+                    querySession.With<ContentApprovalPartIndex>(i => i.ReviewStatus == (int?)ReviewStatus.InReview);
+                    if (reviewType != null)
+                    {
+                        querySession.With<ContentApprovalPartIndex>(i => i.ReviewType == (int)reviewType);
+                    }
                     break;
-                default:
-                    throw new NotImplementedException();
-            }
-
-            var query = await _contentsAdminListQueryService.QueryAsync(filterOptions, updater);
-
-            switch (card)
-            {
+                case DashboardItemsStatusCard.InDraft:
+                    querySession.With<ContentItemIndex>(i => i.Latest && !i.Published); // Equivalent to Orchard Core Draft filter
+                    break;
                 case DashboardItemsStatusCard.Published:
+                    querySession.With<ContentItemIndex>(i => i.Published); // Equivalent to Orchard Core Published filter
                     if (isForcePublished)
                     {
-                        query.With<ContentApprovalPartIndex>(i => i.IsForcePublished);
+                        querySession.With<ContentApprovalPartIndex>(i => i.IsForcePublished);
                     }
                     break;
-                case DashboardItemsStatusCard.WaitingForReview:
-                    query.With<ContentApprovalPartIndex>(i => i.ReviewStatus == (int?)ReviewStatus.ReadyForReview);
-                    if (reviewType != null)
-                    {
-                        query.With<ContentApprovalPartIndex>(i => i.ReviewType == (int)reviewType);
-                    }
-                    break;
-                case DashboardItemsStatusCard.InReview:
-                    filterOptions.ContentsStatus = ContentsStatus.AllVersions;
-                    query.With<ContentApprovalPartIndex>(i => i.ReviewStatus == (int?)ReviewStatus.InReview);
-                    if (reviewType != null)
-                    {
-                        query.With<ContentApprovalPartIndex>(i => i.ReviewType == (int)reviewType);
-                    }
+                default:
+                    querySession.With<ContentItemIndex>(i => i.Latest);
                     break;
             }
-            return await query.CountAsync();
+
+            return querySession.CountAsync().Result;
         }
     }
 }
