70e9c88465a471945a5ff9c1f5d75de3339a81ed
Prasad Ramireddy <pramireddy08@gmail.com>
2021-07-02 11:27:30 +0100

    Merge after cherry-pick of banner module

---
+++ DFC.ServiceTaxonomy.Banners/Drivers/BannerPartDisplayDriver.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.Banners.Models;
+using DFC.ServiceTaxonomy.Banners.ViewModels;
+using Microsoft.Extensions.Localization;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.ContentManagement.Display.Models;
+using OrchardCore.ContentManagement.Metadata.Models;
+using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.DisplayManagement.Views;
+using OrchardCore.Mvc.ModelBinding;
+using YesSql;
+
+namespace DFC.ServiceTaxonomy.Banners.Drivers
+{
+    public class BannerPartDisplayDriver : ContentPartDisplayDriver<BannerPart>
+    {
+        private readonly IStringLocalizer S;
+        private readonly ISession _session;
+
+        public BannerPartDisplayDriver(IStringLocalizer<BannerPartDisplayDriver> localizer, ISession session)
+        {
+            S = localizer;
+            _session = session;
+        }
+
+        public override IDisplayResult Display(BannerPart part, BuildPartDisplayContext context)
+        {
+            return Initialize<BannerPartViewModel>(GetDisplayShapeType(context), model => BuildViewModel(model, part, context.TypePartDefinition))
+                .Location("Detail", "Content")
+                .Location("Summary", "Content");
+        }
+
+        public override IDisplayResult Edit(BannerPart part, BuildPartEditorContext context)
+        {
+            return Initialize<BannerPartViewModel>(GetEditorShapeType(context), model => BuildViewModel(model, part, context.TypePartDefinition));
+        }
+
+        private void BuildViewModel(BannerPartViewModel model, BannerPart part, ContentTypePartDefinition typePartDefinition)
+        {
+            model.WebPageName = part.WebPageName;
+            model.WebPageURL = part.WebPageURL;
+            model.BannerPart = part;
+            model.PartDefinition = typePartDefinition;
+        }
+
+        public override async Task<IDisplayResult> UpdateAsync(BannerPart part, IUpdateModel updater,
+            UpdatePartEditorContext context)
+        {
+            var updated = await updater.TryUpdateModelAsync(part, Prefix, b => b.WebPageName, b => b.WebPageURL);
+            if (updated)
+            {
+                await foreach (var item in part.ValidateAsync(S, _session))
+                {
+                    updater.ModelState.BindValidationResult(Prefix, item);
+                }
+            }
+
+            return await EditAsync(part, context);
+        }
+    }
+}
---
+++ DFC.ServiceTaxonomy.Banners/Startup.cs
@@ -0,0 +1,31 @@
+﻿using DFC.ServiceTaxonomy.Banners.Drivers;
+using DFC.ServiceTaxonomy.Banners.GraphSyncers;
+using DFC.ServiceTaxonomy.Banners.Indexes;
+using DFC.ServiceTaxonomy.Banners.Models;
+using DFC.ServiceTaxonomy.GraphSync.GraphSyncers.Interfaces.Parts;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.Data.Migration;
+using OrchardCore.Modules;
+using YesSql.Indexes;
+
+namespace DFC.ServiceTaxonomy.Banners
+{
+    public class Startup : StartupBase
+    {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddContentPart<BannerPart>()
+                .UseDisplayDriver<BannerPartDisplayDriver>();
+
+            services.AddScoped<IDataMigration, Migrations>();
+
+            services.AddSingleton<IIndexProvider, BannerPartIndexProvider>();
+
+            services.AddTransient<IContentPartGraphSyncer, BannerPartGraphSyncer>();
+
+        }
+    }
+}
+
