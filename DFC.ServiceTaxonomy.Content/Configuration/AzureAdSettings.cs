2f37c09faac39e0899533259951c5e8213b2a147
Prasad Ramireddy <pramireddy08@gmail.com>
2021-07-20 15:56:40 +0100

    Ft2 150 media feature purge cdn (#705)
    Added code to Purge CDN through STAX editor

---
+++ DFC.ServiceTaxonomy.Content/Configuration/AzureAdSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DFC.ServiceTaxonomy.Content.Configuration
+{
+    [ExcludeFromCodeCoverage]
+    public class AzureAdSettings
+    {
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+        public string? Authority { get; set; }
+        public string? SubscriptionId { get; set; }
+        public string? CdnProfileName { get; set; }
+        public string? CdnEndpointName { get; set; }
+        public string? ResourceGroupName { get; set; }
+        public string? KeyVaultAddress { get; set; }
+    }
+}
--- DFC.ServiceTaxonomy.Editor/Startup.cs
+++ DFC.ServiceTaxonomy.Editor/Startup.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-
+using DFC.ServiceTaxonomy.Content.Configuration;
 using DFC.ServiceTaxonomy.CustomEditor.Configuration;
 using DFC.ServiceTaxonomy.Editor.Security;
 using DFC.ServiceTaxonomy.GraphSync.Settings;
@@ -74,6 +74,7 @@ namespace DFC.ServiceTaxonomy.Editor
             services.PostConfigure(SetupMediaConfig());
 
             services.Configure<PagesConfiguration>(Configuration.GetSection("Pages"));
+            services.Configure<AzureAdSettings>(Configuration.GetSection("AzureAdSettings"));
         }
 
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
