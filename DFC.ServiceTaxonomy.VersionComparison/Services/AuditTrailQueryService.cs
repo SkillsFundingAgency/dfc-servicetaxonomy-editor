3bf58babb745789d10aed55f20daf5a669c8bd43
Andy Rose <andy@cyberian.co.uk>
2021-08-13 14:21:18 +0100

    FT2-293 version comparison base module and ddls (#723)
    * Initial version compare module set up with version selectors
    
    * Adding link to version comparison on content items page
    
    * Changes as part of review

---
+++ DFC.ServiceTaxonomy.VersionComparison/Services/AuditTrailQueryService.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using OrchardCore.AuditTrail.Indexes;
+using OrchardCore.AuditTrail.Models;
+using OrchardCore.Contents.AuditTrail.Models;
+using OrchardCore.Entities;
+using YesSql;
+
+namespace DFC.ServiceTaxonomy.VersionComparison.Services
+{
+    public class AuditTrailQueryService : IAuditTrailQueryService
+    {
+        private readonly ISession _session;
+        private const string ContentCategory = "Content";
+
+        public AuditTrailQueryService(ISession session)
+        {
+            _session = session;
+        }
+
+        public async Task<List<AuditTrailContentEvent>> GetVersions(string contentItemId)
+        {
+            var allAuditVersions = await _session
+                .Query<AuditTrailEvent, AuditTrailEventIndex>(collection: AuditTrailEvent.Collection)
+                .Where(index =>
+                    index.Category == ContentCategory &&
+                    index.CorrelationId == contentItemId)
+                .OrderByDescending(index => index.Id)
+                .ListAsync();
+
+            if (allAuditVersions == null || !allAuditVersions.Any())
+            {
+                throw new DataException(
+                    $"No audit trail version information was found in the audit trail index for {contentItemId}");
+            }
+
+            var auditTrailContentEventsList = new List<AuditTrailContentEvent>();
+
+            foreach (var auditTrailEvent in allAuditVersions)
+            {
+                var auditTrailContentEvent = auditTrailEvent.As<AuditTrailContentEvent>();
+                if (auditTrailContentEventsList.All(k => k.VersionNumber != auditTrailContentEvent.VersionNumber))
+                {
+                    auditTrailContentEventsList.Add(auditTrailContentEvent);
+                }
+            }
+
+            return auditTrailContentEventsList;
+        }
+    }
+}
---
+++ DFC.ServiceTaxonomy.VersionComparison/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using DFC.ServiceTaxonomy.VersionComparison.Controllers;
+using DFC.ServiceTaxonomy.VersionComparison.Drivers;
+using DFC.ServiceTaxonomy.VersionComparison.Services;
+using DFC.ServiceTaxonomy.VersionComparison.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrchardCore.Admin;
+using OrchardCore.ContentManagement.Display.ContentDisplay;
+using OrchardCore.DisplayManagement;
+using OrchardCore.DisplayManagement.Handlers;
+using OrchardCore.Modules;
+using OrchardCore.Mvc.Core.Utilities;
+
+namespace DFC.ServiceTaxonomy.VersionComparison
+{
+    public class Startup : StartupBase
+    {
+        private readonly AdminOptions _adminOptions;
+
+        public Startup(IOptions<AdminOptions> adminOptions)
+        {
+            _adminOptions = adminOptions.Value;
+        }
+
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IDisplayManager<VersionComparisonOptions>, DisplayManager<VersionComparisonOptions>>()
+                .AddScoped<IDisplayDriver<VersionComparisonOptions>, VersionComparisonOptionsDisplayDriver>();
+
+            services.AddScoped<IAuditTrailQueryService, AuditTrailQueryService>();
+
+            services.AddScoped<IContentDisplayDriver, VersionComparisonContentsDriver>();
+        }
+
+        public override void Configure(IApplicationBuilder app, IEndpointRouteBuilder routes,
+            IServiceProvider serviceProvider)
+        {
+            var adminControllerName = typeof(AdminController).ControllerName();
+
+            routes.MapAreaControllerRoute(
+                name: "VersionCompareIndex",
+                areaName: "DFC.ServiceTaxonomy.VersionComparison",
+                pattern: _adminOptions.AdminUrlPrefix + "/VersionComparison/{contentItemId}",
+                defaults: new { controller = adminControllerName, action = nameof(AdminController.Index) }
+            );
+        }
+    }
+}
