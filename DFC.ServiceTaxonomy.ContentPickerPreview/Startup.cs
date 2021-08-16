f3c1430189f1f2b7a31b2efa26309eba5e041375
Prasad Ramireddy <pramireddy08@gmail.com>
2021-08-16 17:36:42 +0100

    Story/ft2 835 banner content picker (#724)
    * Added code for BannerContentPicker
    
    * Added code for update styling for BannerContentPicker
    
    * Added code to update styling for BannerContentPicker
    
    * Updated PageBanner contenttype for ContentPickerField option
    
    * Updated Model classes for BannerContentPicker
    
    * MInor changes to resolve PR comments
    
    * Minor change to resolve PR comments

--- DFC.ServiceTaxonomy.ContentPickerPreview/Startup.cs
+++ DFC.ServiceTaxonomy.ContentPickerPreview/Startup.cs
@@ -1,10 +1,12 @@
 using System;
+using DFC.ServiceTaxonomy.ContentPickerPreview.Controllers;
 using DFC.ServiceTaxonomy.ContentPickerPreview.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using OrchardCore.Modules;
 using OrchardCore.ContentManagement;
+using OrchardCore.Modules;
+using OrchardCore.Mvc.Core.Utilities;
 
 namespace DFC.ServiceTaxonomy.ContentPickerPreview
 {
@@ -13,11 +15,19 @@ namespace DFC.ServiceTaxonomy.ContentPickerPreview
         public override void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IContentPickerResultProvider, PreviewContentPickerResultProvider>();
+            services.AddScoped<IBannerContentPickerResultProvider, BannerContentPickerResultProvider>();
         }
 
         public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
         {
             builder.UseStaticFiles();
+
+            routes.MapAreaControllerRoute(
+                name: "BannerContentPicker",
+                areaName: "DFC.ServiceTaxonomy.ContentPickerPreview",
+                pattern: "Admin/BannerContentPicker/SearchContentItems",
+                defaults: new { controller = typeof(BannerContentPickerAdminController).ControllerName(), action = nameof(BannerContentPickerAdminController.SearchContentItems) }
+              );
         }
     }
 }
