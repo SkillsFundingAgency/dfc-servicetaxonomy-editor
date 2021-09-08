728232c136862e7a1ae1f18b8e4fe16af0524b2d
Andy Rose <andy@cyberian.co.uk>
2021-09-08 15:44:48 +0100

    Repositioning buttons on the admin summary page

--- DFC.ServiceTaxonomy.CustomEditor/Startup.cs
+++ DFC.ServiceTaxonomy.CustomEditor/Startup.cs
@@ -1,8 +1,15 @@
-﻿using OrchardCore.Modules;
+﻿using DFC.ServiceTaxonomy.CustomEditor.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.DisplayManagement.Descriptors;
+using OrchardCore.Modules;
 
 namespace DFC.ServiceTaxonomy.CustomEditor
 {
     public class Startup : StartupBase
     {
+        public override void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IShapeTableProvider, SummaryAdminShapes>();
+        }
     }
 }
