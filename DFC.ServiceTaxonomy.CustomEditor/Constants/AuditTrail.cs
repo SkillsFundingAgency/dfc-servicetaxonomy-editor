43c1eb39c321f9f20a8da7829853d87e17f19f13
Andy Rose <andy@cyberian.co.uk>
2021-09-17 08:55:08 +0100

    Type fix and additional constant

--- DFC.ServiceTaxonomy.CustomEditor/Constants/AuditTrail.cs
+++ DFC.ServiceTaxonomy.CustomEditor/Constants/AuditTrail.cs
@@ -3,6 +3,7 @@
     public static class AuditTrail
     {
         public const string ContentEvent_Removed = "Removed";
-        public const string ContentEventName_DiscardDraft = "Discar draft";
+        public const string ContentEventName_DiscardDraft = "Discard draft";
+        public const string UrlPart_DiscardDraft = "discarddraft";
     }
 }
