dfeb0454e11deca722abb1788d7be835b832a11f
Andy Rose <andy@cyberian.co.uk>
2022-01-25 14:59:17 +0000

    FT2-348 showing skills descriptions (#896)
    * Fixing what it takes message
    
    * Adding id to related skills

--- DFC.ServiceTaxonomy.JobProfiles.Module/ServiceBusHandling/Converters/WhatItTakesMessageConverter.cs
+++ DFC.ServiceTaxonomy.JobProfiles.Module/ServiceBusHandling/Converters/WhatItTakesMessageConverter.cs
@@ -10,16 +10,23 @@ using DFC.ServiceTaxonomy.Title.Models;
 using Microsoft.Extensions.DependencyInjection;
 
 using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Records;
 using OrchardCore.Title.Models;
+
+using YesSql;
 
 namespace DFC.ServiceTaxonomy.JobProfiles.Module.ServiceBusHandling.Converters
 {
     public class WhatItTakesMessageConverter : IMessageConverter<WhatItTakesData>
     {
         private readonly IServiceProvider _serviceProvider;
+        private readonly ISession _session;
 
-        public WhatItTakesMessageConverter(IServiceProvider serviceProvider) =>
+        public WhatItTakesMessageConverter(IServiceProvider serviceProvider, ISession session)
+        {
+            _session = session;
             _serviceProvider = serviceProvider;
+        }
 
         public async Task<WhatItTakesData> ConvertFromAsync(ContentItem contentItem)
         {
@@ -27,10 +34,11 @@ namespace DFC.ServiceTaxonomy.JobProfiles.Module.ServiceBusHandling.Converters
             IEnumerable<ContentItem> relatedSocSkillMatrixSkills = await Helper.GetContentItemsAsync(contentItem.Content.JobProfile.Relatedskills, contentManager);
             IEnumerable<ContentItem> relatedDigitalSkills = await Helper.GetContentItemsAsync(contentItem.Content.JobProfile.DigitalSkills, contentManager);
             IEnumerable<ContentItem> relatedRestrictions = await Helper.GetContentItemsAsync(contentItem.Content.JobProfile.Relatedrestrictions, contentManager);
-
+            IEnumerable<ContentItem> skillContentItems = await _session.Query<ContentItem, ContentItemIndex>(c => c.ContentType == ContentTypes.Skill && c.Latest).ListAsync();
+            IEnumerable<ContentItem> socCodes = await _session.Query<ContentItem, ContentItemIndex>(c => c.ContentType == ContentTypes.SOCCode && c.Latest).ListAsync();
             var whatItTakesData = new WhatItTakesData
             {
-                RelatedSocSkillMatrixSkills = GetSkills(relatedSocSkillMatrixSkills),
+                RelatedSocSkillMatrixSkills = GetSkills(relatedSocSkillMatrixSkills, skillContentItems, socCodes),
                 RelatedDigitalSkills = GetDigitalSkills(relatedDigitalSkills),
                 OtherRequirements = contentItem.Content.JobProfile.Otherrequirements.Html,
                 RelatedRestrictions = GetRestrictions(relatedRestrictions)
@@ -38,7 +46,7 @@ namespace DFC.ServiceTaxonomy.JobProfiles.Module.ServiceBusHandling.Converters
             return whatItTakesData;
         }
 
-        private IEnumerable<SocSkillMatrixItem> GetSkills(IEnumerable<ContentItem> contentItems)
+        private IEnumerable<SocSkillMatrixItem> GetSkills(IEnumerable<ContentItem> contentItems, IEnumerable<ContentItem> skillContentItems, IEnumerable<ContentItem> socCodes)
         {
             var skills = new List<SocSkillMatrixItem>();
             if (contentItems.Any())
@@ -52,39 +60,42 @@ namespace DFC.ServiceTaxonomy.JobProfiles.Module.ServiceBusHandling.Converters
                         Title = contentItem.As<UniqueTitlePart>()?.Title,
                         Contextualised = contentItem.Content.SOCSkillsMatrix.Contextualised is null ? default : (string?)contentItem.Content.SOCSkillsMatrix.Contextualised.Text,
                         ONetAttributeType = contentItem.Content.SOCSkillsMatrix.ONetAttributeType is null ? default : (string?)contentItem.Content.SOCSkillsMatrix.ONetAttributeType.Text,
-                        // TODO: If we can provide the Rank/Ordinal this code can be changed
-                        Rank = i + 1, // contentItem.Content.SOCSkillsMatrix.Rank is null ? default : (decimal?)contentItem.Content.SOCSkillsMatrix.Rank.Rank,
+                        Rank = i + 1,
                         ONetRank = contentItem.Content.SOCSkillsMatrix.ONetRank is null ? default : (decimal?)contentItem.Content.SOCSkillsMatrix.ONetRank.Text
                     };
 
                     var skillName = contentItem.Content.SOCSkillsMatrix.RelatedSkill is null ? default : (string?)contentItem.Content.SOCSkillsMatrix.RelatedSkill.Text;
+
                     if (skillName != null)
                     {
-                        var relatedSkills = new List<FrameworkSkillItem>()
+                        var relatedSkill = new FrameworkSkillItem
                         {
-                            new FrameworkSkillItem()
-                            {
-                                // TODO: If absence of Guid ID or other field cause any issue the code will be changed 
-                                //Id = GuidId,
-                                Title = skillName
-                            }
+                            Title = skillName
                         };
-                        skill.RelatedSkill = relatedSkills;
+                        var skillContentItem = skillContentItems.FirstOrDefault(c => c.DisplayText == skillName);
+                        if(skillContentItem != null)
+                        {
+                            relatedSkill.Id = skillContentItem.As<GraphSyncPart>().ExtractGuid();
+                            relatedSkill.Description = skillContentItem.Content.Skill.Description.Text;
+                            relatedSkill.ONetElementId = skillContentItem.Content.Skill.ONetElementId.Text;
+                        }
+
+                        skill.RelatedSkill = new List<FrameworkSkillItem> { relatedSkill };
                     }
 
                     var socCode = contentItem.Content.SOCSkillsMatrix.RelatedSOCcode is null ? default : (string?)contentItem.Content.SOCSkillsMatrix.RelatedSOCcode.Text;
                     if (socCode != null)
                     {
-                        var relatedSOCCodes = new List<RelatedSocCodeItem>()
+                        var relatedSOCCode = new RelatedSocCodeItem
                         {
-                            new RelatedSocCodeItem()
-                            {
-                                // TODO: If absence of Guid ID or other field cause any issue the code will be changed 
-                                //Id = GuidId,
-                                SOCCode = socCode
-                            }
+                            SOCCode = socCode
                         };
-                        skill.RelatedSOC = relatedSOCCodes;
+                        var socCodeContentItem = socCodes.FirstOrDefault(c => c.DisplayText == socCode);
+                        if(socCodeContentItem != null)
+                        {
+                            relatedSOCCode.Id = socCodeContentItem.As<GraphSyncPart>().ExtractGuid();
+                        }
+                        skill.RelatedSOC = new List<RelatedSocCodeItem> { relatedSOCCode };
                     }
                     skills.Add(skill);
                 }
