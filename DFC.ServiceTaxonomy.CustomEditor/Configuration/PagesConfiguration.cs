041da6184d27af1d453667bbb7956065f6bef7ad
Andy Rose <andy@cyberian.co.uk>
2022-02-04 14:54:45 +0000

    Adding additional code and config to allow job profile preview

--- DFC.ServiceTaxonomy.CustomEditor/Configuration/PagesConfiguration.cs
+++ DFC.ServiceTaxonomy.CustomEditor/Configuration/PagesConfiguration.cs
@@ -4,5 +4,7 @@
     {
         public string? PublishedAppPrefix { get; set; }
         public string? PreviewAppPrefix { get; set; }
+        public string? PublishedJobProfileAppPrefix { get; set; }
+        public string? PreviewJobProfileAppPrefix { get; set; }
     }
 }
