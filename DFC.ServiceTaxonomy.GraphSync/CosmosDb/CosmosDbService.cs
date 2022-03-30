3b7784934b17fada34b525b1a555b7fc8ea0542d
Andy Rose <andy@cyberian.co.uk>
2022-03-30 11:47:05 +0100

    Initial draft of delete relationships

--- DFC.ServiceTaxonomy.GraphSync/CosmosDb/CosmosDbService.cs
+++ DFC.ServiceTaxonomy.GraphSync/CosmosDb/CosmosDbService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DFC.ServiceTaxonomy.GraphSync.CosmosDb.Interfaces;
 using DFC.ServiceTaxonomy.GraphSync.Exceptions;
+using DFC.ServiceTaxonomy.GraphSync.Extensions;
 using Microsoft.Azure.Cosmos;
 using Newtonsoft.Json.Linq;
 
@@ -27,7 +28,6 @@ namespace DFC.ServiceTaxonomy.GraphSync.CosmosDb
             return _containers[name];
         }
 
-
         public async Task DeleteIncomingRelationshipAsync(Container container, string contentType, string id, string relationshipId)
         {
             var relationshipItem = await GetContentItemFromDatabase(container, contentType, id);
@@ -49,6 +49,23 @@ namespace DFC.ServiceTaxonomy.GraphSync.CosmosDb
             await container.UpsertItemAsync(relationshipItem);
         }
 
+        public async Task DeleteItemAsync(Container container, string contentType, string id)
+        {
+            var contentItem = await GetContentItemFromDatabase(container, contentType, id);
+            if (contentItem == null) return;
+
+            // find outgoing relations to other content items
+            var existingItemRelationships = (contentItem["_links"] as JObject)!.GetLinks().SelectMany(l => l.Value.Select(GetContentTypeAndId));
+
+            // remove and incoming relationship to the content item to be deleted from these related content items
+            foreach ((string, string) relationship in existingItemRelationships)
+            {
+                await DeleteIncomingRelationshipAsync(container, relationship.Item1, relationship.Item2, $"{contentType}{id}");
+            }
+
+            // delete content item
+            await container.DeleteItemAsync<Dictionary<string, object>>(id, new PartitionKey(contentType.ToLower()));
+        }
 
         public async Task<Dictionary<string, object>?> GetContentItemFromDatabase(Container container, string contentType, string id)
         {
@@ -58,6 +75,14 @@ namespace DFC.ServiceTaxonomy.GraphSync.CosmosDb
 
             var result = await iteratorLoop.ReadNextAsync();
             return result.Resource.SingleOrDefault();
+        }
+
+        private static (string, string) GetContentTypeAndId(string uri)
+        {
+            string id = uri.Split('/')[uri.Split('/').Length - 1];
+            string contentType = uri.Split('/')[uri.Split('/').Length - 2].ToLower();
+
+            return (contentType, id);
         }
     }
 }
