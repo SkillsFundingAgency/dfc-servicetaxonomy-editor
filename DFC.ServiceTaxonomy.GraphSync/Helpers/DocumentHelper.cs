23a0e2e66324bd3a8890c6f5270fd247e00a9456
Andy Rose <andy@cyberian.co.uk>
2022-04-14 10:23:12 +0100

    Changes and refactorings from replace relationships command pr that got missed (#965)

--- DFC.ServiceTaxonomy.GraphSync/Helpers/DocumentHelper.cs
+++ DFC.ServiceTaxonomy.GraphSync/Helpers/DocumentHelper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 
 namespace DFC.ServiceTaxonomy.GraphSync.Helpers
@@ -65,6 +66,34 @@ namespace DFC.ServiceTaxonomy.GraphSync.Helpers
             }
 
             return dictionary;
+        }
+
+        public static List<Dictionary<string, object>> SafeObjectArrayCastToDictionaryList(object? value)
+        {
+            var returnList = new List<Dictionary<string, object>>();
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (value is JObject valObj)
+            {
+                returnList.Add(valObj.ToObject<Dictionary<string, object>>()!);
+            }
+            else if (value is JArray jArray)
+            {
+                returnList.AddRange(jArray.Select(jToken => ((JObject)jToken).ToObject<Dictionary<string, object>>()!));
+            }
+            else if (value is Dictionary<string, object> dictionary)
+            {
+                returnList.Add(dictionary);
+            }
+            else
+            {
+                throw new ArgumentException($"Didn't expect type {value.GetType().Name}");
+            }
+
+            return returnList;
         }
 
         public static bool CanCastToList(object? value)
