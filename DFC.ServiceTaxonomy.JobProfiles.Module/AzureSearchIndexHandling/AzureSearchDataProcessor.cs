20811388396d752012ee7c7fe1c82ddd6cb6a9c9
ravi-methods <ravi.mittal@methods.co.uk>
2022-04-28 08:48:20 +0100

    Fixed search index code to create a new index when it doesn't exist.

--- DFC.ServiceTaxonomy.JobProfiles.Module/AzureSearchIndexHandling/AzureSearchDataProcessor.cs
+++ DFC.ServiceTaxonomy.JobProfiles.Module/AzureSearchIndexHandling/AzureSearchDataProcessor.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Azure;
 using Azure.Search.Documents;
 using Azure.Search.Documents.Indexes;
 using Azure.Search.Documents.Indexes.Models;
 using Azure.Search.Documents.Models;
+
 using DFC.ServiceTaxonomy.JobProfiles.Module.AzureSearchIndexHandling.Interfaces;
 using DFC.ServiceTaxonomy.JobProfiles.Module.Models.AzureSearch;
 using DFC.ServiceTaxonomy.JobProfiles.Module.ServiceBusHandling;
 using DFC.ServiceTaxonomy.JobProfiles.Module.ServiceBusHandling.Interfaces;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+
 using OrchardCore.ContentManagement.Handlers;
 
 namespace DFC.ServiceTaxonomy.JobProfiles.Module.AzureSearchIndexHandling
@@ -38,36 +42,45 @@ namespace DFC.ServiceTaxonomy.JobProfiles.Module.AzureSearchIndexHandling
                 var jobProfileIndexDocument = await _jobProfileIndexConverter.ConvertFromAsync(context.ContentItem);
                 var jobProfileIndexSettings = _configuration.GetSection("AzureSearchSettings").Get<JobProfileIndexSettings>();
                 var jobProfileIndexName = jobProfileIndexSettings.JobProfileSearchIndexName ?? string.Empty;
+                var searchServiceEndPoint = jobProfileIndexSettings.SearchServiceEndPoint ?? string.Empty;
+                var adminApiKey = jobProfileIndexSettings.SearchServiceAdminAPIKey ?? string.Empty;
 
-                SearchIndexClient indexClient = CreateSearchIndexClient(jobProfileIndexSettings.SearchServiceEndPoint ?? string.Empty, jobProfileIndexSettings.SearchServiceAdminAPIKey ?? string.Empty);
+                SearchIndexClient indexClient = new SearchIndexClient(new Uri(searchServiceEndPoint), new AzureKeyCredential(adminApiKey));
                 if (await indexClient.GetIndexesAsync().AnyAsync(index => index.Name == jobProfileIndexName))
                 {
                     SearchClient searchClient = indexClient.GetSearchClient(jobProfileIndexName);
-                    if (actionType == ActionTypes.Published)
+                    if (actionType.Equals(ActionTypes.Published))
                     {
 
                         if (await indexClient.GetIndexesAsync().AnyAsync(index => index.Name == jobProfileIndexName))
                         {
-                            var documentUploadResponse = UploadDocument(searchClient, jobProfileIndexDocument);
-                            if (documentUploadResponse.IsCompletedSuccessfully) _logger.Log(LogLevel.Information, $"{jobProfileIndexDocument.IdentityField} successfully uploaded to Azure Search Index");
+                            var documentUploadResponse = await UploadDocumentAsync(searchClient, jobProfileIndexDocument);
+                            if (IsSuccessful(documentUploadResponse))
+                            {
+                                _logger.Log(LogLevel.Information, $"{jobProfileIndexDocument.IdentityField} successfully uploaded to Azure Search Index");
+                            }
                         }
 
                     }
-                    else if (actionType == ActionTypes.Deleted)
+                    else if (actionType.Equals(ActionTypes.Deleted))
                     {
-                        var documentUploadResponse = DeleteDocument(searchClient, jobProfileIndexDocument);
-                        if (documentUploadResponse.IsCompletedSuccessfully) _logger.Log(LogLevel.Information, $"{jobProfileIndexDocument.IdentityField} successfully deleted document from Azure Search Index");
+                        var documentDeleteResponse = await DeleteDocumentAsync(searchClient, jobProfileIndexDocument);
+                        if (IsSuccessful(documentDeleteResponse))
+                        {
+                            _logger.Log(LogLevel.Information, $"{jobProfileIndexDocument.IdentityField} successfully deleted document from Azure Search Index");
+                        }
                     }
                 }
                 else                 // The creation of the index can be done at application start up
                 {
-                    if (CreateIndexAsync(jobProfileIndexName, indexClient).IsCompletedSuccessfully)
-                    {
-                        SearchClient searchClient = indexClient.GetSearchClient(jobProfileIndexName);
-                        var documentUploadResponse = UploadDocument(searchClient, jobProfileIndexDocument);
-                        if (documentUploadResponse.IsCompletedSuccessfully) _logger.Log(LogLevel.Information, $"{jobProfileIndexName} index successfully created and document{jobProfileIndexDocument.IdentityField} successfully uploaded to Azure Search Index");
-                    }
+                    Response<SearchIndex> response = await CreateIndexAsync(jobProfileIndexName, indexClient);
 
+                    SearchClient searchClient = indexClient.GetSearchClient(jobProfileIndexName);
+                    var documentUploadResponse = await UploadDocumentAsync(searchClient, jobProfileIndexDocument);
+                    if (IsSuccessful(documentUploadResponse))
+                    {
+                        _logger.Log(LogLevel.Information, $"{jobProfileIndexName} index successfully created and document - {jobProfileIndexDocument.IdentityField} successfully uploaded to Azure Search Index");
+                    }
                 }
             }
 
@@ -78,32 +91,46 @@ namespace DFC.ServiceTaxonomy.JobProfiles.Module.AzureSearchIndexHandling
             }
         }
 
-        private static SearchIndexClient CreateSearchIndexClient(string searchServiceEndPoint, string adminApiKey)
-        {
-            SearchIndexClient indexClient = new SearchIndexClient(new Uri(searchServiceEndPoint), new AzureKeyCredential(adminApiKey));
-            return indexClient;
-        }
+        private static bool IsSuccessful(Response<IndexDocumentsResult> response) =>
+            response.Value.Results.All(r => r.Succeeded);
 
         private static Task<Response<SearchIndex>> CreateIndexAsync(string indexName, SearchIndexClient indexClient)
         {
             FieldBuilder builder = new FieldBuilder();
             var definition = new SearchIndex(indexName, builder.Build(typeof(JobProfileIndex)));
+            definition.Suggesters.Add(new SearchSuggester("sg", new[] {
+                nameof(JobProfileIndex.Title),
+                nameof(JobProfileIndex.AlternativeTitle)
+            }));
+            definition.ScoringProfiles.Add(new ScoringProfile("jp")
+            {
+                TextWeights = new TextWeights(
+                new Dictionary<string, double>{
+                  { nameof(JobProfileIndex.TitleAsKeyword), 100},
+                  { nameof(JobProfileIndex.AltTitleAsKeywords), 100 },
+                  { nameof(JobProfileIndex.Title), 7 },
+                  { nameof(JobProfileIndex.AlternativeTitle), 6 },
+                  { nameof(JobProfileIndex.Overview), 5 },
+                  { nameof(JobProfileIndex.JobProfileCategories), 4 },
+                  { nameof(JobProfileIndex.JobProfileSpecialism), 3 },
+                  { nameof(JobProfileIndex.HiddenAlternativeTitle), 3 }
+                })
+            });
             return indexClient.CreateIndexAsync(definition);
         }
 
-        private static Task<Response<IndexDocumentsResult>> UploadDocument(SearchClient searchClient, JobProfileIndex jobProfileIndexDocument)
+        private static Task<Response<IndexDocumentsResult>> UploadDocumentAsync(SearchClient searchClient, JobProfileIndex jobProfileIndexDocument)
         {
             IndexDocumentsBatch<JobProfileIndex> jobProfileIndexBatch = IndexDocumentsBatch.Create(IndexDocumentsAction.MergeOrUpload(jobProfileIndexDocument));
             IndexDocumentsOptions options = new IndexDocumentsOptions { ThrowOnAnyError = true };
             return searchClient.IndexDocumentsAsync(jobProfileIndexBatch, options);
         }
 
-        private static Task<Response<IndexDocumentsResult>> DeleteDocument(SearchClient searchClient, JobProfileIndex jobProfileIndexDocument)
+        private static Task<Response<IndexDocumentsResult>> DeleteDocumentAsync(SearchClient searchClient, JobProfileIndex jobProfileIndexDocument)
         {
             IndexDocumentsBatch<JobProfileIndex> jobProfileIndexBatch = IndexDocumentsBatch.Delete<JobProfileIndex>(new List<JobProfileIndex> { jobProfileIndexDocument });
             IndexDocumentsOptions options = new IndexDocumentsOptions { ThrowOnAnyError = true };
             return searchClient.IndexDocumentsAsync(jobProfileIndexBatch, options);
         }
-
     }
 }
