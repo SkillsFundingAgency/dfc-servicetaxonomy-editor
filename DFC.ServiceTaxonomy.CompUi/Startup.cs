e1aca76031ad0e739765f62160d894120558f221
amjones99 <59894100+amjones99@users.noreply.github.com>
2024-01-22 14:18:57 +0000

    Startup.cs and pipeline changes

--- DFC.ServiceTaxonomy.CompUi/Startup.cs
+++ DFC.ServiceTaxonomy.CompUi/Startup.cs
@@ -4,30 +4,38 @@ using DFC.Common.SharedContent.Pkg.Netcore.IUtilities;
 using DFC.ServiceTaxonomy.CompUi.Dapper;
 using DFC.ServiceTaxonomy.CompUi.Handlers;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.ContentManagement.Handlers;
 using OrchardCore.Modules;
-using YesSql;
+using Microsoft.Extensions.Configuration;
 
 namespace DFC.ServiceTaxonomy.CompUi
 {
-    public class Startup : StartupBase
+    public class Startup
     {
-        public override void ConfigureServices(IServiceCollection services)
+        private const string RedisCacheConnectionStringAppSettings = "Cms:RedisCacheConnectionString";
+
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IContentHandler, CacheHandler>();
             services.AddTransient<IDapperWrapper, DapperWrapper>();
             services.AddTransient<IUtilities, Utilities>();
-            //Todo add the code to read this from the appsettings.json
-            //services.AddSharedContentRedisInterface("dfc-dev-shared-rdc.redis.cache.windows.net:6380,password=Nuzqmeax2bVwFYQQ7YCbDcxexbtBNUuyyAzCaOtGPLo=,ssl=True,abortConnect=False");
 
+            services.AddStackExchangeRedisCache(options => { options.Configuration = configuration.GetSection(RedisCacheConnectionStringAppSettings).Get<string>(); });
             services.AddTransient<ISharedContentRedisInterface, SharedContentRedis>();
 
             services.AddAutoMapper(typeof(Startup).Assembly);
         }
 
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+        public void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
         {
         }
     }
