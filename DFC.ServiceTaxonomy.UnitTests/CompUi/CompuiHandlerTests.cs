2a30018c0d1727bb67d79ab238ad8c578a48ca98
amjones99 <59894100+amjones99@users.noreply.github.com>
2024-01-25 15:46:30 +0000

    Fixed unit test

--- DFC.ServiceTaxonomy.UnitTests/CompUi/CompuiHandlerTests.cs
+++ DFC.ServiceTaxonomy.UnitTests/CompUi/CompuiHandlerTests.cs
@@ -147,7 +147,7 @@ namespace DFC.ServiceTaxonomy.UnitTests.CompUi
 
             //Assert
             A.CallTo(() => _fakeDapperWrapper.QueryAsync<NodeItem>(A<DbConnection>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
-            A.CallTo(() => _fakeSharedContentRedisInterface.InvalidateEntityAsync(A<string>.Ignored)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _fakeSharedContentRedisInterface.InvalidateEntityAsync(A<string>.Ignored)).MustHaveHappened(3, Times.Exactly);
         }
 
         [Fact]
@@ -164,7 +164,7 @@ namespace DFC.ServiceTaxonomy.UnitTests.CompUi
 
             //Assert
             A.CallTo(() => _fakeDapperWrapper.QueryAsync<NodeItem>(A<DbConnection>.Ignored, A<string>.Ignored)).MustHaveHappenedOnceExactly();
-            A.CallTo(() => _fakeSharedContentRedisInterface.InvalidateEntityAsync(A<string>.Ignored)).MustHaveHappenedTwiceExactly();
+            A.CallTo(() => _fakeSharedContentRedisInterface.InvalidateEntityAsync(A<string>.Ignored)).MustHaveHappened(6, Times.Exactly);
         }
 
         [Fact]
