37ae7a50067f596ecaf6b8ed765a743b087106bf
James <152189499+JamesPrior-07@users.noreply.github.com>
2024-02-13 10:49:22 +0000

    Fixed Triage tool 'where' clause issue

--- DFC.ServiceTaxonomy.PageLocation/GraphQL/PageLocationInputObjectType.cs
+++ DFC.ServiceTaxonomy.PageLocation/GraphQL/PageLocationInputObjectType.cs
@@ -1,6 +1,4 @@
-﻿
-
-using DFC.ServiceTaxonomy.PageLocation.Models;
+﻿using DFC.ServiceTaxonomy.PageLocation.Models;
 using GraphQL.Types;
 using Microsoft.Extensions.Localization;
 using OrchardCore.Apis.GraphQL.Queries;
@@ -16,6 +14,7 @@ namespace DFC.ServiceTaxonomy.PageLocation.GraphQL
 
             AddScalarFilterFields<StringGraphType>("url", S["full Url"]);
             AddScalarFilterFields<StringGraphType>("urlName", S["Url Name"]);
+            AddScalarFilterFields<BooleanGraphType>("useInTriageTool", S["Use In triage tool"]);
 
         }
     }
