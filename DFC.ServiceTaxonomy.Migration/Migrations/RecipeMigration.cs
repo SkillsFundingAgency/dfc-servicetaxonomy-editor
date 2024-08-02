ab25b37b8936530a8e63edbf3e39483ed5ce3165
Gavin COMPTON <Gavin.COMPTON@EDUCATION.GOV.UK>
2024-08-02 17:38:08 +0100

    Added header content type and content item and a new migration project to impor them into STAX.

---
+++ DFC.ServiceTaxonomy.Migration/Migrations/RecipeMigration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging;
+using OrchardCore.Data.Migration;
+using OrchardCore.Recipes.Services;
+
+namespace DFC.ServiceTaxonomy.Migration.Migrations
+{
+    public class RecipeMigration : DataMigration
+    {
+        private readonly IRecipeMigrator recipeMigrator;
+        private readonly ILogger<RecipeMigration> logger;
+
+        public RecipeMigration(IRecipeMigrator recipeMigrator, ILogger<RecipeMigration> logger)
+        {
+            this.recipeMigrator = recipeMigrator;
+            this.logger = logger;
+        }
+
+        public async Task<int> CreateAsync()
+        {
+            try
+            {
+                logger.LogInformation($" Started CreateAsync Migration from DFC.ServiceTaxonomy.Migration");
+
+                var recipes = new string[]
+                {
+                    $"{Constants.ContentItemsLocation}AddHeaderContentItem.recipe.json",
+                    $"{Constants.ContentTypesLocation}AddHeaderContentType.recipe.json",
+                };
+
+                foreach (var recipe in recipes)
+                {
+                    await recipeMigrator.ExecuteAsync(recipe, this);
+                }
+            }
+            catch (Exception exception)
+            {
+                logger.LogError($"CreateAsync Migration failed {exception.Message}", exception);
+
+                throw;
+            }
+
+            logger.LogInformation($"Completed CreateAsync Migration from DFC.ServiceTaxonomy.Migration");
+
+            return 1;
+        }
+    }
+}
---
+++ DFC.ServiceTaxonomy.UnitTests/Migrations/MigrationTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using DFC.ServiceTaxonomy.Migration.Migrations;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using OrchardCore.Data.Migration;
+using OrchardCore.Recipes.Services;
+using Xunit;
+
+namespace DFC.ServiceTaxonomy.UnitTests.Migrations
+{
+    public class MigrationTests
+    {
+        private readonly Mock<IRecipeMigrator> recipeMigrator = new(MockBehavior.Strict);
+        private readonly Mock<ILogger<RecipeMigration>> logger = new();
+
+        [Fact]
+        public async Task CreateAsync_ReturnsExpected()
+        {
+            //Arrange
+            recipeMigrator.Setup(service => service.ExecuteAsync(It.IsAny<string>(), It.IsAny<IDataMigration>())).ReturnsAsync(string.Empty);
+            var migration = new RecipeMigration(recipeMigrator.Object, logger.Object);
+
+            //Act
+            var result = await migration.CreateAsync();
+
+            //Assert
+            recipeMigrator.Verify(service => service.ExecuteAsync(It.IsAny<string>(), It.IsAny<IDataMigration>()), Times.Exactly(2));
+            result.Should().Be(1);
+        }
+    }
+}
+
