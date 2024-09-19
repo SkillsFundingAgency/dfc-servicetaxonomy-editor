6fcac9467fa0621f46c4bbda33de6b8c74d2fa13
Chris Ly <chris.ly@education.gov.uk>
2024-09-19 22:47:04 +0100

    Add migration for Explore Careers content type

---
+++ DFC.ServiceTaxonomy.ExploreCareersMigration/Migrations/Migrations.cs
@@ -0,0 +1,22 @@
+﻿using OrchardCore.Data.Migration;
+using OrchardCore.Recipes.Services;
+
+namespace DFC.ServiceTaxonomy.ExploreCareersMigration.Migrations
+{
+    public class ExploreCareersMigrations : DataMigration
+    {
+        private readonly IRecipeMigrator _recipeMigrator;
+
+        public ExploreCareersMigrations(IRecipeMigrator recipeMigrator)
+        {
+            _recipeMigrator = recipeMigrator;
+        }
+
+        public async Task<int> CreateAsync()
+        {
+            await _recipeMigrator.ExecuteAsync("MigrationRecipes/SectorLandingPageContentType.json", this);
+
+            return 1;
+        }
+    }
+}
