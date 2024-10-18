838c02884744b39b826ceb913f13163fa9032c09
James <152189499+JamesPrior-07@users.noreply.github.com>
2024-10-18 15:42:52 +0100

    Update filtering of content items and update page redirect uri retrieval

--- DFC.ServiceTaxonomy.CustomEditor/Constants/ConfigKeys.cs
+++ DFC.ServiceTaxonomy.CustomEditor/Constants/ConfigKeys.cs
@@ -5,5 +5,14 @@
         public const string PreviewRedirectUri = "PreviewRedirectUri";
 
         public const string EventGridAllowedPagesList = "EventGridAllowedPagesList";
+
+        public const string EventGridAllowedContentType = "EventGridAllowedContentList";
+
+        public const string StaxPreviewUrl = "NcsPreview:PreviewLoginUrl";
+
+        public const string CipherTextValiditySeconds = "NcsPreview:CipherTextValiditySeconds";
+
+        public const string CipherTextPrefix = "NcsPreview:CipherTextPrefix";
+
     }
 }
--- DFC.ServiceTaxonomy.CustomEditor/Controllers/StaxPreviewController.cs
+++ DFC.ServiceTaxonomy.CustomEditor/Controllers/StaxPreviewController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DFC.ServiceTaxonomy.CustomEditor.Constants;
 using DfE.NCS.Framework.Core.Crypto.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -9,7 +10,6 @@ namespace DFC.ServiceTaxonomy.CustomEditor.Controllers
     public class StaxPreviewController : Controller
     {
         private readonly IConfiguration _configuration;
-        private const string staxPreviewUrl = "NcsPreview:PreviewLoginUrl";
         private readonly ILogger<StaxPreviewController> _logger;
         private readonly ICryptographyManager _cryptographyManager;
 
@@ -27,20 +27,22 @@ namespace DFC.ServiceTaxonomy.CustomEditor.Controllers
 
         public ActionResult GotoUrl(string path)
         {
-            var staxPreviewLoginUrl = _configuration[staxPreviewUrl];
+            var previewLoginUrl = _configuration[ConfigKeys.StaxPreviewUrl];
             long validFromTicks = DateTime.UtcNow.Ticks;
-            if (!long.TryParse(_configuration["NcsPreview:CipherTextValiditySeconds"], out var cipherValiditySeconds))
+
+            if (!long.TryParse(_configuration[ConfigKeys.CipherTextValiditySeconds], out var cipherValiditySeconds))
             {
-                _logger.LogWarning("Invalid value for 'NcsPreview:CypherTextValiditySeconds' setting it to default 60 seconds");
+                _logger.LogWarning($"Invalid value for: {ConfigKeys.CipherTextValiditySeconds}, setting it to default 60 seconds");
                 cipherValiditySeconds = 60;
             }
+
             long validToTicks = DateTime.UtcNow.AddSeconds(cipherValiditySeconds).Ticks;
-            string cipherText = $"{validFromTicks}:{_configuration["NcsPreview:CipherTextPrefix"]}:{validToTicks}";
+            string cipherText = $"{validFromTicks}:{_configuration[ConfigKeys.CipherTextPrefix]}:{validToTicks}";
             string encryptedText = _cryptographyManager.EncryptString(cipherText);
             string escapedString = Uri.EscapeDataString(encryptedText);
-            var url = $"{staxPreviewLoginUrl}?data={escapedString}&redirect={path}";
-            _logger.LogWarning("Do the dew");
-            return Content(url);
+            var previewUrl = $"{previewLoginUrl}?data={escapedString}&redirect={path}";
+
+            return Content(previewUrl);
         }
     }
 }
