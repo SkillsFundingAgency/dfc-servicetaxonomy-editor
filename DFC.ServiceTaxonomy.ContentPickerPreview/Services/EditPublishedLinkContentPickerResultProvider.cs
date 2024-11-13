37ff912689eb19a2bf6315e6d636451976ad4381
amjones99 <59894100+amjones99@users.noreply.github.com>
2024-11-13 13:38:02 +0000

    Package upgrade, errors to be fixed

--- DFC.ServiceTaxonomy.ContentPickerPreview/Services/EditPublishedLinkContentPickerResultProvider.cs
+++ DFC.ServiceTaxonomy.ContentPickerPreview/Services/EditPublishedLinkContentPickerResultProvider.cs
@@ -33,7 +33,7 @@ namespace DFC.ServiceTaxonomy.ContentPickerPreview.Services
             if (searchContext.DisplayAllContentTypes)
             {
                 contentTypes = _contentDefinitionManager
-                    .ListTypeDefinitions()
+                    .ListTypeDefinitionsAsync().GetAwaiter().GetResult()
                     .Where(x => string.IsNullOrEmpty(x.GetSettings<ContentTypeSettings>().Stereotype))
                     .Select(x => x.Name)
                     .AsEnumerable();
