dfd38e6d579be4c9e40fa54d099a0e83318e81b0
amjones99 <59894100+amjones99@users.noreply.github.com>
2025-01-09 10:27:04 +0000

    Fixed naming issue

--- DFC.ServiceTaxonomy.JobProfiles.DataTransfer/GraphQL/SocSkillsMatrixQueryObjectType.cs
+++ DFC.ServiceTaxonomy.JobProfiles.DataTransfer/GraphQL/SocSkillsMatrixQueryObjectType.cs
@@ -3,14 +3,13 @@ using OrchardCore.Apis.GraphQL;
 using DFC.ServiceTaxonomy.JobProfiles.DataTransfer.Models.ServiceBus;
 
 
-//Custom Media Field object type overriding existing graphql object type to expose the media field alt text field
 namespace DFC.ServiceTaxonomy.JobProfiles.DataTransfer.GraphQL
 {
     public class SocSkillsMatrixQueryObjectType : ObjectGraphType<SocSkillMatrixContentItem>
     {
         public SocSkillsMatrixQueryObjectType()
         {
-            Name = "NCSMediaField";
+            Name = "SocSkillsMatrixField";
 
             Field<StringGraphType>()
                 .Name("Contextualised")
