0a3abb522e2eb1a92bc226d36a23b4ffe7a60b2b
Mark Godfrey <mark1.godfrey@education.gov.uk>
2025-04-30 11:18:30 +0100

    Fix SP fetch - it was using application id instead of object id

--- Resources/PSScripts/New-ServicePrincipalAndStoreSecrets.ps1
+++ Resources/PSScripts/New-ServicePrincipalAndStoreSecrets.ps1
@@ -63,7 +63,7 @@ function New-Password{
 }
 
 $Context = Get-AzContext
-$AzureDevOpsServicePrincipal = Get-AzADServicePrincipal -ApplicationId $Context.Account.Id
+$AzureDevOpsServicePrincipal = Get-AzADServicePrincipal -ObjectId $Context.Account.Id
 # Comment above line and uncomment line below to test localy with your user account
 #$AzureDevOpsServicePrincipal = Get-AzADUser -UserPrincipalName $Context.Account.Id
 
