8dc9666f385b404277f1c05dd298529533ff6967
Karim Bachir <kbachir@spartaglobal.com>
2025-05-06 10:58:10 +0100

    comment out broken code from script (old edgio cdn profile has been removed by Azure) !

--- Resources/PSScripts/New-ServicePrincipalAndStoreSecrets.ps1
+++ Resources/PSScripts/New-ServicePrincipalAndStoreSecrets.ps1
@@ -163,23 +163,23 @@ IF (!$vaultKey){
 }
 
 
-$roleAssignment = Get-AzRoleAssignment `
-    -ResourceType "Microsoft.Cdn/profiles"  `
-    -ResourceName $CdnProfileName  `
-    -ResourceGroupName $SharedResourceGroupName  `
-    -RoleDefinitionName "Contributor" | Where-Object {$_.DisplayName -eq "$($AdServicePrincipal.DisplayName)"}
-if (!$roleAssignment) {
-    Write-Verbose "'Contributor' Role assignment to $($AdServicePrincipal.ServicePrincipalNames) for $($CdnProfileName) NOT FOUND"
-    Write-Verbose "Adding 'Contributor' Role assignment to $($AdServicePrincipal.ServicePrincipalNames) for $($CdnProfileName)"
-    New-AzRoleAssignment -ApplicationId $AdServicePrincipal.ApplicationId  `
-        -ResourceType "Microsoft.Cdn/profiles"  `
-        -ResourceName $CdnProfileName  `
-        -ResourceGroupName $SharedResourceGroupName  `
-        -RoleDefinitionName "Contributor"
-    Write-Verbose "Added 'Contributor' Role assignment to $($AdServicePrincipal.ServicePrincipalNames) for $($CdnProfileName)"
-} else {
-    Write-Verbose "$($roleAssignment.DisplayName) has CONTRIBUTOR permissions for $($CdnProfileName)"   
-}
+# $roleAssignment = Get-AzRoleAssignment `
+#     -ResourceType "Microsoft.Cdn/profiles"  `
+#     -ResourceName $CdnProfileName  `
+#     -ResourceGroupName $SharedResourceGroupName  `
+#     -RoleDefinitionName "Contributor" | Where-Object {$_.DisplayName -eq "$($AdServicePrincipal.DisplayName)"}
+# if (!$roleAssignment) {
+#     Write-Verbose "'Contributor' Role assignment to $($AdServicePrincipal.ServicePrincipalNames) for $($CdnProfileName) NOT FOUND"
+#     Write-Verbose "Adding 'Contributor' Role assignment to $($AdServicePrincipal.ServicePrincipalNames) for $($CdnProfileName)"
+#     New-AzRoleAssignment -ApplicationId $AdServicePrincipal.ApplicationId  `
+#         -ResourceType "Microsoft.Cdn/profiles"  `
+#         -ResourceName $CdnProfileName  `
+#         -ResourceGroupName $SharedResourceGroupName  `
+#         -RoleDefinitionName "Contributor"
+#     Write-Verbose "Added 'Contributor' Role assignment to $($AdServicePrincipal.ServicePrincipalNames) for $($CdnProfileName)"
+# } else {
+#     Write-Verbose "$($roleAssignment.DisplayName) has CONTRIBUTOR permissions for $($CdnProfileName)"   
+# }
 
 
 
