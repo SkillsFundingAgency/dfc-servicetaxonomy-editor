a4a2b525e2df7fdfd9220c999487c39b9248fa5f
mlarkowski-DFE <178817151+mlarkowski-DFE@users.noreply.github.com>
2026-02-25 16:35:32 +0000

    more pipeline error fixes

--- DFC.ServiceTaxonomy.CustomEditor/Shapes/SummaryAdminShapes.cs
+++ DFC.ServiceTaxonomy.CustomEditor/Shapes/SummaryAdminShapes.cs
@@ -6,18 +6,9 @@ namespace DFC.ServiceTaxonomy.CustomEditor.Shapes
 {
     public class SummaryAdminShapes : IShapeTableProvider
     {
-        public void Discover(ShapeTableBuilder builder)
-        {
-            builder.Describe("Content_SummaryAdmin").OnDisplaying(context =>
-            {
-                dynamic shape = context.Shape;
-                Shape actions = (Shape)shape.Actions;
-                actions.Remove("ContentsButtonEdit_SummaryAdmin");
-            });
-        }
-
         public async ValueTask DiscoverAsync(ShapeTableBuilder builder)
         {
+            await Task.Yield();
             builder.Describe("Content_SummaryAdmin").OnDisplaying(context =>
             {
                 dynamic shape = context.Shape;
